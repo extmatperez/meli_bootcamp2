be584783cbca3f487b8e992851d2b528770587cf
Edward Suarez <edward.suarez@mercadolibre.com.co>
2021-11-23 11:26:14 -0500

    feat: Solve the 1st exercise

---
+++ 2_gobases1/TM/Ejercicio1/README.md
@@ -0,0 +1,4 @@
+# Ejercicio 1 - Imprimí tu nombre
+
+- Crea una aplicación donde tengas como variable tu nombre y dirección
+- Imprime en consola el valor de cada una de las variables.
---
+++ 2_gobases1/TM/Ejercicio1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nombre, direccion string = "Edward", "Medellin, Colombia"
+	fmt.Print("Mi nombre es: ", nombre, "\n")
+	fmt.Println("Mi direccion es : ", direccion)
+}
