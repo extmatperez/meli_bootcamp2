1d2d43a5e3b897f061f120f58c43853cd946d0af
Walter Jesus Castillo Bastidas <waltercastillo017@outlook.com>
2021-11-23 10:42:07 -0500

    Ejercicio 4

---
+++ 2_gobases1/Ejercicio4/ejercicio4.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+		var apellido string = "Gomez" correcta
+		var edad int = "35" incorreta
+		boolean := "false"; incorrecta
+		var sueldo string = 45857.90 incorrecta
+		var nombre string = "Julián" correcta
+	*/
+
+	var apellido string = "Gomez"
+	var edad int = 35
+	boolean := false
+	var sueldo string = "45857.90"
+	var nombre string = "Julián"
+
+	fmt.Printf("%T\n", apellido)
+	fmt.Printf("%T\n", edad)
+	fmt.Printf("%T\n", boolean)
+	fmt.Printf("%T\n", sueldo)
+	fmt.Printf("%T\n", nombre)
+
+}
