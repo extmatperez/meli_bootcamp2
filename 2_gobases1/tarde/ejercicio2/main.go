2cb31dca16c32e309be0b48602250459c2df63a2
bribeltran <brian.beltranmercadolibre.com.co>
2021-11-23 18:45:43 -0500

    se agrega solucion ejercicio2

--- 2_gobases1/tarde/ejercicio2/main.go
+++ 2_gobases1/tarde/ejercicio2/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("text")
+	var precio, precioFianl, descuento float64
+
+	precio = 1000
+	descuento = 10
+	precioFianl = precio - precio*(descuento/100)
+	fmt.Println("precio :", precio, "descuento :", descuento, "precio con descuento :", precioFianl)
 }
