1835528c9bcbcef6aa48583b2b0893dbf172c6ed
crispucaMeLi <cristian.puca@mercadolibre.com>
2021-11-23 17:55:13 -0300

    empezando practica tarde

--- 2_gobases1/TM/Ejercicio3/ejercicio3.go
+++ 2_gobases1/TM/Ejercicio3/ejercicio3.go
@@ -6,12 +6,12 @@ var 1nombre string    				//el nombre la variable no debe empezar con un numero
 
 var apellido string   				//esta ok
 
-var int edad          				//esta ok
+var int edad          				//esta mal el orden
 
 1apellido := 6        				//el nombre la variable no debe empezar con un numero
 
-var licencia_de_conducir = true     //mas alla que el editor lo marque como bien, en caso de llevar varias palabras el nombre
-									//de la variable, se debe escribir todo junto y la palabra siguiente empieza con Mayuscula
+var licencia_de_conducir = true     /* mas alla que el editor lo marque como bien, en caso de llevar varias palabras el nombre
+									de la variable, se debe escribir todo junto y la palabra siguiente empieza con Mayuscula*/
 
 var estatura de la persona int      //esta mal definida la variable, no debe existir espacios en el nombre de la misma
 
--- 2_gobases1/TM/Ejercicio4/ejercicio4.go
+++ 2_gobases1/TM/Ejercicio4/ejercicio4.go
@@ -1,10 +1,13 @@
-var apellido string = "Gomez"		//Esta ok la asignacion de tipo
+var apellido string = "Gomez" //Esta ok la asignacion de tipo
 
-var edad int = "35"					//Esta bien la asignacion de tipo
+var edad int = "35" //Esta bien la asignacion de tipo
 
-boolean := "false";					//Esta mal la asignacion de tipo, el false no deberia ir entre comillas
+boolean := "false" /*Esta mal la asignacion de tipo, el false no deberia ir entre comillas
+Aclaracion funciona la declaracion propuesta*/
 
-var sueldo string = 45857.90		//Esta mal la asignacion de tipo, al ser de tipo string, 
-									//ademas al tratarse de un dato como un sueldo seria mejor tratarlo como un float
+var sueldo string = 45857.90 /*Esta mal la asignacion de tipo, al ser de tipo string,
+ademas al tratarse de un dato como un sueldo seria mejor tratarlo como un float*/
 
-var nombre string = "Julián"		//Esta bien la asignacion de tipo de dato
+var nombre string = "Julián" //Esta bien la asignacion de tipo de dato
+
+
