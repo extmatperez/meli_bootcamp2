8235c093a386da5df2b269b0a247ec43af9b9c3c
igvargas <ignacio.vargas@mercadolibre.com>
2021-11-23 12:55:51 -0300

    ex3 finished

---
+++ 2_gobases1/ejercicio_3/ej3.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+Variables correctas:
+	var apellido string
+	cantidadDeHijos := 2
+
+Variables incorrectas:
+	var 1nombre string
+  	var int edad
+	1apellido := 6
+	var licencia_de_conducir = true
+	var estatura de la persona int
+*/
