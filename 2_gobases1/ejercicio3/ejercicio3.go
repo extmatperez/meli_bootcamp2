4123c7f058c03fa38fa864604d6c0b2923587d24
Santiago Nicolas Panceri <spanceri@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-11-23 12:28:25 -0300

    ejercicio3

---
+++ 2_gobases1/ejercicio3/ejercicio3.go
@@ -0,0 +1,10 @@
+
+var nombre string   //Incorrecta. var 1nombre string
+var apellido string //Correcto
+var int edad        //Correcto
+
+var apellido string            //Incorrecta.  1apellido := 6
+licencia_de_conducir := true   //Incorrecta.  var licencia_de_conducir := true
+var estatura_de_la_persona int //Incorrecta.  var estatura_de_la_persona int
+cantidadDeHijos := 2           //Incorrecta. var cantidadDeHijos := 2
+
