cdb053a86ae336a50c5fe55891d6e0683ab6ca8a
Ariel Romero <arielrom27@gmail.com>
2021-11-23 12:23:02 -0300

    add(2_gobases1/ejercicio2) solved exercise 2

---
+++ 2_gobases1/ejercicio2/ejercicio2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var temperatura float64
+	var humedad int
+	var presion float64
+
+	temperatura = 20.0
+	humedad = 80
+	presion = 1018
+
+	fmt.Println("La temperatura es: ", temperatura, "ºC, la humedad es: ", humedad, "%, la presion es: ", presion, "hPa")
+}
