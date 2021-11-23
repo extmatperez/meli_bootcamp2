333aa87d11b3685e71597db5492cb139928ff62d
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2021-11-23 16:32:30 -0500

    Bases 1 Ejercicio 4 Tarde

---
+++ 2_gobases1/ejt_4/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var MONTHS = map[int]string{1:"Enero", 2:"Febrero", 3:"Marzo", 4:"Abril", 5:"Mayo", 6:"Junio", 7:"Julio", 8:"Agosto", 9:"Septiembre", 10:"Octubre", 11:"Noviembre", 12:"Diciembre"}
+	
+	var selectedMonth int = 5
+	fmt.Println(MONTHS[selectedMonth])
+}
+
+// También se podría hacer con un switch o con if else
