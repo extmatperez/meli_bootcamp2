2794f26ba3bf51330077b3924cdd218f72007d90
Federico Archuby <federico.archuby@mercadolibre.com>
2021-11-23 17:45:17 -0300

    Exercises 2 and 3 from practice afternoon

--- 2_gobases1/tarde/main.go
+++ 2_gobases1/tarde/main.go
@@ -12,13 +12,23 @@ func main() {
 	//Ejercicio 1
 	reader := bufio.NewReader(os.Stdin)
 	var palabra string
+	fmt.Println("Ingrese la palabra: ")
 	palabra, _ = reader.ReadString('\n')
-	fmt.Printf("%v%T\n", palabra, palabra)
 	var letras []string = strings.Split(palabra, "")
-	fmt.Printf("%v", letras)
 	fmt.Printf("La palabra tiene %d letras\n", len(letras))
 	fmt.Print("Palabra deletreada: ")
 	for _, letra := range letras {
 		fmt.Printf("%s ", letra)
 	}
+	fmt.Println()
+
+	//Ejercicio 2
+	var precio, descuento float32
+	fmt.Println("Ingrese el precio: ")
+	fmt.Scanf("%f", &precio)
+	fmt.Println("Ingrese el descuento en porcentaje: ")
+	fmt.Scanf("%f", &descuento)
+
+	precioFinal := (precio * descuento) / 100
+	fmt.Printf("El precio final es de %v pesos", precioFinal)
 }
