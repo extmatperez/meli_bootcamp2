5b8da226840778cf622ea508cf430e8449ad0add
igvargas <ignacio.vargas@mercadolibre.com>
2021-11-23 13:05:16 -0300

    ex4 finished

---
+++ 2_gobases1/ejercicio_4/ej4.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var apellido string = "Gomez"
+	// var edad int = 35
+	// boolean := "false"
+	// var sueldo float64 = 45857.90
+	var nombre string = "Julián"
+
+	fmt.Println(nombre)
+}
