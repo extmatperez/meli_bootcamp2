22bf11c7acb16290ac6865b8ae40b506159b9321
benjaminconti <benjamin.conti@mercadolibre.com>
2021-11-23 12:03:28 -0300

    ejercicio1 finished

---
+++ 2_gobases1/ejercicio1/ejercicio1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	nombre := "Benjamin Conti"
+
+	direccion := "Carlos Laveran 5781"
+
+	fmt.Println("Mi nombre es", nombre, "y mi dirección es", direccion)
+}
