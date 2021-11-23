f8c3addcd2d768d7bd321fb8bde09b232400d891
estefaniaPecora <barbara.pecora@mercadolibre.com>
2021-11-23 12:52:50 -0300

    exercise 3

---
+++ 2_gobases1/gb1_ejercicio3/main.go
@@ -0,0 +1,19 @@
+
+
+// Ejercicio 3
+// Un profesor de programación está corrigiendo los exámenes de sus estudiantes de la materia Programación I para poder brindarles las correspondientes devoluciones. Uno de los puntos del examen consiste en declarar distintas variables. 
+// Necesita ayuda para:
+// Detectar cuáles de estas variables que declaró el alumno son correctas.
+// Corregir las incorrectas.
+
+// var 1nombre string 				---> INCORRECTA / var nombre1 string
+// var apellido string 				---> CORRECTA
+// var int edad 					---> INCORRECTA / var edad int
+// 1apellido := 6 					---> INCORRECTA / apellido1 := "apellido"
+// var licencia_de_conducir = true  ---> INCORRECTA / var licencia_de_conducir bool = true
+// var estatura de la persona int	---> INCORRECTA / var estatura_de_la_persona int
+// cantidadDeHijos := 2				---> CORRECTA
+
+
+
+
