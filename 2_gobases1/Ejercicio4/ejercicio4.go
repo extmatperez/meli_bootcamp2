81453369abc7ad316d98e732d93d02261c6d21a7
Walter Jesus Castillo Bastidas <waltercastillo017@outlook.com>
2021-11-23 11:24:45 -0500

    arreglo punto 4

--- 2_gobases1/Ejercicio4/ejercicio4.go
+++ 2_gobases1/Ejercicio4/ejercicio4.go
@@ -7,14 +7,14 @@ func main() {
 	/*
 		var apellido string = "Gomez" correcta
 		var edad int = "35" incorreta
-		boolean := "false"; incorrecta
+		boolean := "false"; correcta
 		var sueldo string = 45857.90 incorrecta
 		var nombre string = "Julián" correcta
 	*/
 
 	var apellido string = "Gomez"
 	var edad int = 35
-	boolean := false
+	boolean := "false"
 	var sueldo string = "45857.90"
 	var nombre string = "Julián"
 
