04da8d8ea33c68d4d0bc4b2c77709bb872328f0e
AG-Meli <andres.ghione@mercadolibre.com>
2021-11-23 12:24:09 -0300

    correction

--- 2_gobases1/clase_matutina/Ejercicios_Bootcamp/ejercicio4/ejercicio4.go
+++ 2_gobases1/clase_matutina/Ejercicios_Bootcamp/ejercicio4/ejercicio4.go
@@ -1,7 +1,5 @@
 var apellido string = "Gomez" //Correcta
-var edad int = "35"           // Correcta
+var edad int = "35"           // Incorrecta, va sin comillas
 boolean := "false"            // Error, el := no lleva tipo, sino el nombre de la variable, ademas, el tipo es bool y va sin comillas
 var sueldo string = 45857.90  // Error, no es un string, faltan las "" en caso de tener ese valor la variable, pero deberia ser un float
 var nombre string = "Julián"  // Correcta
-
-
