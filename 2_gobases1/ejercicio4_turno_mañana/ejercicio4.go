c750a6bd4280d3f8b93fc135727cb75c525fd57f
Franco Julian Andrada Arganaraz <fandrada@ar0c02g310pml85.fibertel.com.ar>
2021-11-23 13:40:05 -0300

    feat(ejercicio4_turno_manana): ejercicio4

---
+++ 2_gobases1/ejercicio4_turno_mañana/ejercicio4.go
@@ -0,0 +1,9 @@
+// Ejercicio 4 - Tipos de datos
+// Un estudiante de programación intentó realizar declaraciones de variables con sus respectivos tipos en Go pero tuvo varias dudas mientras lo hacía. A partir de esto, nos brindó su código y pidió la ayuda de un desarrollador experimentado que pueda:
+// Verificar su código y realizar las correcciones necesarias.
+
+  var apellido string = "Gomez"
+  var edad int = 35
+  boolean := false
+  var sueldo string = "45857.90"
+  var nombre string = "Julián"
