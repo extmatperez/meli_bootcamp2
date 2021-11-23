1b669f15ccc9c7c9298cad34c15a80e6e1d9a08d
Matias Ziliotto <matias.ziliotto@mercadolibre.com>
2021-11-23 12:24:47 -0300

    Ejercicio3

---
+++ 2_gobases1/Ejercicio3/var_declarations.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	/*
+		var 1nombre string 					// BAD
+		var int edad 						// BAD
+		var apellido string 				// OK
+		1apellido := 6 						// BAD
+		var licencia_de_conducir = true 	// OK
+		var estatura de la persona int 		// BAD
+		cantidadDeHijos := 2 				// OK
+	*/
+
+	/*
+		// FIXES
+		var nombre1 string
+		var edad int
+		apellido1 := 6
+		var estatura_de_la_persona int
+	*/
+}
