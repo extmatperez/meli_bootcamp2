8f8ed7bb6f14a28528fa578605346d0db99257a0
Ariel Romero <arielrom27@gmail.com>
2021-11-23 13:56:17 -0300

    fix(ejercicio3/ejercicio3.go) fixed answer

--- 2_gobases1/ejercicio3/ejercicio3.go
+++ 2_gobases1/ejercicio3/ejercicio3.go
@@ -3,7 +3,7 @@ package main
 /*
 var 1nombre string  INCORRECTA porque empieza con un numero
 var apellido string  CORRECTA
-var int edad  CORRECTA
+var int edad  INCORRECTA
 1apellido := 6  INCORRECTA porque empieza con un numero y porque no puede ser un un numero el apellido
 var licencia_de_conducir = true  CORRECTA
 var estatura de la persona int  INCORRECTA porque tiene espacios
@@ -14,7 +14,7 @@ cantidadDeHijos := 2  CORRECTA
 func main() {
 	var nombre string
 	var apellido string
-	var int edad
+	var edad int
 	apellidox := "seis"
 	var licencia_de_conducir = true
 	var estatura_de_la_persona int
