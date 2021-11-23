6f7032e33586a2b75622698836f7963c08119ccd
igvargas <ignacio.vargas@mercadolibre.com>
2021-11-23 12:19:22 -0300

    ex1 finished

---
+++ 2_gobases1/ejercicio_1/ej1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nombre string = "Nacho"
+	var direccion string = "av colon 2577"
+
+	fmt.Printf("Mi nombre es: %s\n", nombre)
+	fmt.Printf("Mi direccion es: %s\n", direccion)
+}
