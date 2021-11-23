c196e3d0da49657b453d1b2d4a12ca8f94c0987e
dignelidxdx <digneli.davila@mercadolibre.com>
2021-11-23 13:02:47 -0300

    Fixed typo exercite 3 and added exercite 4 with correct form

--- 2_gobases1/turn_1/exercite_3/exercite.go
+++ 2_gobases1/turn_1/exercite_3/exercite.go
@@ -4,29 +4,40 @@ import "fmt"
 
 func main() {
 
-	//var 1nombre string
+	//.1
+	//bad: var 1nombre string
 	//correct form:
 	var nombre string
 	nombre = "Mari"
 	fmt.Println("Nombre:", nombre)
+	//2.
+	//Good
 	var apellido string
 	apellido = "Petit"
 	fmt.Println("Apellido:", apellido)
+	//3.
+	//bad: var int edad
+	//correct form:
 	var edad int
 	edad = 20
 	fmt.Println("Edad:", edad)
-	//1apellido := 6
+	//4.
+	//bad: 1apellido := 6
 	//correct form:
 	apellidoUno := 6
 	fmt.Println("Apellido uno:", apellidoUno)
-	//var licencia_de_conducir = true
+	//5.
+	//bad: var licencia_de_conducir = true
 	//correct form:
 	var licenciaDeConducir = true
 	fmt.Println("Licencia de conducir:", licenciaDeConducir)
-	//var estatura de la persona int
+	//6.
+	//bad: var estatura de la persona int
 	//correct form:
 	var estaturaDeLaPersona int
 	fmt.Println("Estatura de la persona:", estaturaDeLaPersona)
+	//7.
+	//Good
 	cantidadDeHijos := 2
 	fmt.Println("Cantidad de hijos:", cantidadDeHijos)
 
