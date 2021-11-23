a36ded9a6530441ac57e7dfc6aa79a9d22092a0d
dignelidxdx <digneli.davila@mercadolibre.com>
2021-11-23 13:03:10 -0300

    Added exercite 4

---
+++ 2_gobases1/turn_1/exercite_4/exercite.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//1.
+	//var apellido string = "Gomez"
+	var apellido string
+	apellido = "Gomez"
+	fmt.Println("Apellido:", apellido)
+	//2.
+	//var edad int = "35"
+	edad := 35
+	fmt.Println("Edad:", edad)
+	//3.
+	//boolean := "false";
+	boolean := false
+	fmt.Println("Verdedo o falso:", boolean)
+	//4.
+	//var sueldo string = 45857.90
+	var sueldo float64
+	sueldo = 45857.90
+	fmt.Println("Sueldo:", sueldo)
+	//5.
+	var nombre string
+	nombre = "Julián"
+	fmt.Println("Nombre:", nombre)
+
+}
