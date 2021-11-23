fad6f346294aff8a554bb2643410fc57dd9c278d
jpacostasc <juan.acosta@mercadolibre.com>
2021-11-23 12:31:42 -0300

    Ejercicio 4 completado

---
+++ 2_gobases1/TMañana/Ejercicio 4/Ejercicio4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var apellido string = "Gomez"
+	var edad int = 35 //No debe ir entre comillas
+	boolean := false //No debe ir entre comillas
+	var sueldo string = "4587.90" //Al ser string, debe ir entre comillas o cambiar el tipo
+	var nombre string = "Julián"
+	fmt.Println(apellido, edad, boolean, sueldo, nombre)
+}
