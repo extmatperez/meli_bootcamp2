196dc69cb5e2bf8f8194852e08f0f7a97d59d84b
FacuBouza <facundo.bouza@mercadolibre.com>
2021-11-23 12:42:34 -0300

    Ejercicio 4 finalizado

---
+++ 2_gobases1/Ejercicio4/ejercicio4.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//apellido bien declarado
+	var apellido string = "Gomez"
+	//edad mal declarada. Se le coloca el tipo int, pero se le asigna un string
+	var edad int = 35
+	//boolean bien declarado
+	boolean := "false";
+	//Sueldo mal declarado, se le está colocando el tipo de variable como string, pero se le asigna un float
+	var sueldo float32 = 45857.90
+	//Variable bien declarada
+	var nombre string = "Julián"
+
+}
+
