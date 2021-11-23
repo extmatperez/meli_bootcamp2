aa33ccca422920cab2008a97a190a56e99c59c25
MatiasDBonis <matias.boni@mercadolibre.com>
2021-11-23 20:28:17 -0300

    update ejercicio 1 (TT)

--- 2_gobases1/TT/ejercicio1/ejercicio1.go
+++ 2_gobases1/TT/ejercicio1/ejercicio1.go
@@ -7,7 +7,6 @@ func main() {
 	fmt.Printf("%v tiene %v letras\n", palabra, len(palabra))
 
 	for _, letra := range palabra {
-		//letras = append(letras, string(letra))
 		fmt.Println(string(letra))
 	}
 }
