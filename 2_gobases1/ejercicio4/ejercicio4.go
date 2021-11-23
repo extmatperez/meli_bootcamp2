59a7ead350724275e42c656fe5d165203435e432
benjaminconti <benjamin.conti@mercadolibre.com>
2021-11-23 12:41:58 -0300

    ejercicio4 finished

---
+++ 2_gobases1/ejercicio4/ejercicio4.go
@@ -0,0 +1,25 @@
+/*
+Un estudiante de programación intentó realizar declaraciones de variables con sus respectivos
+tipos en Go pero tuvo varias dudas mientras lo hacía. A partir de esto, nos brindó su código y
+pidió la ayuda de un desarrollador experimentado que pueda:
+Verificar su código y realizar las correcciones necesarias.
+
+	var apellido string = "Gomez"
+  	var edad int = "35"				Incorrecto
+  	boolean := "false";				Incorrecto
+  	var sueldo string = 45857.90	Incorrecto
+  	var nombre string = "Julián"
+
+	Corrección:
+	var apellido string = "Gomez"
+	var edad int = 35
+	boolean = false
+	var sueldo float64 = 45857.90
+	var nombre string = "Julián"
+*/
+
+package main
+
+func main() {
+
+}
