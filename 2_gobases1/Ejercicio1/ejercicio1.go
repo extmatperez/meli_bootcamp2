fa6f81e2d4c57966518454534735ed84e730603a
FacuBouza <facundo.bouza@mercadolibre.com>
2021-11-23 12:21:06 -0300

    Ejercicio 1 finalizado

---
+++ 2_gobases1/Ejercicio1/ejercicio1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nombre, direccion string;
+	nombre = "Facundo Bouza"
+	direccion = "Crisologo Larralde 1166"
+	
+	fmt.Println("Nombre: ", nombre);
+	fmt.Println("Direccion: ", direccion);
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/extmatperez/meli_bootcamp2/tree/bouza_facundo
+
+go 1.17
