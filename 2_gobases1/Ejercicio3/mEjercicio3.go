9cccee9fd45a8e5fdbe8d31f53915e7ac5002b35
FranciscoPalacioMeli <francisco.fpalacio@mercadolibre.com>
2021-11-23 12:36:54 -0300

    Ejercico 3 listo

---
+++ 2_gobases1/Ejercicio3/mEjercicio3.go
@@ -0,0 +1,30 @@
+package main
+import "fmt"
+
+
+
+
+
+func main() {
+//var 1nombre string
+var nombre string;
+var apellido string;
+//var int edad
+var edad int
+//apellido := 6
+apellido = "PEREZ"
+
+// var licencia_de_cconducir = true
+var licenciaDeCconducir = true
+
+//var estatura de la persona int
+var estaturaDeLaPersona float32 = 1.80
+
+cantidadDeHijos := 2
+fmt.Println("El nombre completo de la persona es:", nombre," " ,apellido) ;
+fmt.Println("Edad: ",edad );
+fmt.Println("¿Puede conducir?", licenciaDeCconducir)
+fmt.Println("Estatura: ", estaturaDeLaPersona)
+fmt.Println("¿Cuantos hijos tiene?", cantidadDeHijos)
+  
+}
