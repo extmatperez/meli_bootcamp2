d1dbe9a0b433b891c25bc0b467049f8ddedda1a0
Julian David Mondaca <jmondaca@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-11-23 12:54:54 -0300

    exercise 4 done

---
+++ 2_gobases1/ejercicio4/main.go
@@ -0,0 +1,13 @@
+package main;
+
+func main(){
+	var apellido, nombre string
+	var edad int
+	var sueldo float64;
+	
+	boolean := "false";
+	nombre = "Julián";
+	apellido = "Gomez";
+	edad = 35;
+	sueldo = 45857.90;
+}
