93c80bf3f319d3ae0fe0df6405131b1a1256b6e2
Franco Julian Andrada Arganaraz <fandrada@ar0c02g310pml85.fibertel.com.ar>
2021-11-23 13:20:02 -0300

    feat(ejercicio3_turno_manana): ejercicio3

---
+++ 2_gobases1/ejercicio3_turno_mañana/ejercicio3.go
@@ -0,0 +1,14 @@
+// Ejercicio 3 - Declaración de variables
+
+// Un profesor de programación está corrigiendo los exámenes de sus estudiantes de la materia Programación I para poder brindarles las correspondientes devoluciones. Uno de los puntos del examen consiste en declarar distintas variables. 
+// Necesita ayuda para:
+// 1-Detectar cuáles de estas variables que declaró el alumno son correctas.
+// 2-Corregir las incorrectas.
+
+var nombre string
+var apellido string
+var int edad
+apellido := 6
+var licencia_de_conducir = true
+var estatura de la persona int
+cantidadDeHijos := 2
