df4daba85c5f277f4b19ff9d9d6559b22127d3b5
Santiago Nicolas Panceri <spanceri@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-11-23 12:18:18 -0300

    ejercicio 2

---
+++ 2_gobases1/ejercicio2/ejercicio2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var temp, pressure, humidity float64
+
+	temp = 26.0
+
+	pressure = 1016
+
+	humidity = 30.0
+
+	fmt.Println("Temperatura ", temp, "C")
+
+	fmt.Println("Presión ", pressure, "hPa")
+
+	fmt.Println("Humedad ", humidity, "%")
+
+}
