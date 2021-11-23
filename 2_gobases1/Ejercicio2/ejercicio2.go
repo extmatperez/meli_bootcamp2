61548bca946131a14a79529f4b9c9a12842a38af
Walter Jesus Castillo Bastidas <waltercastillo017@outlook.com>
2021-11-23 11:03:00 -0500

    mejora de impresion

--- 2_gobases1/Ejercicio2/ejercicio2.go
+++ 2_gobases1/Ejercicio2/ejercicio2.go
@@ -10,7 +10,9 @@ func main() {
 		presion     = 1012
 	)
 	//fmt.Println("La Humedad es:", humedad, "%", "%T", humedad)
-	fmt.Printf("La temprera es de: %v ºC y el tipo de dato es %T \n", temperatura, temperatura)
-	fmt.Printf("La humedad relativa es de: %v %% y el tipo de dato es %T \n", humedad, humedad)
-	fmt.Printf("La Presión es de: %vhPa y el tipo de dato es %T \n", presion, presion)
+	fmt.Println("")
+	fmt.Printf("La temprera es de: %v ºC        -- tipo de dato (%T )\n", temperatura, temperatura)
+	fmt.Printf("La humedad relativa es de: %v %% -- tipo de dato (%T) \n", humedad, humedad)
+	fmt.Printf("La Presión es de: %vhPa       -- tipo de dato (%T) \n", presion, presion)
+	fmt.Println("")
 }
