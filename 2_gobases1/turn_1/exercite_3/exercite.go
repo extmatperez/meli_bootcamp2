6b7c9d0661ae13d5a547c97b63bfb666afd19fd1
dignelidxdx <digneli.davila@mercadolibre.com>
2021-11-23 12:43:15 -0300

    Added excersite 3, fixed variable to correct form

---
+++ 2_gobases1/turn_1/exercite_3/exercite.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var 1nombre string
+	//correct form:
+	var nombre string
+	nombre = "Mari"
+	fmt.Println("Nombre:", nombre)
+	var apellido string
+	apellido = "Petit"
+	fmt.Println("Apellido:", apellido)
+	var edad int
+	edad = 20
+	fmt.Println("Edad:", edad)
+	//1apellido := 6
+	//correct form:
+	apellidoUno := 6
+	fmt.Println("Apellido uno:", apellidoUno)
+	//var licencia_de_conducir = true
+	//correct form:
+	var licenciaDeConducir = true
+	fmt.Println("Licencia de conducir:", licenciaDeConducir)
+	//var estatura de la persona int
+	//correct form:
+	var estaturaDeLaPersona int
+	fmt.Println("Estatura de la persona:", estaturaDeLaPersona)
+	cantidadDeHijos := 2
+	fmt.Println("Cantidad de hijos:", cantidadDeHijos)
+
+}
