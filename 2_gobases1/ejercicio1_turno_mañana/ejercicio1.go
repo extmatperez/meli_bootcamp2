e79c0d271c521a06a38353e71a3e545e1f044c07
Franco Julian Andrada Arganaraz <fandrada@ar0c02g310pml85.fibertel.com.ar>
2021-11-23 12:34:11 -0300

    feat(ejercicio1_turno_manana): ejercicio1

---
+++ 2_gobases1/ejercicio1_turno_mañana/ejercicio1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+
+
+func main () {
+	const NOMBRE = "Franco"
+	direccion := "Perú 67"
+	
+	fmt.Println("Nombre: ", NOMBRE)
+	fmt.Println("Dirección: ", direccion)
+}
