b856f206ccdc25c296d0f4e6acbc5c257ed68b1b
BeSaavedram <be.saavedram@gmail.com>
2021-11-23 13:52:13 -0300

    exercise 3

--- 2_gobases1/practica/Ejercicio_3/main.go
+++ 2_gobases1/practica/Ejercicio_3/main.go
@@ -4,17 +4,22 @@ import "fmt"
 
 func main(){
     // Incorrecta. variable 1nombre se inicia con un numero.
-    var 1nombre string
+    // var 1nombre string
+    var nombre1 string
     // Correcta. cumple con la sintaxis correspondiente.
     var apellido string
     // Incorrecta. variable edad, tiene la sintaxis al reves.
-    var int edad
+    // var int edad
+    var edad int
     // Incorrecta. 1apellido Comienza con un numero y además se declara un numero en el.
-    1apellido := 6
+    // 1apellido := 6
+    apellido1 := "Apellido"
     // Incorrecta. De esta manera no se declara e inicializa una variable.
-    var licencia_de_conducir = true
+    // var licencia_de_conducir = true
+    var licencia_de_conducir boolean = true
     // Incorrecta. el nombre de la variable no puede tener espacios en blancos.
-    var estatura de la persona int
+    // var estatura de la persona int
+    var estatura_de_la_persona int
     // Correcta, tiene la sintaxis correspondiente.
     cantidadDeHijos := 2
 }
