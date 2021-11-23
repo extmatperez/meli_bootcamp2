79507efee820b197c562a25434ee05b5fd4e1bc7
MatiasDBonis <matias.boni@mercadolibre.com>
2021-11-23 12:33:57 -0300

    Ejercicio 3

--- 2_gobases1/practica1.go
+++ 2_gobases1/practica1.go
@@ -18,6 +18,17 @@ func main() {
 	var presion int
 	presion = 1024
 
-	fmt.Printf("Temperatura: %v, Humedad %v%%, presión: %v", temperatura, humedad, presion)
+	fmt.Printf("Temperatura: %v, Humedad %v%%, presión: %v", temperatura,humedad, presion)
+
+	/* Ejercicio 3 */
+	var nombre string /* Las variables no pueden comenzar con un numero */
+	var apellido string
+	var edad int /* Primero se declara el nombre de la variable y luego el tipo */
+	apellido = "Apellido" /* Las variables no pueden comenzar con un numero, la variable ya esta declarda y los : no son necesarios, la variable esta declarada como string */
+	var licencia_de_conducir bool = true
+	var estatura_de_la_persona int /* Las variables no pueden contener espacios */
+	cantidadDeHijos := 2
+
+	fmt.Println(nombre, apellido, edad, licencia_de_conducir, estatura_de_la_persona, cantidadDeHijos)
   
 }
