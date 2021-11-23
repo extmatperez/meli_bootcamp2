93a74d190404a489c893df764fcd42dab9e2d8d6
Walter Jesus Castillo Bastidas <waltercastillo017@outlook.com>
2021-11-23 10:59:31 -0500

    % arreglado

--- 2_gobases1/Ejercicio2/ejercicio2.go
+++ 2_gobases1/Ejercicio2/ejercicio2.go
@@ -5,11 +5,12 @@ import "fmt"
 func main() {
 
 	var (
-		temperatura = 30
+		temperatura = 30.0
 		humedad     = 65
 		presion     = 1012
 	)
+	//fmt.Println("La Humedad es:", humedad, "%", "%T", humedad)
 	fmt.Printf("La temprera es de: %v ºC y el tipo de dato es %T \n", temperatura, temperatura)
-	fmt.Printf("La humedad relativa es de: %v y el tipo de dato es %T \n", humedad, humedad)
+	fmt.Printf("La humedad relativa es de: %v %% y el tipo de dato es %T \n", humedad, humedad)
 	fmt.Printf("La Presión es de: %vhPa y el tipo de dato es %T \n", presion, presion)
 }
