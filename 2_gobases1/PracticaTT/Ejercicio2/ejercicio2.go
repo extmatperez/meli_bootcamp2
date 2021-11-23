abcc911c3d4e37d9105e49101652901047de6a53
FacuBouza <facundo.bouza@mercadolibre.com>
2021-11-23 17:45:20 -0300

    Exercise 2 finished

---
+++ 2_gobases1/PracticaTT/Ejercicio2/ejercicio2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	precio := 20.5
+	descuento := 0.15
+
+	precioConDto := precio + precio*descuento
+
+	fmt.Printf("El precio original de la prenda es $%v\n", precio)
+	fmt.Printf("El precio con un descuento del %v %% es de $%v\n", descuento*100, precioConDto)
+}
