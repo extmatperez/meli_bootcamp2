f7aeda685cb208acc1dd17e4fd7136ca17ff5646
FranciscoPalacioMeli <francisco.fpalacio@mercadolibre.com>
2021-11-23 17:17:20 -0300

    New Folder for Turns

--- 2_gobases1/TurnoManana/Ejercicio2/mEjercicio2.go
+++ 2_gobases1/TurnoManana/Ejercicio2/mEjercicio2.go
@@ -8,6 +8,6 @@ var presion float32=100;
 var txtTemperatura string = "La temperatura actual es: ";
 var simboloGrado string = "°";
 fmt.Println( txtTemperatura,temperatura,simboloGrado);
-fmt.Println("La presion atomesferica es: ",presionAtmosferica, "Pa" );
+fmt.Println("La presion atomesferica es: ",presionAtmosferica, "%" );
 fmt.Println("La presion actual:",presion,"Pa");
 }
