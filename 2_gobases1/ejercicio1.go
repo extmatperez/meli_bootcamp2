525c6119f35fcc5222686bbf55c5b3fea8dc3910
fjgianni <fernando.gianni@mercadolibre.com>
2021-11-23 12:55:16 -0300

    nuevo commit

---
+++ 2_gobases1/ejercicio1.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nombre, direccion := "Fernando", "Luis Maria Campos 4815"
+
+	fmt.Printf("Nombre: %v \nDirección: %v", nombre, direccion)
+}
---
+++ 2_gobases1/ejercicio3.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var 1nombre string //Comienza con un número
+	var apellido string 
+	var int edad //se coloca primero la variable y luego el tipo
+	1apellido := 6 //comienza con un número
+	var licencia_de_conducir = true // Le asigna un valor sin indicar el tipo de variable
+	var estatura de la persona int //utiliza espacios
+	cantidadDeHijos := 2
+  
+}
