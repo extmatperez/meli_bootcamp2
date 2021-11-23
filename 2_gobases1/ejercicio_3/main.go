9555b9a864ce364cc5becbb28488deb81deb35f8
diegoParrad <diego.parra@mercadolibre.com.co>
2021-11-23 11:02:20 -0500

    initial commit

---
+++ 2_gobases1/ejercicio_3/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	//  var 1nombre string
+	var nombre string
+
+	//  var apellido string
+	var apellido string
+
+	//   var int edad
+	var edad int
+
+	// 1apellido := 6
+	apellido = "6"
+
+	//  var licencia_de_conducir = true
+	var licencia_de_conducir = true
+
+	//  var estatura de la persona int
+	var estatura_de_la_persona float32
+
+	//cantidadDeHijos := 2
+	cantidadDeHijos := 2
+
+	fmt.Println(nombre, apellido, edad, licencia_de_conducir, estatura_de_la_persona, cantidadDeHijos)
+
+}
