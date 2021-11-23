336f312c9c0182567c8f7b1de502667d585a4cdb
igvargas <ignacio.vargas@mercadolibre.com>
2021-11-23 12:39:22 -0300

    ex2 finished

---
+++ 2_gobases1/ejercicio_2/ej2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var temperatura float64 = 32.4
+	var humedad int = 66
+	var presion float64 = 6.2
+
+	fmt.Printf("En la ciudad, la temperatura es de: %v", temperatura)
+	fmt.Printf("\nLa humedad: %v", humedad)
+	fmt.Printf("\nLa presion: %v", presion)
+}
