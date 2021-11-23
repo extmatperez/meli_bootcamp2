629218d88674035b94cf406aa1fe6d3e57c4fb15
jriosMELI <jose.rios@mercadolibre.cl>
2021-11-23 12:59:41 -0300

    exercise 2 change in variable rate

--- 2_gobases1/exercise2/exercise2.go
+++ 2_gobases1/exercise2/exercise2.go
@@ -3,13 +3,23 @@ package main
 
 import "fmt"
 
-var temperature float64
-var humidity float64
+var temperature int
+var humidity int
 var press float64
 
 func main() {
-	temperature = 28
-	humidity = 26.5
-	press = 1013
-	fmt.Printf("En Santiago de Chile se tiene que: \nTemperatura: %f ºC \nPresion %f hPa \nHumedad: %f", temperature, press, humidity)
+	temperature = 26
+	humidity = 39
+	press = 1012.9
+	fmt.Printf("En Santiago de Chile se tiene que: \nTemperatura: %d ºC \nPresion %f hPa \nHumedad: %d", temperature, press, humidity)
 }
+
+/* Segun la app de tiempo weather tanto la temperatura como la humedad la muestra como valores enteros, y son valores pequeños, los cuales
+no deberian sobrepasar los 3 digitos, por lo que un int8 esta mas que suficiente:
+
+int8        signed  8-bit integers (-128 to 127)
+int16       signed 16-bit integers (-32768 to 32767)
+int32       signed 32-bit integers (-2147483648 to 2147483647)
+int64       signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
+
+*/
