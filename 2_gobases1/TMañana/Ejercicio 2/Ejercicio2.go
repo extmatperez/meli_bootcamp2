638da605faed01b915e072b012c8fea85d2f3944
jpacostasc <juan.acosta@mercadolibre.com>
2021-11-23 12:07:48 -0300

    Ejercicio 2 completado

---
+++ 2_gobases1/TMañana/Ejercicio 2/Ejercicio2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	var temperatura float64
+	var humedad int
+	var presion int
+
+	temperatura = 31.2
+	humedad = 51
+	presion = 1022
+
+	fmt.Println("La temperatura es", temperatura, "°C con una humedad del ", humedad,"%", "y la presion es", presion, "hpa")
+
+}
