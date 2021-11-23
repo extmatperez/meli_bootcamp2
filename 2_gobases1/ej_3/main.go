bb03694c2f3706a9e323ce82248b45d63bf1aa80
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2021-11-23 10:21:49 -0500

    Bases 1 Ejercicio 3

---
+++ 2_gobases1/ej_3/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main(){
+// var 1nombre string  // MAL no puede empezar en número
+var nombre1 string
+
+var apellido string // BIEN
+
+//var int edad        // MAL orden int
+var edad int
+
+// 1apellido := 6      // MAL no puede empezar en número
+apellido1 := 6
+
+// var licencia_de_conducir = true // MAL no indica el tipo, ni es la forma corta
+var licencia_de_conducir bool = true
+
+// var estatura de la persona int  // MAL no puede tener espacios
+var estatura_de_la_persona int
+
+cantidadDeHijos := 2 // BIEN
+}
