ad2c2fa3ce571583da738dca626503f85f031b60
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2021-11-23 10:01:11 -0500

    Bases 1 Ejercicio 1

---
+++ 2_gobases1/ej_1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var name, address string;
+
+func main() {
+	name = "Juan"
+	address = "Calle falsa 123"
+	fmt.Println("Nombre: "+name)
+	fmt.Println("Dirección: "+address)
+}
