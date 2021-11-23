dee39a86a75fb04c2c968deaaa2fe2bb49c4cff0
Matias Ziliotto <matias.ziliotto@mercadolibre.com>
2021-11-23 12:26:16 -0300

    Ejercicio4

---
+++ 2_gobases1/Ejercicio4/data_types.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	/*
+		var apellido string = "Gomez" // OK
+		var edad int = "35"           // BAD
+		boolean := "false"            // BAD
+		var sueldo string = 45857.90  // BAD
+		var nombre string = "Julián"  // OK
+	*/
+
+	/*
+		// FIXES
+		var edad int = 35
+		boolean := false
+		var sueldo float64 = 45857.90
+	*/
+}
