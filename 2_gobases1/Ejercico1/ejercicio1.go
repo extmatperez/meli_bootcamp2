2ebf9e9064992f3286bda46dbfc7387fc0712800
Walter Jesus Castillo Bastidas <waltercastillo017@outlook.com>
2021-11-23 10:03:44 -0500

    Ejercicio 1

---
+++ 2_gobases1/Ejercico1/ejercicio1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		nombre    = "Walter Jesus Castillo Bastidas"
+		direccion = "calle 18bis #34a - 74"
+	)
+	fmt.Println("Mi nombre es: ", nombre, " y  mi direccion es: ", direccion)
+
+}
