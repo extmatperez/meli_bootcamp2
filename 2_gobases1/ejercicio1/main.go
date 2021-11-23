29654b4d7021ce4bfc5391bde5bb37853b8144e7
Julian David Mondaca <jmondaca@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-11-23 12:18:05 -0300

    2_gobases1 exercise 1

---
+++ 2_gobases1/ejercicio1/main.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt";
+
+func main(){	
+	var nombre, direccion string;
+
+	nombre = "julian";
+	direccion = "canada 6100";
+
+	fmt.Println("mi nombre: " + nombre);
+	fmt.Println("mi direccion: " + direccion);
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/extmatperez/meli_bootcamp2/go-simple-module
+
+go 1.17
