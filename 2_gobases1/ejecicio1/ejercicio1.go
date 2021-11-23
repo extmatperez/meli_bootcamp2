11be903b751a442036e18fab1fc27c35d873575f
Santiago Nicolas Panceri <spanceri@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-11-23 12:02:40 -0300

    ejercicio 1

---
+++ 2_gobases1/ejecicio1/ejercicio1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nombre string = "Panceri Santiago"
+
+	var direccion string = "Perez Colmán 236"
+
+	fmt.Println(nombre)
+
+	fmt.Println(direccion)
+}
