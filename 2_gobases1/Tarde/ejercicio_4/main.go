4d630dbbca4d551e0bd36480b23bd0e52b80f0a7
diegoParrad <diego.parra@mercadolibre.com.co>
2021-11-23 18:35:04 -0500

    load exercise 4 -tarde-

---
+++ 2_gobases1/Tarde/ejercicio_4/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	mes := 9
+	switch mes {
+	case 1:
+		fmt.Println("Enero")
+	case 2:
+		fmt.Println("Febrero")
+	case 3:
+		fmt.Println("Marzo")
+	case 4:
+		fmt.Println("Abril")
+	case 5:
+		fmt.Println("Mayo")
+	case 6:
+		fmt.Println("Junio")
+	case 7:
+		fmt.Println("Julio")
+	case 8:
+		fmt.Println("Agosto")
+	case 9:
+		fmt.Println("Septiembre")
+	case 10:
+		fmt.Println("Octubre")
+	case 11:
+		fmt.Println("Noviembre")
+	case 12:
+		fmt.Println("Diciembre")
+	default:
+		fmt.Println("Desconocido")
+	}
+
+	/*
+		Existen mas maneras de solucionar este problema, una de ellas seria con varios if anidados
+		sin embargo, seria una secuencia de If muy larga y se hace dificil su lectura
+	*/
+}
