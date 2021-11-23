0a36602660a3a4022a271e739c8c238147376758
jmiguelsoto <jose.ssoto@mercadolibre.com>
2021-11-23 13:04:06 -0300

    move to clase1 folder

---
+++ 2_gobases1/clase1/ej4.go
@@ -0,0 +1,9 @@
+package main
+
+func fourthExcercise() {
+	// var apellido string = "Gomez" //bien
+	// var edad int = 35             // le sobraban ""
+	// boolean := false              // le sobraban ""
+	// var sueldo float32 = 45857.90 // mal el tipo, o le faltan ""
+	// var nombre string = "Julián"  // bien
+}
