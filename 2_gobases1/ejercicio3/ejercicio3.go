f075e8ab44c252ff6ffd8b59928c6d8e5e03705d
benjaminconti <benjamin.conti@mercadolibre.com>
2021-11-23 12:35:55 -0300

    ejercicio3 finished

---
+++ 2_gobases1/ejercicio3/ejercicio3.go
@@ -0,0 +1,33 @@
+/*
+Un profesor de programación está corrigiendo los exámenes de sus estudiantes de la materia Programación I
+para poder brindarles las correspondientes devoluciones. Uno de los puntos del examen consiste en declarar
+distintas variables.
+
+Necesita ayuda para:
+Detectar cuáles de estas variables que declaró el alumno son correctas.
+Corregir las incorrectas.
+
+	var 1nombre string					INCORRECTA: no puede comenzar con número
+  	var apellido string
+  	var int edad						INCORRECTA: primero va el nombre de la variable y después el tipo
+  	1apellido := 6						INCORRECTA: no puede comenzar con número y le asigna a un 
+  	var licencia_de_conducir = true		INCORRECTA: al declarar un Boolean no lleva "var"
+  	var estatura de la persona int		INCORRECTA: los nombres de las variables no pueden contener espacios
+  	cantidadDeHijos := 2
+
+
+	Corrección:
+	var nombre string
+	var apellido string
+	var edad int
+	apellido := 6
+	licencia_de_conducir = true
+	var estatura_de_la_persona int
+	cantidadDeHijos := 2
+*/
+
+package main
+
+func main() {
+
+}
