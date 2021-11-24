724a92959cac3e776f14f76f3c8d0c40594df31b
AG-Meli <andres.ghione@mercadolibre.com>
2021-11-24 13:53:25 -0300

    ejercicio4

--- 3_gobases2/clase_vespertina/ejercicio4/ejercicio4.go
+++ 3_gobases2/clase_vespertina/ejercicio4/ejercicio4.go
@@ -1,45 +1,65 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	notas := leerNotas()
+	funcion := obtenerFuncion("maximo")
+	fmt.Printf("El resultado es: %v \n", funcion(notas))
 }
 
-func obtenerFuncion(operacion string) func() {
-	var minimo = func(notas []float64) float64 {
-		aux := 0.0
-		for _, val := range notas {
-			if val < aux || aux == 0 {
-				aux = val
-			}
+func leerNotas() []float64 {
+	var notas []float64
+	fmt.Println("Para salir escriba -1 \n")
+	for {
+		nota := capturarNotas()
+		if nota != -1 {
+			notas = append(notas, nota)
+		} else {
+			break
 		}
-		return aux
+		fmt.Printf("\n")
 	}
+	return notas
+}
 
-	var maximo = func(notas []float64) float64 {
-		aux := 0.0
-		for _, val := range notas {
-			if val > aux || aux == 0 {
-				aux = val
-			}
-		}
-		return aux
-	}
+func capturarNotas() float64 {
+	var nota float64
+	fmt.Println("Ingrese la nota:")
+	fmt.Scanf("%f", &nota)
+	return nota
+}
 
-	var promedio = func(notas []float64) float64 {
-		total := 0.0
-		for _, val := range notas {
-			total += val
-		}
-		return total / float64(len(notas))
-	}
-
+func obtenerFuncion(operacion string) func(notas []float64) float64 {
 	switch operacion {
 	case "minimo":
-		return minimo
+		return func(notas []float64) float64 {
+			aux := 0.0
+			for _, val := range notas {
+				if val < aux || aux == 0 {
+					aux = val
+				}
+			}
+			return aux
+		}
 	case "maximo":
-		return maximo
+		return func(notas []float64) float64 {
+			aux := 0.0
+			for _, val := range notas {
+				if val > aux || aux == 0 {
+					aux = val
+				}
+			}
+			return aux
+		}
 	case "promedio":
-		return promedio
+		return func(notas []float64) float64 {
+			total := 0.0
+			for _, val := range notas {
+				total += val
+			}
+			return total / float64(len(notas))
+		}
 	default:
 		return nil
 	}
