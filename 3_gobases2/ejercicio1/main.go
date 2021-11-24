8d830c92b53749d6fd388d010018276be25e344f
MatiasDBonis <matias.boni@mercadolibre.com>
2021-11-24 12:56:22 -0300

    update ejercicio1

--- 3_gobases2/ejercicio1/main.go
+++ 3_gobases2/ejercicio1/main.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	salario := 80000.0
+	salario := 0.0
+	fmt.Println("Introduzca su salario")
+	fmt.Scanf("%f", &salario)
 	fmt.Printf("Debera pagar $%.2f de impuestos para un salario de %.2f\n", cobrarImpuestos(salario), salario)
 }
 
 func cobrarImpuestos(salario float64) float64 {
-	if salario > 17000 {
-		if salario > 150000 {
-			return (salario * 0.17 * 0.10)
+	if salario >= 17000 {
+		if salario >= 150000 {
+			return (salario * 0.27)
 		}
 		return (salario * 0.17)
 	}
---
+++ 3_gobases2/ejercicio2/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+
+}
+
+func cobrarImpuestos(salario float64) float64 {
+
+}
