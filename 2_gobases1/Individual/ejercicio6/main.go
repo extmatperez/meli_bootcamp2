3396a7e58d79c6ea22ac833e2516888495a8bcba
fjgianni <fernando.gianni@mercadolibre.com>
2021-11-24 09:45:43 -0300

    Excercises

---
+++ 2_gobases1/Individual/ejercicio6/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var employees = map[string]int{"Benjamin": 20, "Nahuel": 26, "Brenda": 19, "Darío": 44, "Pedro": 30}
+
+	fmt.Printf("%T", employees)
+
+}
