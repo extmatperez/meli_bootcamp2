79a55daa333bb94fcc3d0e1317498c2a13b84448
AG-Meli <andres.ghione@mercadolibre.com>
2021-11-24 15:00:09 -0300

    correccion ejercicio4

--- 3_gobases2/clase_vespertina/ejercicio4/ejercicio4.go
+++ 3_gobases2/clase_vespertina/ejercicio4/ejercicio4.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	notas := leerNotas()
 	funcion := obtenerFuncion("maximo")
-	fmt.Printf("El resultado es: %v \n", funcion(notas))
+	val, err := funcion(notas)
+	if err == nil {
+		fmt.Printf("El resultado es: %v \n", val)
+	} else {
+		fmt.Println("Hubo un error")
+	}
 }
 
 func leerNotas() []float64 {
@@ -30,35 +38,46 @@ func capturarNotas() float64 {
 	return nota
 }
 
-func obtenerFuncion(operacion string) func(notas []float64) float64 {
+func obtenerFuncion(operacion string) func(notas []float64) (float64, error) {
 	switch operacion {
 	case "minimo":
-		return func(notas []float64) float64 {
+		return func(notas []float64) (float64, error) {
+			if len(notas) == 0 {
+				return 0, errors.New("No hay notas existentes")
+			}
 			aux := 0.0
 			for _, val := range notas {
 				if val < aux || aux == 0 {
 					aux = val
 				}
 			}
-			return aux
+			return aux, nil
 		}
+
 	case "maximo":
-		return func(notas []float64) float64 {
+		return func(notas []float64) (float64, error) {
+			if len(notas) == 0 {
+				return 0, errors.New("No hay notas existentes")
+			}
 			aux := 0.0
 			for _, val := range notas {
 				if val > aux || aux == 0 {
 					aux = val
 				}
 			}
-			return aux
+			return aux, nil
 		}
+
 	case "promedio":
-		return func(notas []float64) float64 {
+		return func(notas []float64) (float64, error) {
+			if len(notas) == 0 {
+				return 0, errors.New("No hay notas existentes")
+			}
 			total := 0.0
 			for _, val := range notas {
 				total += val
 			}
-			return total / float64(len(notas))
+			return (total / float64(len(notas))), nil
 		}
 	default:
 		return nil
