305d1e1eccf2dbc1173286b6ca80a976426c80af
ivanArevalo1997 <ivan.aarevalo@mercadolibre.com>
2021-11-24 10:29:35 -0300

    exercise 2 scanf

--- 2_gobases1/tarde/ejercicio2/ejercicio2.go
+++ 2_gobases1/tarde/ejercicio2/ejercicio2.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
-func descuento(precio float64, porcentaje float64) {
+func descuento() {
+	var precio float64
+	var porcentaje float64
+
+	fmt.Printf("Ingrese el precio del producto: ")
+	fmt.Scanf("%f", &precio)
+
+	fmt.Printf("Ingrese el descuento: ")
+	fmt.Scanf("%f", &porcentaje)
+
 	var descuento float64 = precio * porcentaje / 100
 	var precioFinal float64 = precio - descuento
+
 	fmt.Printf("El precio con los descuentos es de %v\n", precioFinal)
 }
 
 func main() {
-	descuento(700, 10)
+	descuento()
 }
