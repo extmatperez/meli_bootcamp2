b4f4c81e265ee26edce54bed774abe9a10fa6dc5
emontenegro <edgar.montenegro@mercadolibre.com>
2021-11-24 01:18:29 -0300

    👔  Fix error answers

--- 2_gobases1/vartiables/main.go
+++ 2_gobases1/vartiables/main.go
@@ -2,13 +2,13 @@
   var apellido string
   var int edad   ------------ var edad int
   1apellido := 6 ------------ apellido1 := (no debe iniciar con número y debería ser un string)
-  var licencia_de_conducir = true
+  var licencia_de_conducir = true ---------- var licencia_de_conducir bool = true
   var estatura de la persona int ------------- var estatura_de_la_persona int
   cantidadDeHijos := 2 */
 
   /* var apellido string = "Gomez"
   var edad int = "35" --------- var edad int = 35 (sin "")
-  boolean := "false"; --------- boolean := false (sin "") o boolean := "false"
+  boolean := "false"; --------- Es correcto.... también podría ser boolean := false (sin "")
   var sueldo string = 45857.90  var sueldo float64 = 45857.90
   var nombre string = "Julián" */
 
@@ -26,7 +26,6 @@
 	cantidadDeHijos := 2
 	/* ---------------------- */
 	var edad2 int = 35
-	boolean1 := false
 	boolean := "false"
 	var sueldo float64 = 45857.90
 	var nombre string = "Julián"
@@ -39,9 +38,7 @@
 	fmt.Println("Estatura de la persona : ", estatura_de_la_persona)
 	fmt.Println("Cantidad de hijos: ", cantidadDeHijos)
 	fmt.Println("Edad: ", edad2)
-	fmt.Println("Boolean1: ", boolean1)
 	fmt.Println("Boolean: ", boolean)
 	fmt.Println("Sueldo: ", sueldo)
 	fmt.Println("Nombre: ", nombre)
   }
-
