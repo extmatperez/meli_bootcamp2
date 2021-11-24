c4847742a0009863b70b2c55a9b92854163b36e6
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2021-11-24 10:53:25 -0500

    Bases 2 Ejercicio 3 morning

---
+++ 3_gobases2/ejm_3/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(calcularSalario(1000, "A"))
+	fmt.Println(calcularSalario(1000, "B"))
+	fmt.Println(calcularSalario(1000, "C"))
+}
+
+func calcularSalario(minutos float64, category string) (float64) {
+	horas := minutos / 60;
+	switch category {
+	case "A":
+		base := 3000*horas
+		return  base+base*0.5 
+	case "B":
+		base := 1500*horas
+		return  base+base*0.2 
+	case "C":
+		base := 1000*horas
+		return  base
+	default:
+		return 0; 
+	}
+}
