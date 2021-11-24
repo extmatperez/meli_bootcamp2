02e529fe9db4f83765f3016a8d6c84a8f01908e4
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2021-11-24 10:27:32 -0500

    Bases 2 Ejercicio 2 Morning

---
+++ 3_gobases2/ejm_2/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	promedio, err := calcularPromedioNotas(10, 20, -30, 40, 50)
+	if (err != nil) {
+		fmt.Println(err)
+	} else {
+		fmt.Println(promedio)
+	}
+}
+
+func calcularPromedioNotas(notas ...float64) (float64, error) {
+	if (len(notas) == 0) {
+		return 0, errors.New("no hay notas")
+	}
+	var suma float64 = 0
+	for _, nota := range notas {
+		if (nota < 0) {
+			return 0, errors.New("nota no válida")
+		}
+		suma += nota
+	}
+	return suma / float64(len(notas)), nil
+}
