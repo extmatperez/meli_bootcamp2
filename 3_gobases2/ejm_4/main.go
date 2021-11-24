9ccf4843c50ca174fbc05b87b351ac2e5c3d613a
facundocenteno <facundo.centeno@mercadolibre.com>
2021-11-24 13:40:46 -0300

    ejs-dia2-mañana

---
+++ 3_gobases2/ejm_4/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	minimo   = "minimo"
+	promedio = "promedio"
+	maximo   = "maximo"
+)
+
+func main() {
+	minFunc, _ := operacion(minimo)
+	promFunc, _ := operacion(promedio)
+	maxFunc, _ := operacion(maximo)
+
+	valorMinimo := minFunc(2, 3, 3, 4, 1, 2, 4, 5)
+	valorPromedio := promFunc(2, 3, 3, 4, 1, 2, 4, 5)
+	valorMaximo := maxFunc(2, 3, 3, 4, 1, 2, 4, 5)
+
+	fmt.Println("Valor minimo:", valorMinimo)
+	fmt.Println("Valor promedio:", valorPromedio)
+	fmt.Println("Valor maximo:", valorMaximo)
+}
+
+func operacion(oper string) (func(nums ...float64) float64, error) {
+	switch oper {
+	case minimo:
+		return min, nil
+	case promedio:
+		return prom, nil
+	case maximo:
+		return max, nil
+	default:
+		return nil, errors.New("operacion no valida")
+	}
+}
+
+func min(nums ...float64) float64 {
+	min := nums[0]
+	for _, num := range nums {
+		if num < min {
+			min = num
+		}
+	}
+	return min
+}
+
+func max(nums ...float64) float64 {
+	max := nums[0]
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
+
+func prom(nums ...float64) float64 {
+	var sum float64
+	for _, num := range nums {
+		sum += num
+	}
+	return sum / float64(len(nums))
+}
---
+++ 3_gobases2/ejm_5/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	perro     = "perro"
+	gato      = "gato"
+	hamster   = "hamster"
+	tarantula = "tarantula"
+)
+
+var cantidades map[string]int
+
+func main() {
+	// todos los pesos en gramos
+	cantidades = map[string]int{
+		perro:     10000,
+		gato:      5000,
+		hamster:   250,
+		tarantula: 150,
+	}
+
+	animalPerro, _ := Animal(perro)
+	animalGato, _ := Animal(gato)
+	animalInvalid, err := Animal("invalid")
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		animalInvalid(0)
+
+		fmt.Println(animalPerro(5))
+		fmt.Println(animalGato(2))
+	}
+}
+
+func Animal(animal string) (func(cantidad int) int, error) {
+	return calcularCantidad((animal))
+}
+
+func calcularCantidad(animal string) (func(cantidad int) int, error) {
+	if cantidades[animal] == 0 {
+		return nil, errors.New("animal inválido")
+	}
+	return func(cantidad int) int {
+		return cantidades[animal] * cantidad
+	}, nil
+}
