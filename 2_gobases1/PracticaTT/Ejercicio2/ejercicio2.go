b53b9fdafe3eeb5004a67b75a8943783169309d8
FacuBouza <facundo.bouza@mercadolibre.com>
2021-11-24 10:29:44 -0300

    Excercise 2 modified

--- 2_gobases1/PracticaTT/Ejercicio2/ejercicio2.go
+++ 2_gobases1/PracticaTT/Ejercicio2/ejercicio2.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	precio := 20.5
-	descuento := 0.15
+	var precio, descuento float32
+	fmt.Printf("Ingrese el precio:\n")
+	fmt.Scanf("%f", &precio)
+	fmt.Printf("Ingrese el descuento:\n")
+	fmt.Scanf("%f", &descuento)
 
-	precioConDto := precio - precio*descuento
+	precioConDto := precio - precio*descuento/100
 
 	fmt.Printf("El precio original de la prenda es $%v\n", precio)
-	fmt.Printf("El precio con un descuento del %v %% es de $%v\n", descuento*100, precioConDto)
+	fmt.Printf("El precio con un descuento del %v%% es de $%v\n", descuento, precioConDto)
 }
