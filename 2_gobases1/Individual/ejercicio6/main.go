7f3f6fcf625f0cea77b396592ca60baaab7082bd
fjgianni <fernando.gianni@mercadolibre.com>
2021-11-24 10:31:06 -0300

    excercise 6

--- 2_gobases1/Individual/ejercicio6/main.go
+++ 2_gobases1/Individual/ejercicio6/main.go
@@ -6,7 +6,17 @@ import (
 
 func main() {
 	var employees = map[string]int{"Benjamin": 20, "Nahuel": 26, "Brenda": 19, "Darío": 44, "Pedro": 30}
+	var count int
 
-	fmt.Printf("%T", employees)
+	fmt.Println("La edad de Benjamín es : ", employees["Benjamin"])
+
+	for _, greater := range employees {
+		if greater > 21 {
+			count++
+		}
+
+	}
+
+	fmt.Println(count, " empleados son mayores de 21 años")
 
 }
