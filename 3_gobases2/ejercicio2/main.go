b03a86722cc816b57f23c5d939da26840c74e506
MatiasDBonis <matias.boni@mercadolibre.com>
2021-11-24 13:14:36 -0300

    Add ejercicio2 (TM) gobases2

--- 3_gobases2/ejercicio2/main.go
+++ 3_gobases2/ejercicio2/main.go
@@ -1,9 +1,26 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+func main() {
+	promedio, err := promedio(1, 2, -3, 4, 5, 6, 7, 8, 9, 10)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("El promedio de este alumno es: %v\n", promedio)
+	}
 }
 
-func cobrarImpuestos(salario float64) float64 {
-
+func promedio(notas ...int) (int, error) {
+	suma := 0
+	for _, nota := range notas {
+		suma += nota
+		if nota < 0 {
+			return 0, errors.New("no puede haber calificaciones negativas")
+		}
+	}
+	return (suma / len(notas)), nil
 }
