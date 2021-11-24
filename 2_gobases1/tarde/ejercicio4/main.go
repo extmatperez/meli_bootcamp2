76604bd2d81040735ea6a84f8972d01cc0fa6333
bribeltran <brian.beltranmercadolibre.com.co>
2021-11-23 19:16:23 -0500

    se agrega ejercicio 4

---
+++ 2_gobases1/tarde/ejercicio4/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var meses = map[int]string{1: "enero", 2: "febrero", 3: "marzo", 4: "abril", 5: "mayo", 6: "junio", 7: "julio", 8: "agosto", 9: "septiembre", 10: "octubre", 11: "noviembre", 12: "diciembre"}
+
+	fmt.Println(meses[1])
+}
