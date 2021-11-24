dfef33476ec8d8b816f0f037a85d0fdbeef9c0a9
igvargas <ignacio.vargas@mercadolibre.com>
2021-11-23 22:19:29 -0300

    Ex1 finished

---
+++ 2_gobases1/C1_Go_TT/Ejercicio1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+La Real Academia Española quiere saber cuántas letras tiene una palabra
+y luego tener cada una de las letras por separado para deletrearla.
+	1 - Crear una aplicación que tenga una variable con la palabra e imprimir
+ la cantidad de letras que tiene la misma.
+	2 - Luego imprimí cada una de las letras.
+*/
+
+func main() {
+	palabra := "palabra"
+
+	cadena := strings.Split(palabra, "")
+
+	for i, letra := range cadena {
+		fmt.Println(i, letra)
+	}
+}
