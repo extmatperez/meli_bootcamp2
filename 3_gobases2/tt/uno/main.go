58d8a040c3eb77a213ea085cf5fc55548ca8b4e4
rottavimeli <rocio.ottavianelli@mercadolibre.com>
2021-11-24 18:02:08 -0300

    ejercicio 1 terminado

--- 3_gobases2/tt/uno/main.go
+++ 3_gobases2/tt/uno/main.go
@@ -15,6 +15,8 @@ type Student struct {
 	BDay  Date
 }
 
+var students []Student
+
 func (s Student) details() {
 	fmt.Println("Name: ", s.Name)
 	fmt.Println("Lastname: ", s.LName)
@@ -22,9 +24,19 @@ func (s Student) details() {
 	fmt.Println("Birth Day: ", s.BDay)
 }
 
-func main() {
-	students := []Student{}
+func printStudents(students []Student) {
 	for _, student := range students {
 		student.details()
+		fmt.Println("____________________")
 	}
+}
+
+func main() {
+
+	students = append(students, Student{"Mario", "Santos", 23389375, Date{18, 1, 1964}})
+	students = append(students, Student{"Pablo", "Lamponne", 25639332, Date{28, 10, 1965}})
+	students = append(students, Student{"Emilio", "Ravenna", 22287190, Date{3, 12, 1963}})
+	students = append(students, Student{"Gabriel", "Medina", 24412610, Date{5, 7, 1964}})
+
+	printStudents(students)
 }
