4293d952fcf7ccadbe9f0103829b17abe8379a56
FacuBouza <facundo.bouza@mercadolibre.com>
2021-11-24 09:43:42 -0300

    excercise 4 finished

---
+++ 2_gobases1/PracticaTT/Ejercicio4/ejercicio4.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Realizar una aplicación que contenga una variable con el número del mes.
+// Según el número, imprimir el mes que corresponda en texto.
+// ¿Se te ocurre si se puede resolver de más de una manera? ¿Cuál elegirías y por qué?
+// Ej: 7, Julio
+
+func main() {
+	/*Se me ocurren dos maneras de realizarlo. La primera es utilizando 12 if para ver cual es el mes, y la otra es utilizando
+	  un mapa que asocie cada número del mes con su nombre */
+	mapMonth := map[int]string{1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril",
+		5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto",
+		9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"}
+
+	monthNum := 6
+	monthName := mapMonth[monthNum]
+
+	if monthNum >= 1 && monthNum <= 12 {
+		fmt.Printf("El mes correspondiente al número %v es \"%v\"\n", monthNum, monthName)
+	} else {
+		fmt.Printf("El mes %v no existe.\n", monthNum)
+	}
+
+}
