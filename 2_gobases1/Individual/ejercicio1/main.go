354260b2ac40bff936d25bd55efd0387ac80a037
fjgianni <fernando.gianni@mercadolibre.com>
2021-11-24 10:05:19 -0300

    correccion

--- 2_gobases1/Individual/ejercicio1/main.go
+++ 2_gobases1/Individual/ejercicio1/main.go
@@ -3,3 +3,23 @@ para deletrearla.
 Crear una aplicación que tenga una variable con la palabra e imprimir la cantidad de letras que tiene la misma.
 Luego imprimí cada una de las letras.
 */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var word string = "golang"
+	var long int
+	long = len(word)
+
+	fmt.Printf("La palabra %s tiene %d letras\n", word, long)
+
+	for _, letter := range word {
+		fmt.Printf("%c  ", letter)
+	}
+	fmt.Printf("\n")
+
+}
