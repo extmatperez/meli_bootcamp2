da0cdcceb8c3cdb7da58ac044de9762fd8dc4e1c
MatiasDBonis <matias.boni@mercadolibre.com>
2021-11-25 14:03:49 -0300

    Update ejercicio2 (TM) gobases3 1.1

--- 4_gobases3/TM/ejercicio2/main.go
+++ 4_gobases3/TM/ejercicio2/main.go
@@ -11,22 +11,23 @@ import (
 func main() {
 	data, _ := os.ReadFile("./ejercicio1/productos.csv")
 	r := csv.NewReader(strings.NewReader(string(data)))
-	rows, _ := r.ReadAll()
-	fmt.Println(rows)
-	formatter(rows)
+	filas, _ := r.ReadAll()
+	fmt.Println(filas)
+	formatter(filas)
 }
 
-func formatter(rows [][]string) {
+func formatter(filas [][]string) {
 	fmt.Printf("%-10v %10v %10v\n", "Id", "Precio", "Cantidad")
 	total := 0.00
-	for _, row := range rows {
-		formattedRows := strings.Split(row[0], ";")
+	for _, fila := range filas {
+		formattedRows := strings.Split(fila[0], ";")
 		fmt.Printf("%-10s %10s %10s\n", formattedRows[0], formattedRows[1], formattedRows[2])
-		parsedFloat, err := strconv.ParseFloat(formattedRows[1], 64)
-		if err != nil {
-			fmt.Println(err)
+		parsedFloat1, err := strconv.ParseFloat(formattedRows[1], 64)
+		parsedFloat2, err2 := strconv.ParseFloat(formattedRows[2], 64)
+		if err != nil || err2 != nil {
+			fmt.Println(err, err2)
 		} else {
-			total += parsedFloat
+			total += (parsedFloat1 * parsedFloat2)
 		}
 	}
 	fmt.Printf("%21v\n", total)
