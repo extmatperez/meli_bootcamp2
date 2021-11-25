c64a3c72de29f80729f8fc966ed91fc0fd75cfc5
emontenegro <edgar.montenegro@mercadolibre.com>
2021-11-25 09:09:27 -0300

    📝  Change exercises names in 2_gobases1

---
+++ 2_gobases1/morning_activities/Exercise_1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	const my_name = "Edgar"
+	const my_surname = "Montenegro"
+	const address = "Dorrego 2620"
+
+	fmt.Println("My name is: ", my_name, my_surname)
+	fmt.Println("My address is: ", address)
+}
