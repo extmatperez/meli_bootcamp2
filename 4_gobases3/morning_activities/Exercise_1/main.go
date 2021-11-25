c38dd6714450cf42dcfce6587c8ce1adc98bd359
emontenegro <edgar.montenegro@mercadolibre.com>
2021-11-25 10:21:06 -0300

    ✨ Add structure ti 4_gobases3/morning exercises

---
+++ 4_gobases3/morning_activities/Exercise_1/main.go
@@ -0,0 +1,14 @@
+/* Ejercicio 1 - Guardar archivo
+Una empresa que se encarga de vender productos de limpieza necesita:
+Implementar una funcionalidad para guardar un archivo de texto, con la información de productos comprados, separados por punto y coma (csv).
+Debe tener el id del producto, precio y la cantidad.
+Estos valores pueden ser hardcodeados o escritos en duro en una variable.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
