fba30d7ba7aefd11b5c540ecca7c31be90fd4cea
FranciscoPalacioMeli <francisco.fpalacio@mercadolibre.com>
2021-11-25 20:08:10 -0300

    Excersie 4 finsh

---
+++ 2_gobases1/TurnoTarde/Ejercicio4T/tEjercicio4.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+
+func main() {
+  //mapaMeses = map[int]string{1,"Enero";2,"Febrero";"Marzo","Abril",
+  //                     "Mayo","Junio","Julio","Agosto",
+  //                    "Setiembre","Octubre","Noviembre","Diciembre"}
+  
+  var mes time.Month = 12
+
+   fmt.Println("El mes numero: ",int(mes), "es el mes: ",mes)
+
+ 
+
+
+
+}
