484f20133228c7c93564a20db1f7f3d06927cff3
FacuBouza <facundo.bouza@mercadolibre.com>
2021-11-25 13:49:45 -0300

    Ejercicio1 modified

--- 4_gobases3/PracticaTM/Ejercicio1/ejercicio1.go
+++ 4_gobases3/PracticaTM/Ejercicio1/ejercicio1.go
@@ -6,15 +6,15 @@ import (
 )
 
 type Producto struct {
-	Id       int
-	Precio   float64
-	Cantidad int
+	Id       int     `json:"id"`
+	Precio   float64 `json:"precio"`
+	Cantidad int     `json:"cantidad"`
 }
 
 func main() {
 	var productos []Producto
-	productos = append(productos, Producto{1, 88.50, 10})
-	productos = append(productos, Producto{2, 30.0, 5})
+	productos = append(productos, Producto{1, 1000.50, 10})
+	productos = append(productos, Producto{2, 3050000.0, 5})
 	productos = append(productos, Producto{3, 20.6, 2})
 	productos = append(productos, Producto{4, 33.5, 10})
 	productos = append(productos, Producto{5, 28.4, 20})
@@ -26,5 +26,12 @@ func main() {
 		stringProductos += prodActual
 	}
 	os.WriteFile("./ProductosComprados.csv", []byte(stringProductos), 0644)
+
+	// prodFormateados, err := json.Marshal(productos)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	os.WriteFile("./ProductosComprados.csv", prodFormateados, 0644)
+	// }
 
 }
