0cd853cb36628d5d073b289ac5bf33cc065e725e
ticianomen <ticiano.mensegue@mercadolibre.com>
2021-11-25 18:03:53 -0300

    tarea clase2 tm

--- 3_gobases2/TM/ejercicio4/ejercicio4.go
+++ 3_gobases2/TM/ejercicio4/ejercicio4.go
@@ -1,15 +1,15 @@
 package main
 
 import "fmt"
-import "errors"
-	/*
-		Los profesores de una universidad de Colombia necesitan calcular algunas 
-		estadísticas de calificaciones de los alumnos de un curso, requiriendo calcular 
+
+/*
+		Los profesores de una universidad de Colombia necesitan calcular algunas
+		estadísticas de calificaciones de los alumnos de un curso, requiriendo calcular
 		los valores mínimo, máximo y promedio de sus calificaciones.
 
-Se solicita generar una función que indique qué tipo de cálculo se quiere realizar 
-(mínimo, máximo o promedio) y que devuelva otra función ( y un error en caso que el 
-	cálculo no esté definido) que se le puede pasar una cantidad N de enteros y devuelva 
+Se solicita generar una función que indique qué tipo de cálculo se quiere realizar
+(mínimo, máximo o promedio) y que devuelva otra función ( y un error en caso que el
+	cálculo no esté definido) que se le puede pasar una cantidad N de enteros y devuelva
 	el cálculo que se indicó en la función anterior
 Ejemplo:
 const (
@@ -29,16 +29,16 @@ maxFunc, err := operacion(maximo)
 valorMinimo := minFunc(2,3,3,4,1,2,4,5)
 valorPromedio := promFunc(2,3,3,4,1,2,4,5)
 valorMaximo := maxFunc(2,3,3,4,1,2,4,5)
-	*/
+*/
 
 const (
-	minimo = "minimo"
+	minimo   = "minimo"
 	promedio = "promedio"
-	maximo = "maximo"
+	maximo   = "maximo"
 )
 
-func minFunc(n ...float64) float64{
-	aux:=n[0]
+func minFunc(n ...float64) float64 {
+	aux := n[0]
 	for _, i := range n {
 		if i < aux {
 			aux = i
@@ -46,15 +46,15 @@ func minFunc(n ...float64) float64{
 	}
 	return aux
 }
-func promFunc(n ...float64) float64{
-	aux:=n[0]
+func promFunc(n ...float64) float64 {
+	aux := n[0]
 	for _, i := range n {
-		aux+=i
+		aux += i
 	}
-	return aux/len(n)
+	return aux / len(n)
 }
-func maxFunc(n ...float64) float64{
-	aux:=n[0]
+func maxFunc(n ...float64) float64 {
+	aux := n[0]
 	for _, i := range n {
 		if i > aux {
 			aux = i
@@ -64,7 +64,7 @@ func maxFunc(n ...float64) float64{
 }
 
 func operacion(operacion string) func(n ...float64) {
-switch operacion{
+	switch operacion {
 	case minimo:
 		return minFunc
 	case promedio:
@@ -73,20 +73,19 @@ switch operacion{
 		return maxFunc
 	default:
 		return nil
+	}
 }
-}
-
-minFunc, err := operacion(minimo)
-promFunc, err := operacion(promedio)
-maxFunc, err := operacion(maximo)
-
 
 func main() {
-	valorMinimo := minFunc(2,3,3,4,1,2,4,5)
-	valorPromedio := promFunc(2,3,3,4,1,2,4,5)
-	valorMaximo := maxFunc(2,3,3,4,1,2,4,5)
+	minFunc := operacion(minimo)
+	promFunc := operacion(promedio)
+	maxFunc := operacion(maximo)
 
-	fmt.Printf("Minimo: %v \n",valorMinimo)
-	fmt.Printf("Promedio: %v \n",valorPromedio)
-	fmt.Printf("Maximo: %v \n",valorMaximo)
+	valorMinimo := minFunc(2, 3, 3, 4, 1, 2, 4, 5)
+	valorPromedio := promFunc(2, 3, 3, 4, 1, 2, 4, 5)
+	valorMaximo := maxFunc(2, 3, 3, 4, 1, 2, 4, 5)
+
+	fmt.Printf("Minimo: %v \n", valorMinimo)
+	fmt.Printf("Promedio: %v \n", valorPromedio)
+	fmt.Printf("Maximo: %v \n", valorMaximo)
 }
