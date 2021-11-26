5821e037ea438efbcdc49740ab3730f380bffe74
estefaniaPecora <barbara.pecora@mercadolibre.com>
2021-11-26 18:19:17 -0300

    added date struct TT_gb2_exercise1

--- 3_gobases2/TT_gb2/ejercicio1/main.go
+++ 3_gobases2/TT_gb2/ejercicio1/main.go
@@ -11,21 +11,27 @@ import (
 // Los valores que están en corchetes deben ser reemplazados por los datos brindados por los alumnos/as.
 // Para ello es necesario generar una estructura Alumnos con las variables Nombre, Apellido, DNI, Fecha y que tenga un método detalle
 
+type Date struct {
+	Day   int
+	Month int
+	Year  int
+}
+
 type Student struct {
 	Name    string
 	Surname string
 	DNI     int
-	Date    string
+	Entered Date
 }
 
 func (s *Student) details() {
-	fmt.Printf("Student info: \n Name: %s %s\n DNI: %d\n Date: %s\n", s.Name, s.Surname, s.DNI, s.Date)
+	fmt.Printf("Student info: \n Name: %s %s\n DNI: %d\n Date: %v\n", s.Name, s.Surname, s.DNI, s.Entered)
 
 }
 
 func main() {
 
-	student1 := Student{Name: "John", Surname: "Foo", DNI: 34567890, Date: "March 3 / 2021"}
+	student1 := Student{Name: "John", Surname: "Foo", DNI: 34567890, Entered: Date{3, 4, 2021}}
 
 	student1.details()
 
