c28e8fcb5c47b293c2553ccc06ee66e6d424d669
jpacostasc <juan.acosta@mercadolibre.com>
2021-11-26 10:40:10 -0300

    Ejercicio 4 mejorado

--- 4_gobases3/TTarde/Ejercicio 3/Ejercicio3.go
+++ 4_gobases3/TTarde/Ejercicio 3/Ejercicio3.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Productos struct {
-	Nombre string
-	Precio float64
+	Nombre   string
+	Precio   float64
 	Cantidad float64
 }
 
 type Servicios struct {
-	Nombre string
-	Precio float64
+	Nombre     string
+	Precio     float64
 	Minutos_tr float64
 }
 
@@ -21,13 +21,13 @@ type Mantenimiento struct {
 	Precio float64
 }
 
-func sumarProductos(p []Productos, c chan float64){
+func sumarProductos(p []Productos, c chan float64) {
 	var sum float64
 	for i := 0; i < len(p); i++ {
-		sum += p[i].Precio*p[i].Cantidad
+		sum += p[i].Precio * p[i].Cantidad
 	}
 	fmt.Println("Listo")
-	c<-sum
+	c <- sum
 }
 
 func sumarServicios(s []Servicios, c chan float64) {
@@ -37,51 +37,43 @@ func sumarServicios(s []Servicios, c chan float64) {
 		sum += s[i].Precio * conv
 	}
 	fmt.Println("Listo")
-	c<-sum
+	c <- sum
 }
 
-func sumarMantenimiento(m []Mantenimiento, c chan float64){
-var sum float64
-for i := 0; i < len(m);i++ {
-	sum += m[i].Precio
-}
+func sumarMantenimiento(m []Mantenimiento, c chan float64) {
+	var sum float64
+	for i := 0; i < len(m); i++ {
+		sum += m[i].Precio
+	}
 	fmt.Println("Listo")
-	c<-sum
+	c <- sum
 }
-
-
-
 
 func main() {
 	p1 := Productos{Nombre: "Juan", Precio: 244.65, Cantidad: 5}
-	p2 := Productos{Nombre:"Pedro", Precio: 214.88, Cantidad: 9}
+	p2 := Productos{Nombre: "Pedro", Precio: 214.88, Cantidad: 9}
 
-	s1 := Servicios{Nombre:"Luz", Precio: 100, Minutos_tr: 55}
-	s2 := Servicios{Nombre:"Gas", Precio: 160, Minutos_tr: 35}
+	s1 := Servicios{Nombre: "Luz", Precio: 100, Minutos_tr: 55}
+	s2 := Servicios{Nombre: "Gas", Precio: 160, Minutos_tr: 35}
 
-	m1 := Mantenimiento{Nombre:"Reparacion", Precio:544}
-	m2 := Mantenimiento{Nombre:"Actualizacion", Precio:437}
+	m1 := Mantenimiento{Nombre: "Reparacion", Precio: 544}
+	m2 := Mantenimiento{Nombre: "Actualizacion", Precio: 437}
 
 	list := make([]Productos, 0)
 	list = append(list, p1, p2)
 	list2 := make([]Servicios, 0)
 	list2 = append(list2, s1, s2)
 	list3 := make([]Mantenimiento, 0)
-	list3 = append(list3,m1,m2)
+	list3 = append(list3, m1, m2)
 
 	c := make(chan float64)
-	c1 := make(chan float64)
-	c2 := make(chan float64)
-
-	
-
 
 	go sumarProductos(list, c)
-	variable := <- c
-	go sumarServicios(list2, c1)
-	variable1 := <- c1
-	go sumarMantenimiento(list3, c2)
-	variable2 := <- c2
+	variable := <-c
+	go sumarServicios(list2, c)
+	variable1 := <-c
+	go sumarMantenimiento(list3, c)
+	variable2 := <-c
 
 	fmt.Printf("La suma de los totales es: %v", variable+variable1+variable2)
 
--- 4_gobases3/TTarde/Ejercicio 4/Ejercicio4.go
+++ 4_gobases3/TTarde/Ejercicio 4/Ejercicio4.go
@@ -6,19 +6,19 @@ import (
 	"time"
 )
 
-func burbuja(num []int, c chan int){
+func burbuja(num []int, c chan int) {
 	begin := time.Now()
-	for i:=0; i< len(num)-1; i++ {
-	   for j:=0; j < len(num)-i-1; j++ {
-		  if (num[j] > num[j+1]) {
-			 num[j], num[j+1] = num[j+1], num[j]
-		  }
-	   }
+	for i := 0; i < len(num)-1; i++ {
+		for j := 0; j < len(num)-i-1; j++ {
+			if num[j] > num[j+1] {
+				num[j], num[j+1] = num[j+1], num[j]
+			}
+		}
 	}
-	time.Sleep(1000*time.Millisecond)
-	end:= time.Now()
+	time.Sleep(1000 * time.Millisecond)
+	end := time.Now()
 	total := int(end.Sub(begin).Milliseconds())
-	
+
 	c <- total
 }
 
@@ -29,66 +29,77 @@ func insercion(num []int, c chan int) {
 			num[j], num[j-1] = num[j-1], num[j]
 		}
 	}
-	time.Sleep(1000*time.Millisecond)
-	end:= time.Now()
+	time.Sleep(1000 * time.Millisecond)
+	end := time.Now()
 	total := int(end.Sub(begin).Milliseconds())
-	
+
 	c <- total
 }
-
 
 func seleccion(num []int, c chan int) {
 	begin := time.Now()
-    for i := 0; i < len(num)-1; i++ {
-        min := i
-        for j := i + 1; j < len(num); j++ {
-            if num[j] < num[min] {
-                num[j], num[min] = num[min], num[j]
-            }
-        }
-    }
-	time.Sleep(1000*time.Millisecond)
-	end:= time.Now()
+	for i := 0; i < len(num)-1; i++ {
+		min := i
+		for j := i + 1; j < len(num); j++ {
+			if num[j] < num[min] {
+				num[j], num[min] = num[min], num[j]
+			}
+		}
+	}
+	time.Sleep(1000 * time.Millisecond)
+	end := time.Now()
 	total := int(end.Sub(begin).Milliseconds())
-	
+
 	c <- total
 
 }
 
- 
 func main() {
-   variable1 := rand.Perm(100)
-   variable2 := rand.Perm(1000)
+	variable1 := rand.Perm(100)
+	variable2 := rand.Perm(1000)
 	variable3 := rand.Perm(10000)
+	var copia []int
 
-   c := make(chan int)
-   c1 := make(chan int)
-   c2 := make(chan int)
+	c := make(chan int)
+	c1 := make(chan int)
+	c2 := make(chan int)
+    ini := time.Now()
+	copy(copia, variable1)
+	go burbuja(variable1, c)
+	go burbuja(variable2, c1)
+	go burbuja(variable3, c2)
+    tiempo1 := <-c
+    tiempo2 := <-c1
+	tiempo3 := <-c2
+	fmt.Println("Tiempo de burbuja:", tiempo1+tiempo2+tiempo3)
+    //fmt.Println("Tiempo de burbuja:", <-c+<-c1+<-c2)
+    fin := time.Now()
+    fmt.Println("Tiempo de ejecución:", fin.Sub(ini))
 
+    
+    ini = time.Now()
+	go seleccion(variable1, c)
+	go seleccion(variable2, c1)
+	go seleccion(variable3, c2)
+	fmt.Println("Tiempo de seleccion:", tiempo1+tiempo2+tiempo3)
+    tiempo1 = <-c
+    tiempo2 = <-c1
+	tiempo3 = <-c2
+	fmt.Println("Tiempo de burbuja:", tiempo1+tiempo2+tiempo3)
+    fin = time.Now()
+    fmt.Println("Tiempo de ejecución:", fin.Sub(ini))
 
-   go burbuja(variable1, c)
-   tiempo1 := <-c
-   go burbuja(variable2, c1)
-   tiempo2 := <-c1
-   go burbuja(variable3, c2)
-   tiempo3 := <-c2
-   fmt.Println("Tiempo de burbuja:", tiempo1 + tiempo2 + tiempo3)
-
-   go seleccion(variable1, c)
-   tiempo1 = <-c
-   go seleccion(variable2, c1)
-   tiempo2 = <-c1
-   go seleccion(variable3, c2)
-   tiempo3 = <-c2
-   fmt.Println("Tiempo de seleccion:", tiempo1 + tiempo2 + tiempo3)
-
-   go insercion(variable1, c)
-   tiempo1 = <-c
-   go insercion(variable2, c1)
-   tiempo2 = <-c1
-   go insercion(variable3, c2)
-   tiempo3 = <-c2
-   fmt.Println("Tiempo de insercion:", tiempo1 + tiempo2 + tiempo3)
-
+    
+    ini = time.Now()
+	go insercion(variable1, c)
+	go insercion(variable2, c1)
+	go insercion(variable3, c2)
+    tiempo1 = <-c
+    tiempo2 = <-c1
+	tiempo3 = <-c2
+	fmt.Println("Tiempo de burbuja:", tiempo1+tiempo2+tiempo3)
+    fin = time.Now()
+    fmt.Println("Tiempo de ejecución:", fin.Sub(ini))
+	fmt.Println("Tiempo de insercion:", tiempo1+tiempo2+tiempo3)
 
 }
