fc22379678ab8b8202426196104ab80bb9e94a0e
BeSaavedram <be.saavedram@gmail.com>
2021-11-26 10:00:02 -0300

    Exercise_2 / practice 2

---
+++ 4_gobases3/practica2/Ejercicio_2/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Products struct {
+	Name 		string
+	Price		int
+	Quantity	int
+}
+
+type User struct {
+	Name 		string
+	LastName 	string
+	Email		string
+	Products	[]Products
+}
+
+func main() {
+	/*
+	Una importante empresa de ventas web necesita agregar una funcionalidad para agregar productos a los usuarios.
+	Para ello requieren que tanto los usuarios como los productos tengan la misma dirección de memoria en el main
+	del programa como en las funciones.
+	Se necesitan las estructuras:
+		Usuario: Nombre, Apellido, Correo, Productos (array de productos).
+		Producto: Nombre, precio, cantidad.
+	Se requieren las funciones:
+		Nuevo producto: recibe nombre y precio, y retorna un producto.
+		Agregar producto: recibe usuario, producto y cantidad, no retorna nada, agrega el producto al usuario.
+		Borrar productos: recibe un usuario, borra los productos del usuario.
+	*/
+
+	var pList []Products
+	// set products
+	p := newProduct("cucharas", 500)
+	p2 := newProduct("tenedores", 500)
+	pList = append(pList, p)
+	//set User
+	u := User{"juan", "soza", "JuanZosa@mail.com", nil}
+
+	addProduct(&u, &p, 5)
+	addProduct(&u, &p2, 4)
+
+	fmt.Println("El usuario: ", u)
+	addProduct(&u, &p, 5)
+	fmt.Println("El usuario: ", u)
+	deleteProducts(&u)
+	fmt.Println("Sin productos", u)
+}
+func newProduct(name string, price int) Products {
+	nProduct := Products{name, price, 0}
+	return nProduct
+}
+func addProduct(user *User, product *Products, quantity int)  {
+	checker := false
+	for i, value := range user.Products{
+		if value.Name == product.Name {
+			user.Products[i].Quantity = user.Products[i].Quantity + quantity
+			user.Products[i].Price = product.Price * user.Products[i].Quantity
+			checker = true
+		}
+	}
+	if checker == false {
+		product.Quantity = quantity
+		user.Products = append(user.Products, *product)
+		for i, _ := range user.Products{
+				user.Products[i].Price = product.Price * user.Products[i].Quantity
+		}
+	}
+}
+func deleteProducts(user *User) {
+	user.Products = nil
+}
