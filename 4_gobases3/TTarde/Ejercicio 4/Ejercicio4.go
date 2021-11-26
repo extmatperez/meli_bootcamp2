4674f2e0e4ef170eef759747de41faba34ac15e5
jpacostasc <juan.acosta@mercadolibre.com>
2021-11-26 00:36:02 -0300

    Ejercicio 4 TT terminado

---
+++ 4_gobases3/TTarde/Ejercicio 4/Ejercicio4.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func burbuja(num []int, c chan int){
+	begin := time.Now()
+	for i:=0; i< len(num)-1; i++ {
+	   for j:=0; j < len(num)-i-1; j++ {
+		  if (num[j] > num[j+1]) {
+			 num[j], num[j+1] = num[j+1], num[j]
+		  }
+	   }
+	}
+	time.Sleep(1000*time.Millisecond)
+	end:= time.Now()
+	total := int(end.Sub(begin).Milliseconds())
+	
+	c <- total
+}
+
+func insercion(num []int, c chan int) {
+	begin := time.Now()
+	for i := 0; i < len(num); i++ {
+		for j := i; j > 0 && num[j-1] > num[j]; j-- {
+			num[j], num[j-1] = num[j-1], num[j]
+		}
+	}
+	time.Sleep(1000*time.Millisecond)
+	end:= time.Now()
+	total := int(end.Sub(begin).Milliseconds())
+	
+	c <- total
+}
+
+
+func seleccion(num []int, c chan int) {
+	begin := time.Now()
+    for i := 0; i < len(num)-1; i++ {
+        min := i
+        for j := i + 1; j < len(num); j++ {
+            if num[j] < num[min] {
+                num[j], num[min] = num[min], num[j]
+            }
+        }
+    }
+	time.Sleep(1000*time.Millisecond)
+	end:= time.Now()
+	total := int(end.Sub(begin).Milliseconds())
+	
+	c <- total
+
+}
+
+ 
+func main() {
+   variable1 := rand.Perm(100)
+   variable2 := rand.Perm(1000)
+	variable3 := rand.Perm(10000)
+
+   c := make(chan int)
+   c1 := make(chan int)
+   c2 := make(chan int)
+
+
+   go burbuja(variable1, c)
+   tiempo1 := <-c
+   go burbuja(variable2, c1)
+   tiempo2 := <-c1
+   go burbuja(variable3, c2)
+   tiempo3 := <-c2
+   fmt.Println("Tiempo de burbuja:", tiempo1 + tiempo2 + tiempo3)
+
+   go seleccion(variable1, c)
+   tiempo1 = <-c
+   go seleccion(variable2, c1)
+   tiempo2 = <-c1
+   go seleccion(variable3, c2)
+   tiempo3 = <-c2
+   fmt.Println("Tiempo de seleccion:", tiempo1 + tiempo2 + tiempo3)
+
+   go insercion(variable1, c)
+   tiempo1 = <-c
+   go insercion(variable2, c1)
+   tiempo2 = <-c1
+   go insercion(variable3, c2)
+   tiempo3 = <-c2
+   fmt.Println("Tiempo de insercion:", tiempo1 + tiempo2 + tiempo3)
+
+
+}
