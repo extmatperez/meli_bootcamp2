d24d120e9aaa911915578af0bc0426a2d0a4e826
diegoParrad <diego.parra@mercadolibre.com.co>
2021-11-25 21:28:19 -0500

    update exercise 1 - mañana-

--- 4_gobases3/Mañana/ejercicio_1/main.go
+++ 4_gobases3/Mañana/ejercicio_1/main.go
@@ -7,31 +7,32 @@ import (
 )
 
 type producto struct {
-	id       int
-	precio   float64
-	cantidad int
+	Id       int     `json:"id"`
+	Precio   float64 `json:"precio"`
+	Cantidad int     `json:"cantidad"`
 }
 
 func main() {
 	p1 := producto{
-		id:       111223,
-		precio:   30012.00,
-		cantidad: 1,
+		Id:       99,
+		Precio:   30012.00,
+		Cantidad: 1,
 	}
 
 	p2 := producto{
-		id:       444321,
-		precio:   1000000.00,
-		cantidad: 4,
+		Id:       100,
+		Precio:   55000.00,
+		Cantidad: 4,
 	}
-	fmt.Printf("ID \t\tPrecio \t\tCantidad\n")
+	//fmt.Printf("ID \t\tPrecio \t\tCantidad\n")
 	var lista []producto
 	//text := fmt.Sprint(p1.id, "\t\t", p1.precio, "\t\t", p1.cantidad)
 	lista = append(lista, p1)
 	lista = append(lista, p2)
 	p1formateado, err := json.Marshal(lista)
-
-	err = os.WriteFile("./archivo/myFile.txt", p1formateado, 0644)
+	// fmt.Println(lista)
+	// fmt.Println(p1formateado)
+	err = os.WriteFile("./myFile.txt", p1formateado, 0644)
 
 	if err != nil {
 		fmt.Println("No se pudo escribir")
--- 4_gobases3/Tarde/ejercicio_4/main.go
+++ 4_gobases3/Tarde/ejercicio_4/main.go
@@ -1,11 +1,11 @@
 package main
 
-import (
-	"math/rand"
-)
+// import (
+// 	"math/rand"
+// )
 
 func main() {
-	variable1 := rand.Perm(100)
-	variable2 := rand.Perm(1000)
-	variable3 := rand.Perm(10000)
+	// variable1 := rand.Perm(100)
+	// variable2 := rand.Perm(1000)
+	// variable3 := rand.Perm(10000)
 }
