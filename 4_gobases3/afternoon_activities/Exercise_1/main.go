8d3bb2d90fba8375bf1e6ffb5bb3011db7bc0d75
emontenegro <edgar.montenegro@mercadolibre.com>
2021-11-25 22:38:41 -0300

    ✨ Add folders & files to start 4_gobases3/afternoon activities

---
+++ 4_gobases3/afternoon_activities/Exercise_1/main.go
@@ -0,0 +1,17 @@
+/* Ejercicio 1 - Red social
+Una empresa de redes sociales requiere implementar una estructura usuario con funciones que vayan agregando información a la estructura.
+Para optimizar y ahorrar memoria requieren que la estructura usuarios ocupe el mismo lugar en memoria para el main del programa y para las funciones:
+La estructura debe tener los campos: Nombre, Apellido, edad, correo y contraseña
+Y deben implementarse las funciones:
+cambiar nombre: me permite cambiar el nombre y apellido.
+cambiar edad: me permite cambiar la edad.
+cambiar correo: me permite cambiar el correo.
+cambiar contraseña: me permite cambiar la contraseña. */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
--- 4_gobases3/morning_activities/Exercise_1/main.go
+++ 4_gobases3/morning_activities/Exercise_1/main.go
@@ -9,6 +9,89 @@ package main
 
 import "fmt"
 
+type Product struct {
+	Product_id int     `json:"nombre"`
+	Price      float64 `json:"price"`
+	Quantity   int     `json:"quantity"`
+}
+
+func save_product() {
+
+}
+
 func main() {
 	fmt.Println("Hello World!")
 }
+
+/* package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type producto struct {
+	Id       int     `json:"id"`
+	Precio   float64 `json:"precio"`
+	Cantidad int     `json:"cantidad"`
+}
+
+func guardarArchivo(prod producto, file string) {
+
+	data, err := os.ReadFile(file)
+
+	var ListaProds []producto
+
+	json.Unmarshal(data, &ListaProds)
+
+	ListaProds = append(ListaProds, prod)
+
+	formProdList, err := json.Marshal(ListaProds)
+
+	erro := os.WriteFile(file, formProdList, 0644)
+
+	if err != nil {
+		fmt.Println("E
+	if err != nil {
+		fmt.Println("Error json: ", err)
+	}
+
+	if erro != nil {
+		fmt.Println("Error", err)
+	}
+}
+
+func leerArchivo(file string) {
+	data, err := os.ReadFile(file)
+
+	var ListaProds []producto
+
+	json.Unmarshal(data, &ListaProds)
+
+	if err == nil {
+		formatoPrint(ListaProds)
+	} else {
+		fmt.Println("El archivo no existe...")
+	}
+}
+
+func formatoPrint(lista []producto) {
+	var total float64
+	fmt.Printf("ID\t Precio Cantidad")
+	for i := 0; i < len(lista); i++ {
+		total += lista[i].Precio * float
+Arevalo Ivan13:42
+for i := 0; i < len(lista); i++ {
+		total += lista[i].Precio * float64(lista[i].Cantidad)
+		fmt.Printf("\n%v\t %v\t %v", lista[i].Id, lista[i].Precio, lista[i].Cantidad)
+	}
+	fmt.Printf("\n\t %v\n", total)
+
+}
+
+func main() {
+	// prod1 := producto{1112, 100.50, 3}
+	// guardarArchivo(prod1, "../../a.txt")
+	leerArchivo("../../a.txt")
+} */
