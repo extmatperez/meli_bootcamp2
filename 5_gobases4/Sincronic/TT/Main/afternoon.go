c08cec822aac8b8a1a8e9c3ddd1ccfba45766fc6
jriosMELI <jose.rios@mercadolibre.cl>
2021-11-26 15:43:01 -0300

    Previo al defer

--- 5_gobases4/Sincronic/TT/Main/afternoon.go
+++ 5_gobases4/Sincronic/TT/Main/afternoon.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	//Deffer
+	func() {
+		fmt.Println("Hola soy una funcion anonima")
+	}() // Al agregar los parentecis al final se ejecuta inmediatamente esta funcion
+
+	num := 15
+	if num == 15 {
+		panic("Panic Generado!")
+	}
+
+	func() {
+		fmt.Println("Hola soy la segunda funcion anonima y no me ejecuto por que hay un panic anterior!")
+	}()
 
 }
