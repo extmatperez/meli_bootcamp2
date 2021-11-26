adaa74fbf821a567c9d037c935490e34d4ee7de9
dignelidxdx <digneli.davila@mercadolibre.com>
2021-11-26 12:53:27 -0300

    Completed exercise 3 for go bases 4 turn morning

---
+++ 5_gobases4/turn_morning/exercise_3/exercise.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+type myError struct {
+	message string
+	code    int
+}
+
+func (e *myError) Error() string {
+	return e.message
+}
+
+func errorControl(salary int) (int, error) {
+
+	if salary < 150000 {
+		return 0, &myError{
+			message: "error: el salario ingresado no alcanza el mínimo imponible",
+			code:    400,
+		}
+	}
+	return salary, nil
+}
+
+func main() {
+	var salary int = 15000
+	response, err := errorControl(salary)
+
+	if err != nil {
+		error := fmt.Errorf("mensaje: %s", err)
+		fmt.Println("Get error:", error)
+	} else {
+		fmt.Println("El salario ingresado es: ", response)
+	}
+}
