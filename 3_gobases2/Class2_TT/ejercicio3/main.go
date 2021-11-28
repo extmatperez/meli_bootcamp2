95d6b1ee2e7604f482116e0ea4afce54c03cdcdd
igvargas <ignacio.vargas@mercadolibre.com>
2021-11-28 18:05:00 -0300

    Ex3 done

--- 3_gobases2/Class2_TT/ejercicio3/main.go
+++ 3_gobases2/Class2_TT/ejercicio3/main.go
@@ -1,14 +1,22 @@
 package main
 
-const (
-	pequeño = "pequeño"
-	mediano = "mediano"
-	grande  = "grande"
-)
+import "fmt"
 
 func main() {
-	//productosTienda := 
+	tipoGrande := Grande{}
+	tipoMediano := Mediano{}
+	tipoChico := Chico{}
+	producto1 := producto{tipoChico, "llavero", 37.5}
+	producto11 := producto{tipoChico, "llavero", 37.5}
+	producto2 := producto{tipoMediano, "caja", 400}
+	producto21 := producto{tipoMediano, "caja", 400}
+	producto3 := producto{tipoGrande, "televisor", 8538.4}
+	producto31 := producto{tipoGrande, "televisor", 8538.4}
+	productosTienda := []producto{producto1, producto11, producto2, producto21, producto31, producto2, producto3}
 	tienda1 := nuevaTienda(productosTienda, "tienda1")
+
+	fmt.Printf("El costo total de los productos es de: $ %.2f\n", tienda1.total())
+	//fmt.Println(tienda1)
 }
 
 type tienda struct {
@@ -16,9 +24,9 @@ type tienda struct {
 }
 
 type producto struct {
-	nombre       string
-	precio       float64
-	tipoProducto string
+	Tipo   Tipo
+	nombre string
+	precio float64
 }
 
 type Producto interface {
@@ -27,28 +35,63 @@ type Producto interface {
 
 type Ecommerce interface {
 	total() float64
-	agregar(producto producto) float64
-}
-
-func (p *producto) calcularCosto() float64 {
-	return 0
+	agregar(producto producto)
 }
 
 func (t *tienda) total() float64 {
-	return 0
+	total := 0.0
+	for _, prod := range t.productos {
+		fmt.Printf("El precio del producto %v es de: $%.2f\n", prod.nombre, prod.CalcularCosto())
+		total += prod.precio
+	}
+	return total
 }
 
-func (t *tienda) agregar(nuevoProduct producto) float64 {
-	return 0
-}
-
-func nuevoProduct() producto {
-	return producto{}
+func (t *tienda) agregar(nuevoProduct producto) {
+	t.productos = append(t.productos, nuevoProduct)
 }
 
 func nuevaTienda(productosTienda []producto, tipo string) Ecommerce {
 	switch tipo {
 	case "tienda1":
-		return &tienda{productosTienda,
-		}
+		return &tienda{productosTienda}
+	}
+	return nil
+}
+
+func (p *producto) CalcularCosto() float64 {
+	return p.Tipo.CalcularAdicional()(p.precio)
+}
+
+type Tipo interface {
+	CalcularAdicional() func(precio float64) float64
+}
+
+// --------------------------------------------- Tipos de producto -----------------------------------------------------
+
+type Grande struct{}
+
+func (n Grande) CalcularAdicional() func(precio float64) float64 {
+	fun := func(precio float64) float64 {
+		return precio + ((precio * 6) / 100) + 2500
+	}
+	return fun
+}
+
+type Mediano struct{}
+
+func (n Mediano) CalcularAdicional() func(precio float64) float64 {
+	fun := func(precio float64) float64 {
+		return precio + ((precio * 3) / 100)
+	}
+	return fun
+}
+
+type Chico struct{}
+
+func (n Chico) CalcularAdicional() func(precio float64) float64 {
+	fun := func(precio float64) float64 {
+		return precio
+	}
+	return fun
 }
