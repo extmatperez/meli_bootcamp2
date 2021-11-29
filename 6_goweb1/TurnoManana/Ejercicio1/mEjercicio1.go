2ddcf6469b5b80bc05cab23988a607ae0694e8d7
FranciscoPalacioMeli <francisco.fpalacio@mercadolibre.com>
2021-11-29 12:30:04 -0300

    Ejercio 1  6_goweb1 finsh

---
+++ 6_goweb1/TurnoManana/Ejercicio1/mEjercicio1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+type Transaccion struct{
+
+	Id int
+	Codigo string
+	Moneda string
+	Monto string
+	Emisor string
+	Receptor string
+	Fecha time.Time
+
+}
+
+
+func main() {
+
+ }
+
+
