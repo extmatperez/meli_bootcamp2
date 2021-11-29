4b7e0d289edb84151936bae8201f6af585f1a464
jpacostasc <juan.acosta@mercadolibre.com>
2021-11-29 20:58:31 -0300

    Ejercicios de la tarde completados 29/11

--- 6_goweb1/main.go
+++ 6_goweb1/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
+///////////////////////////////////////////////////////////////////////////
 type Transaccion struct {
 	ID                int     `json:"id"`
 	CodigoTransaccion int     `json:"codigo_transaccion"`
@@ -15,13 +18,61 @@ type Transaccion struct {
 	Receptor          string  `json:"receptor"`
 	FechaTransaccion  string  `json:"fecha_transaccion"`
 }
-
+///////////////////////////////////////////////////////////////////////////
 func GetAll(c *gin.Context){
 	var lista []Transaccion
 	data, _ := os.ReadFile("6_goweb1/transacciones.json")
 	json.Unmarshal(data, &lista)
 	c.JSON(200, lista)
 }
+
+///////////////////////////////////////////////////////////////////////////
+func filtrarTransacciones(ctx *gin.Context){
+	var lista []Transaccion
+	data, _ := os.ReadFile("6_goweb1/transacciones.json")
+	json.Unmarshal(data, &lista)
+	var filtrados []*Transaccion
+
+	for i, e := range lista{
+		if ctx.Query("codigo") ==  e.Emisor{
+			filtrados = append(filtrados, &lista[i])
+		}
+	}
+	if len(filtrados) == 0{
+		ctx.String(404, "No se encontraron coincidencias")
+	}else{
+		ctx.JSON(200, &filtrados)
+	}
+}
+///////////////////////////////////////////////////////////////////////////
+func buscarTransaccion(ctx *gin.Context){
+	parametro := ctx.Param("codigo_transaccion")
+
+	var lista []Transaccion
+	data, _ := os.ReadFile("6_goweb1/transacciones.json")
+	json.Unmarshal(data, &lista)
+
+	var tran Transaccion
+	se := false
+
+	for _, v := range lista{
+		str := fmt.Sprint(v.CodigoTransaccion)
+		if str == parametro{
+			tran = v
+			se = true
+			break
+		}
+	}
+	if se{
+		ctx.JSON(200, tran)
+	}else{
+		ctx.String(404, "Registro no encontrado")
+	}
+}
+
+
+
+
 
 func main() {
 
@@ -32,7 +83,8 @@ func main() {
 	})
 
 	router.GET("/transacciones", GetAll)
-
+	router.GET("/filtrar",filtrarTransacciones)
+	router.GET("/buscar/:codigo_transaccion",buscarTransaccion)
 
 	router.Run()
 	
