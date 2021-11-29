e09d7224991e5a14c2d0df98dbf055bcfb28a01f
FranciscoPalacioMeli <francisco.fpalacio@mercadolibre.com>
2021-11-29 13:38:49 -0300

    Ejercicio 3  6_goweb1 finsh

--- 6_goweb1/TurnoManana/Ejercicio1/mEjercicio1.go
+++ 6_goweb1/TurnoManana/Ejercicio1/mEjercicio1.go
@@ -1,25 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 
-type Transaccion struct{
-
-	Id int
-	Codigo string
-	Moneda string
-	Monto string
-	Emisor string
-	Receptor string
-	Fecha time.Time
-
+ 
+type Transaccion []struct {
+	ID       int    `json:"id"`
+	Codigo   string `json:"codigo"`
+	Moneda   string `json:"moneda"`
+	Monto    string `json:"monto"`
+	Emisor   string `json:"emisor"`
+	Receptor string `json:"receptor"`
+	Fecha    string `json:"fecha"`
 }
 
 
+func GetTransactionFromFolder(fileName string) Transaccion{
+	file, _ := ioutil.ReadFile(fileName)
+	
+	var transaction Transaccion
+ 
+	_ = json.Unmarshal([]byte(file), &transaction)
+
+	return transaction
+
+}
+
+func GetAllTransactions(router *gin.Engine,filename string){
+	transactions := GetTransactionFromFolder(filename)
+
+	router.GET("/alltransaction", func(c *gin.Context){
+		c.JSON(http.StatusOK,transactions)
+	})
+}
 func main() {
+	ruta := "./6_goweb1/transactions.json"
+
+	router := gin.Default()
+	GetAllTransactions(router,ruta)
+	router.Run()
 
  }
 
