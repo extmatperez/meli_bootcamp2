39c3b10a05618ba594a5a742d9a673d1e3a1c851
FranciscoPalacioMeli <francisco.fpalacio@mercadolibre.com>
2021-11-29 09:51:26 -0300

    Ejercio 4  4_gobases3tt ordenamiento Insert,burbuja

--- 4_gobases3/TurnoTarde/Ejercicio4T/tEjercicio1.go
+++ 4_gobases3/TurnoTarde/Ejercicio4T/tEjercicio1.go
@@ -3,35 +3,72 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 
 func OrdenamientoInsercion(numeros []int) []int{
+	init := time.Now()
 	var clave,j int
 	for i :=1;i<=len(numeros)-1;i++ {
 		clave = numeros[i]
 		j = i-1
-		fmt.Println(i,clave)
-		fmt.Println(j,	numeros[j])
-	
 		for (j >= 0 && clave < numeros[j]){
-					fmt.Println("Clave",clave,"<",numeros[j])
+
 					numeros[j+1] = numeros[j] 
 					numeros[j] = clave				
 					j--
 		}
-		
-		fmt.Println(numeros)
 	}
+	fin := time.Now()
+	tiempo := fin.Sub(init).Seconds()
+	fmt.Println("El tiempo para ordenar por insercion: ",tiempo)
 	return numeros
 }
 
+func OrdenamientoBrubuja(ListaDesordenada []int) []int{
+	init := time.Now()
+	var auxiliar int
+	for i := 0; i < len(ListaDesordenada); i++ {
+	 for j := 0; j < len(ListaDesordenada); j++ {
+	  if ListaDesordenada[i] > ListaDesordenada[j] {
+	   auxiliar = ListaDesordenada[i]
+	   ListaDesordenada[i] = ListaDesordenada[j]
+	   ListaDesordenada[j] = auxiliar
+	  }
+	 }
+	}
+	fin := time.Now()
+
+	tiempo := fin.Sub(init).Seconds()
+
+	fmt.Println("El tiempo para ordenar por burbuja: ",tiempo)
+	return ListaDesordenada
+}
+
+
+
+
+
 func main() {
 	variable1 := rand.Perm(100)
+   variable2 := rand.Perm(1000)
+   variable3 := rand.Perm(10000)
 
-	
-	fmt.Println(variable1)
-	fmt.Println(OrdenamientoInsercion(variable1))
+
+ 	fmt.Println("Variable 1 Tiempo")
+	OrdenamientoInsercion(variable1)
+	OrdenamientoBrubuja(variable1)
+
+	fmt.Println("Variable 2 Tiempo")
+	OrdenamientoInsercion(variable2)
+	OrdenamientoBrubuja(variable2)
+
+
+
+	fmt.Println("Variable 3 Tiempo")
+	OrdenamientoInsercion(variable3)
+	OrdenamientoBrubuja(variable3)
 
 }
 
