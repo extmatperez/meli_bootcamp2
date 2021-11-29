905e177186d3cfe601a46a3b8f75650db1f69563
igvargas <ignacio.vargas@mercadolibre.com>
2021-11-28 22:45:50 -0300

    Ex1 done

--- 4_gobases3/Class3_TT/ejercicio1/main.go
+++ 4_gobases3/Class3_TT/ejercicio1/main.go
@@ -1,7 +1,26 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+func main() {
+	u1 := usuario{}
+
+	u1.setNombre("ariel", "lol")
+	u1.setEdad(23)
+	u1.setCorreo("arielol@meli.com")
+	u1.setContraseña("1234567890")
+
+	usuarios := []usuario{u1}
+	uFormated, err := json.Marshal(usuarios)
+
+	if err != nil {
+		fmt.Println("Ha ocurrido un error")
+	} else {
+		fmt.Println(string(uFormated))
+	}
 }
 
 type usuario struct {
@@ -12,6 +31,19 @@ type usuario struct {
 	Contraseña string
 }
 
-func (u usuario) String() string {
+func (u *usuario) setNombre(nombre string, apellido string) {
+	u.Nombre = nombre
+	u.Apellido = apellido
+}
 
+func (u *usuario) setEdad(edad int) {
+	u.Edad = edad
+}
+
+func (u *usuario) setContraseña(contraseña string) {
+	u.Contraseña = contraseña
+}
+
+func (u *usuario) setCorreo(correo string) {
+	u.Correo = correo
 }
