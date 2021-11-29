71c9a2a79e88143de9343c2bf5a75f88b5af69a4
jriosMELI <jose.rios@mercadolibre.cl>
2021-11-29 09:55:20 -0300

    In proces go bases4 TT y TM last exercise

---
+++ 5_gobases4/Morning/Exercise4/exercise4.go
@@ -0,0 +1,27 @@
+/*
+Vamos a hacer que nuestro programa sea un poco más complejo y útil.
+1- Desarrolla las funciones necesarias para permitir a la empresa calcular:
+	a- Salario mensual de un trabajador según la cantidad de horas trabajadas.
+		- La función recibirá las horas trabajadas en el mes y el valor de la hora como
+		argumento.
+		- Dicha función deberá retornar más de un valor (salario calculado y error).
+		- En caso de que el salario mensual sea igual o superior a $150.000,
+		se le deberá descontar el 10% en concepto de impuesto.
+		- En caso de que la cantidad de horas mensuales ingresadas sea menor a 80 o un número
+		negativo, la función debe devolver un error. El mismo deberá indicar “error: el
+		trabajador no puede haber trabajado menos de 80 hs mensuales”.
+	b- Calcular el medio aguinaldo correspondiente al trabajador
+		- Fórmula de cálculo de aguinaldo:
+		[mejor salario del semestre] / 12 * [meses trabajados en el semestre].
+		- La función que realice el cálculo deberá retornar más de un valor, incluyendo un error
+		en caso de que se ingrese un número negativo.
+
+2- Desarrolla el código necesario para cumplir con las funcionalidades requeridas,
+utilizando “errors.New()”, “fmt.Errorf()” y “errors.Unwrap()”.
+No olvides realizar las validaciones de los retornos de error en tu función “main()”.
+*/
+package main
+
+func main() {
+
+}
