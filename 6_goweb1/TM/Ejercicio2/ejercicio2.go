fa9bf8ae292983ef46bb4bf0b2536f4e2b2238c1
Matias Ziliotto <matias.ziliotto@mercadolibre.com>
2021-11-29 12:28:52 -0300

    Add query params to Ejercicio2-TM

--- 6_goweb1/TM/Ejercicio2/ejercicio2.go
+++ 6_goweb1/TM/Ejercicio2/ejercicio2.go
@@ -8,15 +8,23 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/hello", func(c *gin.Context) {
+		name := c.Query("name")
+
+		message := "Hola"
+
+		if name != "" {
+			message = message + " " + name
+		}
+
 		c.JSON(200, gin.H{
-			"message": "hola",
+			"message": message,
 		})
 	})
 
 	router.GET("/hello/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.JSON(200, gin.H{
-			"message": "hola " + name,
+			"message": "Hola " + name,
 		})
 	})
 
