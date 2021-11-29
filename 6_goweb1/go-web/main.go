ee43bf69c13e7ec0eb14261931d17ab8e70ba90b
Franco Julian Andrada Arganaraz <fandrada@ar0c02g310pml85.fibertel.com.ar>
2021-11-29 16:44:28 -0300

    feat(6_goweb1/go-web):getAll

--- 6_goweb1/go-web/main.go
+++ 6_goweb1/go-web/main.go
@@ -13,11 +13,93 @@
 // Crea un servidor web con Gin que te responda un JSON que tenga una clave “message” y diga Hola seguido por tu nombre.
 // Pegale al endpoint para corroborar que la respuesta sea la correcta.
 
+////////////////////////////////////////
+
+// Ejercicio 3 - Listar Entidad
+
+// Ya habiendo creado y probado nuestra API que nos saluda, generamos una ruta que devuelve un listado de la temática elegida.
+// Dentro del “main.go”, crea una estructura según la temática con los campos correspondientes.
+// Genera un endpoint cuya ruta sea /temática (en plural). Ejemplo: “/productos”
+// Genera un handler para el endpoint llamado “GetAll”.
+// Crea una slice de la estructura, luego devuelvela a través de nuestro endpoint.
+//	"github.com/gin-gonic/gin"
+
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Product []struct {
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	Color        string `json:"color"`
+	Price        string `json:"price"`
+	Stock        string `json:"stock"`
+	Code         string `json:"code"`
+	Published    bool   `json:"published"`
+	CreationDate string `json:"creation_date"`
+}
+
+// var products []interface{}
+// 		jsonFile, err := os.ReadFile("./products.json")
+// 		if err == nil {
+// 			json.Unmarshal(jsonFile, &products)
+// 			c.JSON(http.StatusOK, products)
+// 		}
+// Diego Alejandro Parra Daza16:39
+// 	archivo, _ := os.ReadFile("./products.json")
+// 	var listProducts []Product
+// 	json.Unmarshal(archivo, &listProducts)
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"products": listProducts,
+// 	})
+
+func getAll(c *gin.Context) {
+	var products []interface{}
+	//file, _ := ioutil.ReadFile("./products.json")
+	file, _ := os.ReadFile("./products.json")
+
+	// if err := json.Unmarshal(file, &products); err != nil {
+	// 	log.Fatal(err)
+	// }
+	json.Unmarshal([]byte(file), &products)
+
+	c.JSON(200, gin.H{
+		"data": products,
+	})
+}
+
+// func getAll(c *gin.Context) {
+
+// 	datos, _ := os.ReadFile("./productos.json")
+// 	var lista []Productos
+// 	json.Unmarshal(datos, &lista)
+
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"productos": lista,
+// 	})
+// }
 
 func main() {
+
+	// file, err := ioutil.ReadFile("./products.json")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// var as Product
+
+	// jsonData, err := json.Marshal(file)
+
+	// as = jsonData
+
+	//ext := string(file)
+	//fmt.Println(text)
+
 	// Crea un router con gin
 	router := gin.Default()
 	// Captura la solicitud GET “/hello-world”
@@ -26,6 +108,8 @@ func main() {
 			"message": "Hello Franco!",
 		})
 	})
+	router.GET("/products", getAll)
 	// Corremos nuestro servidor sobre el puerto 8080
 	router.Run()
+
 }
