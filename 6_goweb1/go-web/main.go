84776a09725105e4308c0d1ffd62bb211c6f5875
Walter Jesus Castillo Bastidas <waltercastillo017@outlook.com>
2021-11-29 15:12:40 -0500

    ejercicio1T clase 29nov

--- 6_goweb1/go-web/main.go
+++ 6_goweb1/go-web/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -38,11 +39,50 @@ func GetAll(c *gin.Context) {
 		"message": products,
 	})
 }
-
+func GetById(c *gin.Context) {
+	var products []Products
+	var product Products
+	ps, err := os.ReadFile("./6_goweb1/go-web/archivos/products.json")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		json.Unmarshal(ps, &products)
+		id, _ := strconv.Atoi(c.Param("id"))
+		for _, p := range products {
+			if p.ID == id {
+				product = p
+			}
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": product,
+		})
+	}
+}
+func GetByPublicado(c *gin.Context) {
+	var products []Products
+	var filtrados []Products
+	ps, err := os.ReadFile("./6_goweb1/go-web/archivos/products.json")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		json.Unmarshal(ps, &products)
+		b, _ := strconv.ParseBool(c.Query("publicado"))
+		for _, p := range products {
+			if b == p.Publicado {
+				filtrados = append(filtrados, p)
+			}
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": filtrados,
+		})
+	}
+}
 func main() {
 
 	router := gin.Default()
 	router.GET("/hola/:fulano", saludar)
 	router.GET("/products", GetAll)
+	router.GET("product/:id", GetById)             //localhost:8080/product/1
+	router.GET("productpublicado", GetByPublicado) // localhost:8080/productpublicado?publicado=false
 	router.Run()
 }
