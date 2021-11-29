49468a8cbe1ea7c12bd6f7007dadc047e05bd31e
nicoarguello <nicolas.arguello@mercadolibre.com>
2021-11-29 09:41:58 -0300

    changes

--- 3_gobases2/Tarde/Ej3/main.go
+++ 3_gobases2/Tarde/Ej3/main.go
@@ -32,3 +32,112 @@ los adicionales si los hubiera.
 */
 
 package main
+
+import "fmt"
+
+type tienda struct {
+	ListaProductos []Producto
+}
+
+func (t *tienda) Total() (tota float64) {
+	total := 0.0
+	for _, prod := range t.ListaProductos {
+		total += prod.CalcularCosto()
+	}
+	return
+}
+
+func (t *tienda) Agregar(p Producto) {
+	t.ListaProductos = append(t.ListaProductos, p)
+}
+
+type tipo interface {
+	CalcularAdicional() func(precio float64) float64
+}
+
+type grande struct{}
+type mediano struct{}
+type chico struct{}
+
+func (t grande) CalcularAdicional() func(precio float64) float64 {
+	funcion := func(precio float64) float64 {
+		return precio*1.06 + 2500.0
+	}
+	return funcion
+}
+
+func (t mediano) CalcularAdicional() func(precio float64) float64 {
+	return func(precio float64) float64 {
+		return precio * 1.03
+	}
+}
+
+func (t chico) CalcularAdicional() func(precio float64) float64 {
+	return func(precio float64) float64 {
+		return precio
+	}
+}
+
+type producto struct {
+	Nombre string
+	Precio float64
+	Tipo   tipo
+}
+
+func (p producto) CalcularCosto() float64 {
+	return p.Tipo.CalcularAdicional()(p.Precio)
+}
+
+type Producto interface {
+	CalcularCosto() float64
+}
+
+type Ecommerce interface {
+	Total() float64
+	Agregar(p Producto)
+}
+
+func nuevoProducto(tipoProducto string, nombre string, precio float64) Producto {
+	switch tipoProducto {
+	case "Grande":
+		// tip := grande{}
+		// return producto{tip, nombre, precio}
+	case "Mediano":
+		// tip := mediano{}
+		// return producto{tip, nombre, precio}
+	case "Chico":
+		// tip := chico{}
+		// return producto{tip, nombre, precio}
+	}
+	return producto{}
+}
+
+func NuevaTienda() Ecommerce {
+	return &tienda{}
+}
+
+func main() {
+	ecom := NuevaTienda()
+	ecom.Agregar(nuevoProducto("Chico", "coca", 100.0))
+	fmt.Printf("Tienda 1: Total: %.2f\n", ecom.Total())
+	// ecom.Agregar(nuevoProducto("Mediano", "arroz", 100.0))
+	// fmt.Printf("Tienda 2: Total: %.2f\n", ecom.Total())
+	// ecom.Agregar(nuevoProducto("Grande", "leche", 100.0))
+	// fmt.Printf("Tienda 2: Total: %.2f\n", ecom.Total())
+
+	ecom2 := NuevaTienda()
+	// ecom2.Agregar(nuevoProducto("Chico", "coca", 100.0))
+	// fmt.Printf("Tienda 1: Total: %.2f\n", ecom2.Total())
+	ecom2.Agregar(nuevoProducto("Mediano", "arroz", 100.0))
+	fmt.Printf("Tienda 2: Total: %.2f\n", ecom2.Total())
+	// ecom2.Agregar(nuevoProducto("Grande", "leche", 100.0))
+	// fmt.Printf("Tienda 2: Total: %.2f\n", ecom2.Total())
+
+	ecom3 := NuevaTienda()
+	// ecom3.Agregar(nuevoProducto("Chico", "coca", 100.0))
+	// fmt.Printf("Tienda 1: Total: %.2f\n", ecom3.Total())
+	// ecom3.Agregar(nuevoProducto("Mediano", "arroz", 100.0))
+	// fmt.Printf("Tienda 2: Total: %.2f\n", ecom3.Total())
+	ecom3.Agregar(nuevoProducto("Grande", "leche", 100.0))
+	fmt.Printf("Tienda 2: Total: %.2f\n", ecom3.Total())
+}
