abd8b810bf603f5316e1f1d9580b24753692d323
estefaniaPecora <barbara.pecora@mercadolibre.com>
2021-11-29 13:27:41 -0300

    TM_goweb1_exercise3

--- 6_goweb1/web_server/main.go
+++ 6_goweb1/web_server/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,15 +19,35 @@ type Products struct {
 	Created   string  `json:"created"`
 }
 
+///// FUNCIONES HANDLERS ///////
+
+func sayHello(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello Estefi!",
+	})
+}
+
+func getAll(c *gin.Context) {
+
+	var prodList []Products
+	readProducts, _ := os.ReadFile("./products.json")
+
+	if err := json.Unmarshal([]byte(readProducts), &prodList); err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(200, gin.H{
+		"data": prodList,
+	})
+}
+
 func main() {
 
 	router := gin.Default()
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello Estefi!",
-		})
-	})
+	router.GET("/hello", sayHello)
+
+	router.GET("/products", getAll)
 
 	router.Run()
 }
