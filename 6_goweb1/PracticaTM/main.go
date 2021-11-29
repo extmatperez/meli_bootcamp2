8db1b541a36f93b4bc7ead53d34a20c4b74a3de7
FacuBouza <facundo.bouza@mercadolibre.com>
2021-11-29 13:43:43 -0300

    Excercise finished

--- 6_goweb1/PracticaTM/main.go
+++ 6_goweb1/PracticaTM/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,13 +12,13 @@ type Fecha struct {
 	Dia, Mes, Anio int
 }
 type Transaccion struct {
-	Id             int
-	CodTransaccion string
-	Moneda         string
-	Monto          float64
-	Emisor         string
-	Receptor       string
-	FechaTrans     string
+	Id             int     `json:"id"`
+	CodTransaccion string  `json:"cod_transaccion"`
+	Moneda         string  `json:"moneda"`
+	Monto          float64 `json:"monto"`
+	Emisor         string  `json:"emisor"`
+	Receptor       string  `json:"receptor"`
+	FechaTrans     string  `json:"fecha_trans"`
 }
 
 func main() {
@@ -28,5 +30,24 @@ func main() {
 		})
 	})
 
-	router.Run(":8081")
+	data, err := os.ReadFile("./transactions.json")
+
+	if err != nil {
+		panic("error abriendo el archivo")
+	}
+	var transacciones []Transaccion
+
+	err = json.Unmarshal(data, &transacciones)
+
+	if err != nil {
+		panic("error haciendo el unmarshal")
+	}
+
+	router.GET("/transacciones", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"GetAll": transacciones,
+		})
+	})
+
+	router.Run()
 }
