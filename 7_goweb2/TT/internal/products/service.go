067ee47c6b5f865718c0ec620f80e69d5c096538
Matias Ziliotto <matias.ziliotto@mercadolibre.com>
2021-11-30 18:18:54 -0300

    Add some filters to Ejercicio1-TT

--- 7_goweb2/TT/internal/products/service.go
+++ 7_goweb2/TT/internal/products/service.go
@@ -1,9 +1,16 @@
 package internal
 
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 type Service interface {
 	GetAll() ([]Product, error)
 	Store(name string, color string, stock int, code string, published bool, created_at string) (Product, error)
 	FindById(id int64) (Product, error)
+	FilterProducts(allProducts []Product, queryParams map[string]string) []Product
 }
 
 type service struct {
@@ -44,6 +51,51 @@ func (s *service) FindById(id int64) (Product, error) {
 	}
 
 	return product, nil
+}
+
+func (s *service) FilterProducts(allProducts []Product, queryParams map[string]string) []Product {
+	var fields []string
+
+	//TODO: at the moment only filter strings
+	fields = append(fields, "name", "color", "code")
+
+	productsFiltered := allProducts
+
+	for _, field := range fields {
+		if len(queryParams[field]) != 0 && len(productsFiltered) != 0 {
+			productsFiltered = filterProductsByField(productsFiltered, field, queryParams[field])
+		}
+	}
+
+	return productsFiltered
+}
+
+func filterProductsByField(productsToFilter []Product, field, fieldValue string) []Product {
+	var filteredProducts []Product
+	var product Product
+
+	productTypeOf := reflect.TypeOf(product)
+	fieldIndex := 0
+
+	for fieldIndex = 0; fieldIndex < productTypeOf.NumField(); fieldIndex++ {
+		if strings.ToLower(productTypeOf.Field(fieldIndex).Name) == field {
+			break
+		}
+	}
+
+	// If index is out of range (field not found), return the same slice of products
+	if fieldIndex == productTypeOf.NumField() {
+		return productsToFilter
+	}
+
+	for _, productLoop := range productsToFilter {
+		productFieldVal := fmt.Sprintf("%v", reflect.ValueOf(productLoop).Field(fieldIndex).Interface())
+		if strings.Contains(strings.ToLower(productFieldVal), strings.ToLower(fieldValue)) {
+			filteredProducts = append(filteredProducts, productLoop)
+		}
+	}
+
+	return filteredProducts
 }
 
 func NewService(r Repository) Service {
