546a1cf3aa7f8dda6d456831a13faf53bfb1af94
estefaniaPecora <barbara.pecora@mercadolibre.com>
2021-11-30 09:44:44 -0300

    TT_goweb_exercise2

--- 6_goweb1/web_server/main.go
+++ 6_goweb1/web_server/main.go
@@ -10,12 +10,12 @@ import (
 )
 
 type Products struct {
-	ID        int     `json:"id"`
+	ID        int64   `json:"id"`
 	Name      string  `json:"name"`
 	Color     string  `json:"color"`
 	Price     float64 `json:"price"`
-	Stock     int     `json:"stock"`
-	Code      int     `json:"code"`
+	Stock     int64   `json:"stock"`
+	Code      int64   `json:"code"`
 	Published string  `json:"published"`
 	Created   string  `json:"created"`
 }
@@ -47,57 +47,146 @@ func getAll(c *gin.Context) {
 		"data": prodList,
 	})
 }
-
-func getbyName(c *gin.Context) {
+func getbyFilter(c *gin.Context) {
 
 	var prodList = readData()
 	var filtrados []Products
 
-	for _, p := range prodList {
-		if c.Query("name") == p.Name {
-			filtrados = append(filtrados, p)
+	selectedFilter := c.Request.URL.Query().Get("filterValue")
+	switch c.Request.URL.Query().Get("filter") {
+	case "name":
+		for _, p := range prodList {
+			if selectedFilter == p.Name {
+				filtrados = append(filtrados, p)
+			}
 		}
-	}
 
-	c.JSON(200, gin.H{
-		"data": filtrados,
-	})
+		c.JSON(200, gin.H{
+			"data": filtrados,
+		})
+	case "color":
+		for _, p := range prodList {
+			if selectedFilter == p.Color {
+				filtrados = append(filtrados, p)
+			}
+		}
+		c.JSON(200, gin.H{
+			"data": filtrados,
+		})
+	case "price":
+		i, _ := strconv.ParseFloat(c.Query(selectedFilter), 64)
+
+		for _, p := range prodList {
+			if i == p.Price {
+				filtrados = append(filtrados, p)
+			}
+		}
+		c.JSON(200, gin.H{
+			"data": filtrados,
+		})
+	case "stock":
+		i, _ := strconv.ParseInt(c.Query(selectedFilter), 10, 64)
+
+		for _, p := range prodList {
+			if i == p.Stock {
+				filtrados = append(filtrados, p)
+			}
+		}
+		c.JSON(200, gin.H{
+			"data": filtrados,
+		})
+	case "code":
+		i, _ := strconv.ParseInt(c.Query(selectedFilter), 10, 64)
+
+		for _, p := range prodList {
+			if i == p.Code {
+				filtrados = append(filtrados, p)
+			}
+		}
+		c.JSON(200, gin.H{
+			"data": filtrados,
+		})
+	case "published":
+		for _, p := range prodList {
+			if selectedFilter == p.Published {
+				filtrados = append(filtrados, p)
+			}
+		}
+		c.JSON(200, gin.H{
+			"data": filtrados,
+		})
+
+	}
 
 }
 
-func getbyColor(c *gin.Context) {
+// func getbyName(c *gin.Context) {
+
+// 	var prodList = readData()
+// 	var filtrados []Products
+
+// 	for _, p := range prodList {
+// 		if c.Query("name") == p.Name {
+// 			filtrados = append(filtrados, p)
+// 		}
+// 	}
+
+// 	c.JSON(200, gin.H{
+// 		"data": filtrados,
+// 	})
+
+// }
+
+// func getbyColor(c *gin.Context) {
+
+// 	var prodList = readData()
+// 	var filtrados []Products
+
+// 	for _, p := range prodList {
+// 		if c.Query("color") == p.Color {
+// 			filtrados = append(filtrados, p)
+// 		}
+// 	}
+
+// 	c.JSON(200, gin.H{
+// 		"data": filtrados,
+// 	})
+
+// }
+
+// func getbyPrice(c *gin.Context) {
+
+// 	var prodList = readData()
+// 	var filtrados []Products
+
+// 	for _, p := range prodList {
+// 		i, _ := strconv.ParseFloat(c.Query("price"), 64)
+// 		if i == p.Price {
+// 			filtrados = append(filtrados, p)
+// 		}
+// 	}
+
+// 	c.JSON(200, gin.H{
+// 		"data": filtrados,
+// 	})
+
+// }
+
+func getbyId(c *gin.Context) {
 
 	var prodList = readData()
-	var filtrados []Products
+	var selectedProd []Products
+
+	prodId, _ := strconv.ParseInt(c.Param("id"), 10, 64)
 
 	for _, p := range prodList {
-		if c.Query("color") == p.Color {
-			filtrados = append(filtrados, p)
+		if prodId == p.ID {
+			selectedProd = append(selectedProd, p)
 		}
 	}
-
 	c.JSON(200, gin.H{
-		"data": filtrados,
+		"data": selectedProd,
 	})
-
-}
-
-func getbyPrice(c *gin.Context) {
-
-	var prodList = readData()
-	var filtrados []Products
-
-	for _, p := range prodList {
-		i, _ := strconv.ParseFloat(c.Query("price"), 64)
-		if i == p.Price {
-			filtrados = append(filtrados, p)
-		}
-	}
-
-	c.JSON(200, gin.H{
-		"data": filtrados,
-	})
-
 }
 
 func main() {
@@ -106,16 +195,18 @@ func main() {
 
 	router.GET("/", sayHello)
 	router.GET("/products", getAll)
+	router.GET("/products/:id", getbyId)
+	router.GET("/products/filter/select", getbyFilter)
 
-	productfilter := router.Group("/productfilter")
-	{
-		productfilter.GET("/name", getbyName)
-		productfilter.GET("/color", getbyColor)
-		productfilter.GET("/price", getbyPrice)
-		// 	productfilter.GET("/stock", getbyStock)
-		// 	productfilter.GET("/code", getbyCode)
-		// 	productfilter.GET("/published", getbyPublished)
-	}
+	// productfilter := router.Group("/productfilter")
+	// {
+	// 	productfilter.GET("/name", getbyName)
+	// 	productfilter.GET("/color", getbyColor)
+	// 	productfilter.GET("/price", getbyPrice)
+	// 	productfilter.GET("/stock", getbyStock)
+	// 	productfilter.GET("/code", getbyCode)
+	// 	productfilter.GET("/published", getbyPublished)
+	// }
 
 	router.Run()
 }
