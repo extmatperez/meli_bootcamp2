f32ff09efcc56aa790fdf01efcc42de0b6473b91
ivanArevalo1997 <ivan.aarevalo@mercadolibre.com>
2021-11-29 21:41:41 -0300

    improved

--- go_web/main.go
+++ go_web/main.go
@@ -5,22 +5,23 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Transaction struct {
-	ID              int    `json:"id"`
-	TransactionCode string `json:"transaction_code"`
-	Coin            string `json:"coin"`
-	Amount          string `json:"amount"`
-	Emitor          string `json:"emitor"`
-	Receptor        string `json:"receptor"`
-	TransactionDate string `json:"transaction_date"`
+	ID               int     `form:"id", json:"id"`
+	Transaction_Code string  `form:"transaction_code", json:"transaction_code"`
+	Coin             string  `form:"coin", json:"coin"`
+	Amount           float64 `form:"amount", json:"amount"`
+	Emitor           string  `form:"emitor", json:"emitor"`
+	Receptor         string  `form:"receptor", json:"receptor"`
+	Transaction_Date string  `form:"transaction_date", json:"transaction_date"`
 }
 
-func GetAll(file string) []Transaction {
-	data, err := os.ReadFile(file)
+func TransactionsGetter() []Transaction {
+	data, err := os.ReadFile("./transactions.json")
 
 	var TransactionList []Transaction
 
@@ -33,19 +34,176 @@ func GetAll(file string) []Transaction {
 }
 
 func main() {
-
 	router := gin.Default()
 
-	router.GET("/hola", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hola ivan",
-		})
-	})
-
-	router.GET("/transactions", func(c *gin.Context) {
-		c.JSON(http.StatusOK, GetAll("./transactions.json"))
-	})
-
+	router.GET("/hola/:nombre", Greetings)
+	router.GET("/transactions", GetAll)
+	router.GET("/filtrar", FilterQuery)
+	router.GET("/transaction/:id", GetOne)
 	router.Run(":8080")
 
+}
+
+func Greetings(ctx *gin.Context) {
+	name := ctx.Param("name")
+
+	if name != "" {
+		ctx.String(200, "Hola %v!!!", name)
+	} else {
+		ctx.String(200, "Hola anonimo!")
+	}
+}
+func GetAll(ctx *gin.Context) {
+	transactions := TransactionsGetter()
+	ctx.JSON(http.StatusOK, transactions)
+}
+
+func FilterQuery(ctx *gin.Context) {
+	filter := ctx.Query("filter")
+	value := ctx.Query("value")
+	//header := ctx.Request.Body
+
+	//for k, v := range header {
+	fmt.Printf("%v\n", filter)
+	fmt.Printf("%v\n", value)
+
+	//}
+
+	var TransactionList []Transaction
+
+	switch filter {
+	case "transaction_code":
+		TransactionList = SearchByTransactionCode(value)
+		ctx.JSON(http.StatusOK, TransactionList)
+	case "coin":
+		TransactionList = SearchByCoin(value)
+		ctx.JSON(http.StatusOK, TransactionList)
+	case "amount":
+		TransactionList = SearchInRangeAmount(value)
+		ctx.JSON(http.StatusOK, TransactionList)
+	case "emitor":
+		TransactionList = SearchEmitor(value)
+		ctx.JSON(http.StatusOK, TransactionList)
+	case "receptor":
+		TransactionList = SearchReceptor(value)
+		ctx.JSON(http.StatusOK, TransactionList)
+	case "transaction_date":
+		TransactionList = SearchDate(value)
+		ctx.JSON(http.StatusOK, TransactionList)
+	default:
+		ctx.String(400, "Invalid Query")
+	}
+}
+
+func SearchReceptor(receptor string) []Transaction {
+	var TransactionList []Transaction = TransactionsGetter()
+
+	var transactionToReceptor []Transaction
+
+	for _, tx := range TransactionList {
+		if receptor == tx.Receptor {
+			transactionToReceptor = append(transactionToReceptor, tx)
+		}
+	}
+
+	return transactionToReceptor
+}
+
+func SearchByCoin(coin string) []Transaction {
+	var TransactionList []Transaction = TransactionsGetter()
+
+	var transactionsByCoin []Transaction
+
+	for _, tx := range TransactionList {
+		if coin == tx.Coin {
+			transactionsByCoin = append(transactionsByCoin, tx)
+		}
+	}
+
+	return transactionsByCoin
+}
+
+func SearchEmitor(emitor string) []Transaction {
+	var TransactionList []Transaction = TransactionsGetter()
+
+	var transactionFromEmitor []Transaction
+
+	for _, tx := range TransactionList {
+		if emitor == tx.Emitor {
+			transactionFromEmitor = append(transactionFromEmitor, tx)
+		}
+	}
+
+	return transactionFromEmitor
+}
+
+func SearchInRangeAmount(amount string) []Transaction {
+	amountFloat, _ := strconv.ParseFloat(amount, 64)
+	var TransactionList []Transaction = TransactionsGetter()
+
+	var transactionsBetweenAmount []Transaction
+
+	for _, tx := range TransactionList {
+		if amountFloat == tx.Amount {
+			transactionsBetweenAmount = append(transactionsBetweenAmount, tx)
+		}
+	}
+
+	return transactionsBetweenAmount
+}
+
+func SearchDate(date string) []Transaction {
+	var TransactionList []Transaction = TransactionsGetter()
+
+	var transactionsByDate []Transaction
+
+	for _, tx := range TransactionList {
+		if date == tx.Transaction_Date {
+			transactionsByDate = append(transactionsByDate, tx)
+		}
+	}
+
+	return transactionsByDate
+}
+
+func SearchByTransactionCode(code string) []Transaction {
+	var TransactionList []Transaction = TransactionsGetter()
+
+	var transactionsByCode []Transaction
+
+	for _, tx := range TransactionList {
+		if code == tx.Transaction_Code {
+			transactionsByCode = append(transactionsByCode, tx)
+		}
+	}
+
+	return transactionsByCode
+}
+
+func GetOne(ctx *gin.Context) {
+	var TransactionList []Transaction = TransactionsGetter()
+
+	var TransactionFound Transaction
+
+	idSearched, err := strconv.Atoi(ctx.Param("id"))
+
+	if err != nil {
+		ctx.String(500, "Error")
+	}
+
+	finded := false
+
+	for _, tx := range TransactionList {
+		if tx.ID == idSearched {
+			TransactionFound = tx
+			finded = true
+			break
+		}
+	}
+
+	if finded {
+		ctx.JSON(http.StatusOK, TransactionFound)
+	} else {
+		ctx.String(400, "No se encontro una transaccion con el id: %v", idSearched)
+	}
 }
