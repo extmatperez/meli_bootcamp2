7e7e57933edb28d8b63e0f2986695419001eaf0e
rbarra <ramiro.barra@mercadolibre.com>
2021-11-29 22:07:22 -0300

    add endpoint

--- 6_goweb1/turno_manana/goWeb1/main.go
+++ 6_goweb1/turno_manana/goWeb1/main.go
@@ -1,6 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Users struct {
+	ID           int    `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Email        string `json:"email"`
+	Age          int    `json:"age"`
+	Height       int    `json:"height"`
+	Active       bool   `json:"active"`
+	CreationDate string `json:"creation_date"`
+}
 
 func main() {
 
@@ -11,9 +28,24 @@ func main() {
 			"message": "hola Ramiro",
 		})
 	}) */
-	router.GET("/users/:name", func(c *gin.Context) {
+	/* router.GET("/users/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.String(200, "hello %s", name)
+	}) */
+	router.GET("/users", func(c *gin.Context) {
+		var userArr []Users
+		readUsers, err := os.ReadFile("./users.json")
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			json.Unmarshal(readUsers, &userArr)
+		}
+		c.JSON(200, gin.H{
+			"users": userArr,
+		})
+		fmt.Println(userArr)
+		fmt.Println(string(readUsers))
 	})
 
 	//inicializamos el router
