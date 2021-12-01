f1e9a3e7f4b42fea4bf0e33bd3b7e6acf9cce0f9
jpesciemeli <juan.pescie@mercadolibre.com>
2021-12-01 01:35:50 -0300

    create router with controller applied

---
+++ 7_goweb2/ejTT/cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/extmatperez/meli_bootcamp2/tree/pescie_juan/7_goweb2/ejTT/cmd/server/handler"
+	internal "github.com/extmatperez/meli_bootcamp2/tree/pescie_juan/7_goweb2/ejTT/internal/productos"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	repository := internal.NewRepository()
+	service := internal.NewService(repository)
+	producto := handler.NewProducto(service)
+	router.GET("/producto", producto.GetAll())
+	router.POST("/producto", producto.Store())
+
+	router.Run()
+
+}
---
+++ 7_goweb2/ejTT/cmd/server/handler/producto.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	internal "github.com/extmatperez/meli_bootcamp2/tree/pescie_juan/7_goweb2/ejTT/internal/productos"
+	"github.com/gin-gonic/gin"
+)
+
+type request struct {
+	Nombre         string  `json:"nombre"`
+	Color          string  `json:"color" `
+	Precio         float64 `json:"precio" `
+	Stock          int     `json:"stock" `
+	Codigo         string  `json:"codigo"`
+	Publicado      bool    `json:"publicado"`
+	Fecha_creacion string  `json:"fecha_creacion"`
+}
+type Producto struct {
+	service internal.Service
+}
+
+func NewProducto(serv internal.Service) *Producto {
+	return &Producto{service: serv}
+}
+
+func (p *Producto) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		products, err := p.service.GetAll()
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		ctx.JSON(200, products)
+	}
+
+}
+
+func (p *Producto) Store() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var prod request
+
+		err := ctx.ShouldBindJSON(&prod)
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"error": err,
+			})
+		}
+
+		product, e := p.service.Store(prod.Nombre, prod.Color, prod.Precio, prod.Stock, prod.Codigo, prod.Publicado, prod.Fecha_creacion)
+		if e != nil {
+			ctx.JSON(400, gin.H{
+				"error": e,
+			})
+			return
+		}
+		ctx.JSON(200, product)
+	}
+
+}
