5eeca6a5d329aed212208a7c508d66bade284a45
rbarra <ramiro.barra@mercadolibre.com>
2021-12-01 20:56:55 -0300

    execise complete

--- 7_goweb2/turno_manana/ejercicio_3/main.go
+++ 7_goweb2/turno_manana/ejercicio_3/main.go
@@ -1,3 +1,13 @@
+/* Ejercicio 1 - Crear Entidad
+Se debe implementar la funcionalidad para crear la entidad. para eso se deben seguir los
+siguientes pasos:
+1. Crea un endpoint mediante POST el cual reciba la entidad.
+2. Se debe tener un array de la entidad en memoria (a nivel global), en el cual se
+deberán ir guardando todas las peticiones que se vayan realizando.
+3. Al momento de realizar la petición se debe generar el ID. Para generar el ID se debe
+buscar el ID del último registro generado, incrementarlo en 1 y asignarlo a nuestro
+nuevo registro (sin tener una variable de último ID a nivel global). */
+
 /* Ejercicio 2 - Validación de campos
 Se debe implementar las validaciones de los campos al momento de enviar la petición, para
 eso se deben seguir los siguientes pasos:
@@ -6,6 +16,14 @@ requeridos
 2. En caso que algún campo no esté completo se debe retornar un código de error 400
 con el mensaje “el campo %s es requerido”.
 (En %s debe ir el nombre del campo que no está completo). */
+
+/* Ejercicio 3 - Validar Token
+Para agregar seguridad a la aplicación se debe enviar la petición con un token, para eso se
+deben seguir los siguientes pasos::
+1. Al momento de enviar la petición se debe validar que un token sea enviado
+2. Se debe validar ese token en nuestro código (el token puede estar hardcodeado).
+3. En caso que el token enviado no sea correcto debemos retornar un error 401 y un
+mensaje que diga “no tiene permisos para realizar la petición solicitada”. */
 
 package main
 
@@ -80,23 +98,30 @@ func validate_fields(user_id Users) string {
 // Add new user to json file
 func post_users(c *gin.Context) {
 	var user_id Users
+	token := c.GetHeader("token")
+	value := "12345678"
 
-	err := c.ShouldBindJSON(&user_id)
-	last_id := user[len(user)-1].ID + 1
-	user_id.ID = last_id
-	validate := validate_fields(user_id)
+	if token == value {
+		err := c.ShouldBindJSON(&user_id)
+		last_id := user[len(user)-1].ID + 1
+		user_id.ID = last_id
+		validate := validate_fields(user_id)
 
-	if validate != "" {
-		c.String(http.StatusBadRequest, validate)
-		return
-	}
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
+		if validate != "" {
+			c.String(http.StatusBadRequest, validate)
+			return
+		}
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			user = append(user, user_id)
+			c.JSON(http.StatusOK, user_id)
+		}
 	} else {
-		user = append(user, user_id)
-		c.JSON(http.StatusOK, user_id)
+		fmt.Println(http.StatusBadRequest, "Token not valid, please try again!")
+		c.JSON(401, "Token not valid, please try again!")
 	}
 }
 
