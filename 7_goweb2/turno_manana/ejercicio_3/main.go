d216e25421f9a32d2dbb547f9dea2b8b2219218e
rbarra <ramiro.barra@mercadolibre.com>
2021-12-01 20:59:59 -0300

    exercise 3 finished

--- 7_goweb2/turno_manana/ejercicio_3/main.go
+++ 7_goweb2/turno_manana/ejercicio_3/main.go
@@ -1,22 +1,3 @@
-/* Ejercicio 1 - Crear Entidad
-Se debe implementar la funcionalidad para crear la entidad. para eso se deben seguir los
-siguientes pasos:
-1. Crea un endpoint mediante POST el cual reciba la entidad.
-2. Se debe tener un array de la entidad en memoria (a nivel global), en el cual se
-deberán ir guardando todas las peticiones que se vayan realizando.
-3. Al momento de realizar la petición se debe generar el ID. Para generar el ID se debe
-buscar el ID del último registro generado, incrementarlo en 1 y asignarlo a nuestro
-nuevo registro (sin tener una variable de último ID a nivel global). */
-
-/* Ejercicio 2 - Validación de campos
-Se debe implementar las validaciones de los campos al momento de enviar la petición, para
-eso se deben seguir los siguientes pasos:
-1. Se debe validar todos los campos enviados en la petición, todos los campos son
-requeridos
-2. En caso que algún campo no esté completo se debe retornar un código de error 400
-con el mensaje “el campo %s es requerido”.
-(En %s debe ir el nombre del campo que no está completo). */
-
 /* Ejercicio 3 - Validar Token
 Para agregar seguridad a la aplicación se debe enviar la petición con un token, para eso se
 deben seguir los siguientes pasos::
