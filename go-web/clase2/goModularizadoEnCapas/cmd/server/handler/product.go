9e46aee55287f023124c1b3dfaaed850920fdfab
Damian Pablo Zamora <dazamora@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-12-01 13:46:39 -0300

    ModularizadoPUT

--- go-web/clase2/goModularizadoEnCapas/cmd/server/handler/product.go
+++ go-web/clase2/goModularizadoEnCapas/cmd/server/handler/product.go
@@ -1,25 +1,31 @@
 package handler
 
 import (
-	personas "github.com/extmatperez/w2GoPrueba/GoWeb/Clase2TT/proyecto/internal/product"
+	"strconv"
+
+	productos "github.com/extmatperez/meli_bootcamp2/tree/zamora_damian/go-web/clase2/goModularizadoEnCapas/Internal/productos"
 	"github.com/gin-gonic/gin"
 )
 
-type request struct {
-	Nombre   string `json:"nombre"`
-	Apellido string `json:"apellido"`
-	Edad     int    `json:"edad"`
+type product struct {
+	Nombre        string `json:"nombre"`
+	Color         string `json:"color"`
+	Precio        string `json:"precio"`
+	Stock         string `json:"stock"`
+	Codigo        string `json:"codigo"`
+	Publicado     bool   `json:"publicado"`
+	FechaCreacion string `json:"fechaCreacion"`
 }
 
-type Persona struct {
-	service personas.Service
+type Producto struct {
+	service productos.Service
 }
 
-func NewPersona(ser personas.Service) *Persona {
-	return &Persona{service: ser}
+func NewPersona(ser productos.Service) *Producto {
+	return &Producto{service: ser}
 }
 
-func (per *Persona) GetAll() gin.HandlerFunc {
+func (per *Producto) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		personas, err := per.service.GetAll()
 
@@ -31,17 +37,37 @@ func (per *Persona) GetAll() gin.HandlerFunc {
 	}
 }
 
-func (controller *Persona) Store() gin.HandlerFunc {
+func (controller *Producto) Store() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
-		var perso request
+		var producto productos.Product
 
-		err := ctx.ShouldBindJSON(&perso)
+		err := ctx.ShouldBindJSON(&producto)
 
 		if err != nil {
 			ctx.String(400, "Hubo un error al querer cargar una persona %v", err)
 		} else {
-			response, err := controller.service.Store(perso.Nombre, perso.Apellido, perso.Edad)
+			response, err := controller.service.Store(producto)
+			if err != nil {
+				ctx.String(400, "No se pudo cargar la persona %v", err)
+			} else {
+				ctx.JSON(200, response)
+			}
+		}
+
+	}
+}
+
+func (controller *Producto) Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		var productoAux productos.Product
+		err := ctx.ShouldBindJSON(&productoAux)
+		varID, _ := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.String(400, "Hubo un error al querer cargar una persona %v", err)
+		} else {
+			response, err := controller.service.Update(varID, productoAux)
 			if err != nil {
 				ctx.String(400, "No se pudo cargar la persona %v", err)
 			} else {
--- go-web/clase2/goModularizadoEnCapas/cmd/server/main.go
+++ go-web/clase2/goModularizadoEnCapas/cmd/server/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"github.com/extmatperez/tree/zamora_damian/go-web/clase2/goModularizadoEnCapas/cmd/server/handler"
-	personas "github.com/extmatperez/tree/zamora_damian/go-web/clase2/goModularizadoEnCapas/cmd/server/handler"
+	productos "github.com/extmatperez/meli_bootcamp2/tree/zamora_damian/go-web/clase2/goModularizadoEnCapas/Internal/productos"
+	"github.com/extmatperez/meli_bootcamp2/tree/zamora_damian/go-web/clase2/goModularizadoEnCapas/cmd/server/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	repo := personas.NewRepository()
-	service := personas.NewService(repo)
+	repo := productos.NewRepository()
+	service := productos.NewService(repo)
 	controller := handler.NewPersona(service)
 
 	router.GET("/personas/get", controller.GetAll())
 	router.POST("/personas/add", controller.Store())
-
+	router.PUT("/modificar/:id", controller.Update())
+	//router.PATCH("/modificarNombre/:id", controller.UpdateName())
+	//router.DELETE("/delete/:id", controller.delete())
 	router.Run()
 }
