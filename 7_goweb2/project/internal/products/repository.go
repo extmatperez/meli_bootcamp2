9efa5d7329d168112743f6ddc7cd0b0a70092d79
Franco Julian Andrada Arganaraz <fandrada@ar0c02g310pml85.fibertel.com.ar>
2021-12-01 12:25:06 -0300

    feat(7_goweb2/project): struct project

---
+++ 7_goweb2/project/internal/products/repository.go
@@ -0,0 +1,42 @@
+package internal
+
+type Products struct {
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	Color        string `json:"color"`
+	Price        string `json:"price"`
+	Stock        string `json:"stock"`
+	Code         string `json:"code"`
+	Published    bool   `json:"published"`
+	CreationDate string `json:"creation_date"`
+}
+
+var products []Products
+var lastID int
+
+type Repository interface {
+	GetAll() ([]Products, error)
+	Store(id int, name string, color string, price string, stock string, code string, published bool, creationdate string) (Products, error)
+	LastID() (int, error)
+}
+
+type repository struct{}
+
+func NewRepository() Repository {
+	return &repository{}
+}
+
+func (r *repository) GetAll() ([]Products, error) {
+	return products, nil
+}
+
+func (r *repository) Store(id int, name string, color string, price string, stock string, code string, published bool, creationdate string) (Products, error) {
+	prod := Products{id, name, color, price, stock, code, published, creationdate}
+	products = append(products, prod)
+	lastID = prod.ID
+	return prod, nil
+}
+
+func (r *repository) LastID() (int, error) {
+	return lastID, nil
+}
