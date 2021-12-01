8bf05f81caee3ed97ea8e173e623d0684e7db9c3
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-01 10:29:15 -0300

    📝  Add Exercise_1 (separated)

---
+++ 7_goweb2/morning_activities/Exercise_1/main.go
@@ -0,0 +1,11 @@
+/* Ejercicio 1 - Crear Entidad
+Se debe implementar la funcionalidad para crear la entidad. para eso se deben seguir los
+siguientes pasos:
+1. Crea un endpoint mediante POST el cual reciba la entidad.
+2. Se debe tener un array de la entidad en memoria (a nivel global), en el cual se
+deberán ir guardando todas las peticiones que se vayan realizando.
+3. Al momento de realizar la petición se debe generar el ID. Para generar el ID se debe
+buscar el ID del último registro generado, incrementarlo en 1 y asignarlo a nuestro
+nuevo registro (sin tener una variable de último ID a nivel global). */
+
+package main
