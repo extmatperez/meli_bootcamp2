1a6f83a4744386ecf9ab6b55f3e1a7ab128c7b6b
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-01 10:33:18 -0300

    📝  Add Exercise_2 (Explanation)

--- 7_goweb2/morning_activities/Exercise_2/main.go
+++ 7_goweb2/morning_activities/Exercise_2/main.go
@@ -1,1 +1,10 @@
+/* Ejercicio 2 - Validación de campos
+Se debe implementar las validaciones de los campos al momento de enviar la petición, para
+eso se deben seguir los siguientes pasos:
+1. Se debe validar todos los campos enviados en la petición, todos los campos son
+requeridos
+2. En caso que algún campo no esté completo se debe retornar un código de error 400
+con el mensaje “el campo %s es requerido”.
+(En %s debe ir el nombre del campo que no está completo). */
+
 package main
