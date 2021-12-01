9a895902af730517a98dbb6161464e5bcbad4ea1
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-01 10:33:31 -0300

    📝  Add Exercise_3 (Explanation)

--- 7_goweb2/morning_activities/Exercise_3/main.go
+++ 7_goweb2/morning_activities/Exercise_3/main.go
@@ -1,1 +1,10 @@
+/* Ejercicio 3 - Validar Token
+Para agregar seguridad a la aplicación se debe enviar la petición con un token, para eso se
+deben seguir los siguientes pasos::
+
+1. Al momento de enviar la petición se debe validar que un token sea enviado
+2. Se debe validar ese token en nuestro código (el token puede estar hardcodeado).
+3. En caso que el token enviado no sea correcto debemos retornar un error 401 y un
+mensaje que “no tiene permisos para realizar la petición solicitada”. */
+
 package main
