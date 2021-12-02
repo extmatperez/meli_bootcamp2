cb4e1d0ddebd6253faa295793120e184b6f7c615
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-02 05:55:24 -0300

    📝  Add request struct

--- 7_goweb2/afternoon_activities/Exercise_1/cmd/server/handler/users.go
+++ 7_goweb2/afternoon_activities/Exercise_1/cmd/server/handler/users.go
@@ -1,2 +1,13 @@
 // Agregamos el package handler
 package handler
+
+// Creamos la request struct
+type request struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Age       int    `json:"age"`
+	Height    int    `json:"height"`
+	Active    bool   `json:"active"`
+	Date      string `json:"date"`
+}
