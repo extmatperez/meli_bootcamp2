cac64fcf56c30ae39b1ba58c41c43e23309fdc14
Nahuel-Scerca <nahuel.scerca@mercadolibre.com>
2021-12-02 16:30:21 -0300

    GOWEB C4 Manejando respuestas claseTT

--- 8_goweb3/ClaseTM/ProyectoEstructura/cmd/server/handler/productoController.go
+++ 8_goweb3/ClaseTM/ProyectoEstructura/cmd/server/handler/productoController.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	producto "github.com/extmatperez/meli_bootcamp2/tree/scerca_nahuel/8_goweb3/ClaseTM/ProyectoEstructura/internal/producto"
+	"github.com/extmatperez/meli_bootcamp2/tree/scerca_nahuel/8_goweb3/ClaseTM/ProyectoEstructura/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -40,18 +41,19 @@ func NewProductoController(ser producto.ServiceProducto) *ProductoController {
 
 func (prodController *ProductoController) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+
 		validado, status, err := validarToken(ctx)
-		fmt.Println(validado, err, status)
+
 		if validado {
 			personas, err := prodController.service.GetAll()
 
 			if err != nil {
-				ctx.String(400, "Hubo un error %v", err)
+				ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error %v", err)))
 			} else {
-				ctx.JSON(200, personas)
+				ctx.JSON(200, web.NewResponse(200, personas, ""))
 			}
 		} else {
-			ctx.JSON(status, err.Error())
+			ctx.JSON(status, web.NewResponse(status, nil, fmt.Sprintf("Hubo un error %v", err)))
 		}
 	}
 }
@@ -66,18 +68,17 @@ func (prodController *ProductoController) Store() gin.HandlerFunc {
 			err := ctx.ShouldBindJSON(&produc)
 
 			if err != nil {
-				ctx.String(400, "Hubo un error al querer cargar una persona %v", err)
+				ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error al querer cargar una persona %v", err)))
 			} else {
 				response, err := prodController.service.Store(produc.Nombre, produc.Color, produc.Precio, produc.Stock, produc.Codigo, produc.Publicado, produc.FechaCreacion)
 				if err != nil {
-					ctx.String(400, "No se pudo cargar la persona %v", err)
+					ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("No se pudo cargar la persona %v", err)))
 				} else {
-					ctx.JSON(200, response)
+					ctx.JSON(200, web.NewResponse(200, response, ""))
 				}
 			}
 		} else {
-			ctx.JSON(status, err.Error())
-
+			ctx.JSON(status, web.NewResponse(status, nil, fmt.Sprintf("Hubo un error de autorizacion:  %v", err.Error())))
 		}
 	}
 }
@@ -99,20 +100,19 @@ func (prodController *ProductoController) Update() gin.HandlerFunc {
 					prod, err := prodController.service.Update(id, produc.Nombre, produc.Color, produc.Precio, produc.Stock, produc.Codigo, produc.Publicado, produc.FechaCreacion)
 
 					if err == nil {
-						ctx.JSON(200, prod)
+						ctx.JSON(200, web.NewResponse(200, prod, ""))
 					} else {
-						ctx.JSON(404, gin.H{
-							"message": err.Error(),
-						})
+						ctx.JSON(404, web.NewResponse(404, nil, fmt.Sprintf("Hubo un problema al updatear  %v", err.Error())))
+
 					}
 				} else {
-					ctx.String(400, "Error con los datos del body")
+					ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Error con los datos del body  %v", err.Error())))
 				}
 			} else {
-				ctx.String(400, "Error con el id")
+				ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Error con el id:  %v", err.Error())))
 			}
 		} else {
-			ctx.JSON(status, err.Error())
+			ctx.JSON(status, web.NewResponse(status, nil, fmt.Sprintf("Hubo un error de autorizacion:  %v", err.Error())))
 		}
 
 	}
@@ -129,17 +129,16 @@ func (prodController *ProductoController) Detele() gin.HandlerFunc {
 			if err == nil {
 				err := prodController.service.Delete(id)
 				if err == nil {
-					ctx.String(200, "Producto %d borrado exitosamente", id)
+					ctx.JSON(200, web.NewResponse(200, fmt.Sprintf("Producto %d borrado exitosamente", id), ""))
+
 				} else {
-					ctx.JSON(400, gin.H{
-						"message": err.Error(),
-					})
+					ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error al borrar:  %v", err.Error())))
 				}
 			} else {
-				ctx.String(400, "Error con el id")
+				ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Error con el id:  %v", err.Error())))
 			}
 		} else {
-			ctx.JSON(status, err.Error())
+			ctx.JSON(status, web.NewResponse(status, nil, fmt.Sprintf("Hubo un error de autorizacion:  %v", err.Error())))
 		}
 
 	}
@@ -157,24 +156,20 @@ func (prodController *ProductoController) UpdateNameAndPrice() gin.HandlerFunc {
 				if errorCast == nil {
 					productoMod, errorMod := prodController.service.UpdateNameAndPrice(id, prod.Nombre, prod.Precio)
 					if errorMod == nil {
-						ctx.JSON(200, gin.H{
-							"message":  "Campos Modificados correctamente",
-							"producto": productoMod,
-						})
+						ctx.JSON(200, web.NewResponse(200, fmt.Sprintf("campos modificados correctamente", productoMod), ""))
+
 					} else {
-						ctx.JSON(200, gin.H{
-							"message": errorMod.Error(),
-						})
+						ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error con el id:  %v", err.Error())))
 					}
 				} else {
-					ctx.String(200, "Hubo un error con body, asegurese de pasar bien los campos nombre y precio")
+					ctx.String(400, "Hubo un error con body, asegurese de pasar bien los campos nombre y precio")
 				}
 
 			} else {
-				ctx.String(200, "Hubo un error con el id")
+				ctx.JSON(404, web.NewResponse(404, nil, fmt.Sprintf("Hubo un error con el id:  %v", err.Error())))
 			}
 		} else {
-			ctx.JSON(status, err.Error())
+			ctx.JSON(status, web.NewResponse(status, nil, fmt.Sprintf("Hubo un error de autorizacion:  %v", err.Error())))
 		}
 
 	}
