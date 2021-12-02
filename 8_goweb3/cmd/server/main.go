8164559e70c758c34ecd9f8a4f3858ca3ac20b4b
jmiguelsoto <jose.ssoto@mercadolibre.com>
2021-12-02 12:17:23 -0300

    set up dotenv

--- 8_goweb3/cmd/server/main.go
+++ 8_goweb3/cmd/server/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/extmatperez/meli_bootcamp2/tree/soto_jose/8_goweb3/cmd/server/handler"
 	transactions "github.com/extmatperez/meli_bootcamp2/tree/soto_jose/8_goweb3/transactions"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("No se pudo abrir el archivo .env")
+	}
 	router := gin.Default()
 
 	repo := transactions.NewRepository()
