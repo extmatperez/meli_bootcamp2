eb7cf11086c0bf394d25c0545f7f07d059000330
benjaminconti <benjamin.conti@mercadolibre.com>
2021-12-02 16:53:16 -0300

    generic responses

--- 8_goweb3/TT/proyecto/cmd/server/handler/product.go
+++ 8_goweb3/TT/proyecto/cmd/server/handler/product.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 
 	productos "github.com/extmatperez/meli_bootcamp2/8_goweb3/TT/proyecto/internal/productos"
+	"github.com/extmatperez/meli_bootcamp2/8_goweb3/TT/proyecto/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,26 +32,30 @@ func (controller *Producto) GetAll() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token :=  c.GetHeader("token")
+		token := c.GetHeader("token")
 
-		if token == ""{
-			c.String(400, "Token no enviado")
+		if token == "" {
+			//c.String(400, "Token no enviado")
+			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
 			return
 		}
 
 		tokenENV := os.Getenv("TOKEN")
 
-		if token != tokenENV{
-			c.String(401, "Token inválido")
+		if token != tokenENV {
+			//c.String(401, "Token inválido")
+			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
 			return
 		}
 
 		productos, err := controller.service.GetAll()
 
 		if err != nil {
-			c.String(400, "Hubo un error: %v", err.Error())
+			//c.String(400, "Hubo un error: %v", err.Error())
+			c.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error: %v", err)))
 		} else {
-			c.JSON(200, productos)
+			//c.JSON(200, productos)
+			c.JSON(200, web.NewResponse(200, productos, ""))
 		}
 
 	}
@@ -59,10 +65,19 @@ func (controller *Producto) Store() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token :=  c.GetHeader("token")
+		token := c.GetHeader("token")
 
-		if token != "123"{
-			c.String(401, "Token inválido")
+		if token == "" {
+			//c.String(400, "Token no enviado")
+			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
+			return
+		}
+
+		tokenENV := os.Getenv("TOKEN")
+
+		if token != tokenENV {
+			//c.String(401, "Token inválido")
+			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
 			return
 		}
 
@@ -71,40 +86,48 @@ func (controller *Producto) Store() gin.HandlerFunc {
 		err := c.ShouldBindJSON(&nuevoProducto)
 
 		if err != nil {
-			c.String(400, "Hubo un error al recibir los datos en el body: %v", err.Error())
+			//c.String(400, "Hubo un error al recibir los datos en el body: %v", err.Error())
+			c.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error al recibir los datos en el body: %v", err.Error())))
 		} else {
 
 			if nuevoProducto.Nombre == "" {
-				c.String(400, "Error: el nombre es obligatorio")
+				//c.String(400, "Error: el nombre es obligatorio")
+				c.JSON(400, web.NewResponse(400, nil, "Error: el nombre es obligatorio"))
 				return
 			}
 
 			if nuevoProducto.Color == "" {
-				c.String(400, "Error: El color es obligatorio")
+				//c.String(400, "Error: el color es obligatorio")
+				c.JSON(400, web.NewResponse(400, nil, "Error: el color es obligatorio"))
 				return
 			}
 
 			if nuevoProducto.Precio == "" {
-				c.String(400, "Error: El precio es obligatorio")
+				//c.String(400, "Error: el precio es obligatorio")
+				c.JSON(400, web.NewResponse(400, nil, "Error: el precio es obligatorio"))
 				return
 			}
 
 			if nuevoProducto.Codigo == "" {
-				c.String(400, "Error: El stock es obligatorio")
+				//c.String(400, "Error: el stock es obligatorio")
+				c.JSON(400, web.NewResponse(400, nil, "Error: el stock es obligatorio"))
 				return
 			}
 
 			if nuevoProducto.FechaCreacion == "" {
-				c.String(400, "Error: La fecha de cración es obligatoria")
+				//c.String(400, "Error: la fecha de cración es obligatoria")
+				c.JSON(400, web.NewResponse(400, nil, "Error: la fecha de cración es obligatoria"))
 				return
 			}
 
 			response, err := controller.service.Store(nuevoProducto.Nombre, nuevoProducto.Color, nuevoProducto.Precio, nuevoProducto.Stock, nuevoProducto.Codigo, nuevoProducto.Publicado, nuevoProducto.FechaCreacion)
 
 			if err != nil {
-				c.String(400, "Hubo un error al guardar los datos: %v", err.Error())
+				//c.String(400, "Hubo un error al guardar los datos: %v", err.Error())
+				c.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error al guardar los datos: %v", err.Error())))
 			} else {
-				c.JSON(200, response)
+				//c.JSON(200, response)
+				c.JSON(200, web.NewResponse(200, response, ""))
 			}
 		}
 
@@ -115,10 +138,19 @@ func (controller *Producto) Update() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token :=  c.GetHeader("token")
+		token := c.GetHeader("token")
 
-		if token != "123"{
-			c.String(401, "Token inválido")
+		if token == "" {
+			//c.String(400, "Token no enviado")
+			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
+			return
+		}
+
+		tokenENV := os.Getenv("TOKEN")
+
+		if token != tokenENV {
+			//c.String(401, "Token inválido")
+			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
 			return
 		}
 
@@ -127,46 +159,55 @@ func (controller *Producto) Update() gin.HandlerFunc {
 		id, err := strconv.Atoi(c.Param("id"))
 
 		if err != nil {
-			c.String(400, "No se pudo leer el id por parámetro")
+			//c.String(400, "No se pudo leer el id por parámetro")
+			c.JSON(400, web.NewResponse(400, nil, "No se pudo leer el id por parámetro"))
 		} else {
 
 			err = c.ShouldBindJSON(&productoActualizado)
 
 			if err != nil {
-				c.String(400, "No se pudo leer el body")
+				//c.String(400, "Hubo un error al recibir los datos en el body: %v", err.Error())
+				c.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error al recibir los datos en el body: %v", err.Error())))
 			} else {
 
 				if productoActualizado.Nombre == "" {
-					c.String(400, "Error: el nombre es obligatorio")
+					//c.String(400, "Error: el nombre es obligatorio")
+					c.JSON(400, web.NewResponse(400, nil, "Error: el nombre es obligatorio"))
 					return
 				}
 
 				if productoActualizado.Color == "" {
-					c.String(400, "Error: El color es obligatorio")
+					//c.String(400, "Error: el color es obligatorio")
+					c.JSON(400, web.NewResponse(400, nil, "Error: el color es obligatorio"))
 					return
 				}
 
 				if productoActualizado.Precio == "" {
-					c.String(400, "Error: El precio es obligatorio")
+					//c.String(400, "Error: el precio es obligatorio")
+					c.JSON(400, web.NewResponse(400, nil, "Error: el precio es obligatorio"))
 					return
 				}
 
 				if productoActualizado.Codigo == "" {
-					c.String(400, "Error: El stock es obligatorio")
+					//c.String(400, "Error: el stock es obligatorio")
+					c.JSON(400, web.NewResponse(400, nil, "Error: el stock es obligatorio"))
 					return
 				}
 
 				if productoActualizado.FechaCreacion == "" {
-					c.String(400, "Error: La fecha de cración es obligatoria")
+					//c.String(400, "Error: la fecha de cración es obligatoria")
+					c.JSON(400, web.NewResponse(400, nil, "Error: la fecha de cración es obligatoria"))
 					return
 				}
 
 				productoActualizado, err := controller.service.Update(id, productoActualizado.Nombre, productoActualizado.Color, productoActualizado.Precio, productoActualizado.Stock, productoActualizado.Codigo, productoActualizado.Publicado, productoActualizado.FechaCreacion)
 
 				if err != nil {
-					c.String(404, err.Error())
+					//c.String(404, err.Error())
+					c.JSON(404, web.NewResponse(404, nil, err.Error()))
 				} else {
-					c.JSON(200, productoActualizado)
+					//c.JSON(200, productoActualizado)
+					c.JSON(200, web.NewResponse(200, productoActualizado, ""))
 				}
 
 			}
@@ -178,25 +219,37 @@ func (controller *Producto) Delete() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token :=  c.GetHeader("token")
+		token := c.GetHeader("token")
 
-		if token != "123"{
-			c.String(401, "Token inválido")
+		if token == "" {
+			//c.String(400, "Token no enviado")
+			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
+			return
+		}
+
+		tokenENV := os.Getenv("TOKEN")
+
+		if token != tokenENV {
+			//c.String(401, "Token inválido")
+			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
 			return
 		}
 
 		id, err := strconv.Atoi(c.Param("id"))
 
 		if err != nil {
-			c.String(400, "No se pudo leer el id por parámetro")
+			//c.String(400, "No se pudo leer el id por parámetro")
+			c.JSON(400, web.NewResponse(400, nil, "No se pudo leer el id por parámetro"))
 		} else {
 
 			err := controller.service.Delete(id)
 
 			if err != nil {
-				c.String(404, err.Error())
+				//c.String(404, err.Error())
+				c.JSON(404, web.NewResponse(404, nil, err.Error()))
 			} else {
-				c.String(200, "El producto %v fue eliminado", id)
+				//c.String(200, "El producto %v fue eliminado", id)
+				c.JSON(200, web.NewResponse(200, fmt.Sprintf("El producto %v fue eliminado", id), ""))
 			}
 
 		}
@@ -207,10 +260,19 @@ func (controller *Producto) UpdateNombrePrecio() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token :=  c.GetHeader("token")
+		token := c.GetHeader("token")
 
-		if token != "123"{
-			c.String(401, "Token inválido")
+		if token == "" {
+			//c.String(400, "Token no enviado")
+			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
+			return
+		}
+
+		tokenENV := os.Getenv("TOKEN")
+
+		if token != tokenENV {
+			//c.String(401, "Token inválido")
+			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
 			return
 		}
 
@@ -219,31 +281,37 @@ func (controller *Producto) UpdateNombrePrecio() gin.HandlerFunc {
 		id, err := strconv.Atoi(c.Param("id"))
 
 		if err != nil {
-			c.String(400, "No se pudo leer el id por parámetro")
+			//c.String(400, "No se pudo leer el id por parámetro")
+			c.JSON(400, web.NewResponse(400, nil, "No se pudo leer el id por parámetro"))
 		} else {
 
 			err = c.ShouldBindJSON(&productoActualizado)
 
 			if err != nil {
-				c.String(400, "No se pudo leer el body")
+				//c.String(400, "Hubo un error al recibir los datos en el body: %v", err.Error())
+				c.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error al recibir los datos en el body: %v", err.Error())))
 			} else {
 
 				if productoActualizado.Nombre == "" {
-					c.String(400, "Error: el nombre es obligatorio")
+					//c.String(400, "Error: el nombre es obligatorio")
+					c.JSON(400, web.NewResponse(400, nil, "Error: el nombre es obligatorio"))
 					return
 				}
 
 				if productoActualizado.Precio == "" {
-					c.String(400, "Error: El precio es obligatorio")
+					//c.String(400, "Error: el precio es obligatorio")
+					c.JSON(400, web.NewResponse(400, nil, "Error: el precio es obligatorio"))
 					return
 				}
 
 				productoActualizado, err := controller.service.UpdateNombrePrecio(id, productoActualizado.Nombre, productoActualizado.Precio)
 
 				if err != nil {
-					c.String(404, err.Error())
+					//c.String(404, err.Error())
+					c.JSON(404, web.NewResponse(404, nil, err.Error()))
 				} else {
-					c.JSON(200, productoActualizado)
+					//c.JSON(200, productoActualizado)
+					c.JSON(200, web.NewResponse(200, productoActualizado, ""))
 				}
 
 			}
