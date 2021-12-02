88be0385f1d3f2601ea12f98e63a45313707cc97
jpacostasc <juan.acosta@mercadolibre.com>
2021-12-02 15:41:32 -0300

    Copia del proyecto de goweb2

---
+++ 8_goweb3/proyecto/cmd/server/handler/transacciones.go
@@ -0,0 +1,146 @@
+package handler
+
+import (
+	"os"
+	"strconv"
+
+	transacciones "github.com/extmatperez/meli_bootcamp2/7_goweb3/proyecto/internal/transacciones"
+	"github.com/gin-gonic/gin"
+)
+
+
+type request struct {
+	CodigoTransaccion int     `json:"codigo_transaccion"`
+	Moneda            string  `json:"moneda"`
+	Monto             float64 `json:"monto"`
+	Emisor            string  `json:"emisor"`
+	Receptor          string  `json:"receptor"`
+	FechaTransaccion  string  `json:"fecha_transaccion"`
+}
+
+type Transaccion struct {
+	service transacciones.Service
+}
+
+func NewTransaccion(t transacciones.Service) *Transaccion {
+	return &Transaccion{service: t}
+}
+
+func (t Transaccion)GetAll() gin.HandlerFunc{
+	return func(ctx *gin.Context) {
+			data, err := t.service.GetAll()
+			if err != nil{
+			ctx.String(200, "Ocurrio un error")
+			return
+			}
+			ctx.JSON(200, data)
+		}
+	}
+
+
+func (t *Transaccion) Store() gin.HandlerFunc{
+	return func(ctx *gin.Context){
+	var req request
+	err := ctx.ShouldBind(&req)
+	token := ctx.GetHeader("token")
+
+	if token == os.Getenv("TOKEN") {
+		if err != nil {
+			ctx.String(400, "Ha ocurrido un error")
+		} else {
+			p, err := t.service.Store(req.CodigoTransaccion, req.Moneda, req.Monto, req.Emisor, req.Receptor, req.FechaTransaccion)
+			if err != nil {
+				ctx.String(400, "Ha ocurrido un error al escribir")
+				return
+			}
+			ctx.JSON(200, p)
+		}
+	} else {
+		ctx.String(401, "No tiene permisos para realizar la petición realizada")
+	}
+}
+
+}
+
+func (t *Transaccion) Update() gin.HandlerFunc{
+	return func(ctx *gin.Context) {
+		token := ctx.Request.Header.Get("token")
+		if token != "secure"{
+			ctx.JSON(401, gin.H{"error": "Token invalido"})
+			return
+		}
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(401, gin.H{"error": "ID invalido"})
+			return
+		}
+		var req request
+		err = ctx.ShouldBind(&req)
+		if err != nil {
+			ctx.JSON(404, gin.H{"error": err.Error()})
+			return
+		}
+
+		t, err := t.service.Update(int(id), req.CodigoTransaccion, req.Moneda, req.Monto, req.Emisor, req.Receptor, req.FechaTransaccion)
+		if err != nil {
+		ctx.JSON(404, gin.H{"error": err.Error()})
+		return
+		}
+		ctx.JSON(200, t)
+	}
+}
+
+func (t *Transaccion) UpdateEmisor() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.Request.Header.Get("token")
+		if token != "secure"{
+			ctx.JSON(401, gin.H{"error": "Token invalido"})
+			return
+		}
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(401, gin.H{"error": "ID invalido"})
+			return
+		}
+		var req request
+		err = ctx.ShouldBind(&req)
+		if err != nil {
+			ctx.JSON(404, gin.H{"error": err.Error()})
+			return
+		}
+
+		t, err := t.service.UpdateEmisor(int(id), req.Emisor)
+		if err != nil {
+		ctx.JSON(404, gin.H{"error": err.Error()})
+		return
+		}
+		ctx.JSON(200, t)
+	}
+}
+
+
+func (t *Transaccion) Delete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.Request.Header.Get("token")
+		if token != "secure"{
+			ctx.JSON(401, gin.H{"error": "Token invalido"})
+			return
+		}
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(401, gin.H{"error": "ID invalido"})
+			return
+		}
+		err = t.service.Delete(int(id))
+
+		if err != nil {
+		ctx.JSON(404, gin.H{"error": err.Error()})
+		return
+		}
+
+		ctx.JSON(200, gin.H{"data": "El producto ha sido eliminado"})
+	}
+}
+
+
+
---
+++ 8_goweb3/proyecto/cmd/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	handler "github.com/extmatperez/meli_bootcamp2/7_goweb2/proyecto/cmd/server/handler"
+	transacciones "github.com/extmatperez/meli_bootcamp2/7_goweb2/proyecto/internal/transacciones"
+	"github.com/extmatperez/meli_bootcamp2/7_goweb2/proyecto/pkg/store"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error al intentar cargar el archivo .env")
+	}
+	db := store.New(store.FileType, "/transacciones.json")
+	repo := transacciones.NewRepository(db)
+	service := transacciones.NewService(repo)
+	t := handler.NewTransaccion(service)
+
+	r := gin.Default()
+	tr := r.Group("/transacciones")
+	tr.POST("/", t.Store())
+	tr.GET(("/"), t.GetAll())
+	tr.PUT(":/id", t.Update())
+	tr.PATCH(":/id", t.UpdateEmisor())
+	tr.DELETE(":/id", t.Delete())
+
+	r.Run()
+
+}
