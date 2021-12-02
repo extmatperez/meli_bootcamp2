9a06c3e32963ea371ddb1d6ad8b77913b9f0a2dc
rbarra <ramiro.barra@mercadolibre.com>
2021-12-01 21:51:06 -0300

    add struct

---
+++ 7_goweb2/turno_tarde/ejercicio_1/internal/users/repository.go
@@ -0,0 +1,15 @@
+package users
+
+type Users struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Age       int    `json:"age"`
+	Height    int    `json:"height"`
+	Active    bool   `json:"active"`
+	Date      string `json:"date"`
+}
+
+var user []Users
+var last_ID int
