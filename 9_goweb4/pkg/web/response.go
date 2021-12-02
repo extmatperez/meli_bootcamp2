c5a2f39304c9fe049ad0fd0575c146958a52add4
Matias Ziliotto <matias.ziliotto@mercadolibre.com>
2021-12-02 15:29:15 -0300

    change response->code from string to int

--- 9_goweb4/pkg/web/response.go
+++ 9_goweb4/pkg/web/response.go
@@ -1,9 +1,7 @@
 package web
 
-import "strconv"
-
 type Response struct {
-	Code  string      `json:"code"`
+	Code  int         `json:"code"`
 	Data  interface{} `json:"data,omitempty"`
 	Error string      `json:"error,omitempty"`
 }
@@ -11,14 +9,14 @@ type Response struct {
 func NewResponse(code int, data interface{}, err string) Response {
 	if code < 300 {
 		return Response{
-			Code:  strconv.FormatInt(int64(code), 10),
+			Code:  code,
 			Data:  data,
 			Error: "",
 		}
 	}
 
 	return Response{
-		Code:  strconv.FormatInt(int64(code), 10),
+		Code:  code,
 		Data:  nil,
 		Error: err,
 	}
