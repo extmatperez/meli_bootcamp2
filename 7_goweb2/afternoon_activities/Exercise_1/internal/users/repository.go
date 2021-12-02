ef5127ef0c98b34d022af10e74f8e7ec32e9982b
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-02 03:37:41 -0300

    📝  Add structure to manipulate data

--- 7_goweb2/afternoon_activities/Exercise_1/internal/users/repository.go
+++ 7_goweb2/afternoon_activities/Exercise_1/internal/users/repository.go
@@ -1,2 +1,18 @@
 // Repository pertenece al paquete internal (carpeta general a la que pertenece)
 package internal
+
+// Estructura de los datos que voy a manipular
+type Users struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Age       int    `json:"age"`
+	Height    int    `json:"height"`
+	Active    bool   `json:"active"`
+	Date      string `json:"date"`
+}
+
+// Mientras no tengamos base de datos necesitamos saber donde almacenar los datos
+var users []Users
+var last_id int
