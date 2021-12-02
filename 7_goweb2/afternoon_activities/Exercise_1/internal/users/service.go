53a8ebdb6931bd9e9663fc6f006ef9560e788d77
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-02 05:29:35 -0300

    📝  Add Service interface

--- 7_goweb2/afternoon_activities/Exercise_1/internal/users/service.go
+++ 7_goweb2/afternoon_activities/Exercise_1/internal/users/service.go
@@ -1,2 +1,8 @@
 // Repository pertenece al paquete internal (carpeta general a la que pertenece)
 package internal
+
+// Creamos la interface Service
+type Service interface {
+	Get_users() ([]Users, error)
+	Post_users(first_name string, last_name string, email string, age int, height int, active bool, date string) (Users, error)
+}
