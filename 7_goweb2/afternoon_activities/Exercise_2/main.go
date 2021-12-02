930d0e0d670ceee86b1102b099be8f5fb6f636cc
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-01 21:11:15 -0300

    📝  Add Exercise_2 comments

---
+++ 7_goweb2/afternoon_activities/Exercise_2/main.go
@@ -0,0 +1,16 @@
+/*Ejercicio 2 - Generar paquete server
+
+Se debe separar la estructura del proyecto, como segundo paso se debe generar el paquete server donde se agregaran las funcionalidades del proyecto que dependan de paquetes externos y el main del programa.
+
+Dentro del paquete deben estar:
+El main del programa.
+Se debe importar e inyectar el repositorio, servicio y handler
+Se debe implementar el router para los diferentes endpoints
+El paquete handler con el controlador de la entidad seleccionada.
+Se debe generar la estructura request
+Se debe generar la estructura del controlador que tenga como campo el servicio
+Se debe generar la función que retorne el controlador
+Se deben generar todos los métodos correspondientes a los endpoints
+*/
+
+package main
