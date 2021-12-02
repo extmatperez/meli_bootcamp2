a3d6bd674a3ebe54c47bf5a46d050bbeff959f4d
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-02 04:17:56 -0300

    📝  Add new_repository function & the methods

--- 7_goweb2/afternoon_activities/Exercise_1/internal/users/repository.go
+++ 7_goweb2/afternoon_activities/Exercise_1/internal/users/repository.go
@@ -20,9 +20,29 @@ var last_id int
 // Definimos métodos a utilizar en nuestro repo
 type Repository interface {
 	get_users() ([]Users, error)
-	validate_fields(user_id Users) (string, error)
-	post_users() (Users, error)
+	post_users(id int, first_name string, last_name string, email string, age int, height int, active bool, date string) (Users, error)
+	/* validate_fields(user_id Users) (string, error) */
 }
 
 // Agregamos la estructura repository donde vamos a tener implementados los métodos de la interface
 type repository struct{}
+
+// Creo una función que retorne mi interface (si por ejemplo tengo otra interface para base de datos no relacionales agrego otra función que retorne esa interface)
+func new_repository() Repository {
+	return &repository{}
+}
+
+// Implementamos los métodos para que no marque erros la func new_repository
+func (repo *repository) get_users() ([]Users, error) {
+	return users, nil
+}
+func (repo *repository) post_users(id int, first_name string, last_name string, email string, age int, height int, active bool, date string) (Users, error) {
+	user := Users{id, first_name, last_name, email, age, height, active, date}
+	last_id = id
+	users = append(users, user)
+	return user, nil
+}
+
+/* func (repo *repository) validate_fields(user_id Users)(string, error){
+	return ???? <---------------
+} */
