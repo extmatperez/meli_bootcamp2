c62c059ed7b8134af68d7b90c25d786b319c97d8
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-02 06:38:05 -0300

    📝  Add Package Service (import github)

--- 7_goweb2/afternoon_activities/Exercise_1/cmd/server/handler/users.go
+++ 7_goweb2/afternoon_activities/Exercise_1/cmd/server/handler/users.go
@@ -1,6 +1,11 @@
 // Agregamos el package handler
 package handler
 
+// importo el package handler
+import (
+	users "github.com/extmatperez/meli_bootcamp2/tree/montenegro_edgar/7_goweb2/afternoon_activities/Exercise_1/internal/users"
+)
+
 // Creamos la request struct
 type request struct {
 	FirstName string `json:"first_name"`
@@ -10,4 +15,9 @@ type request struct {
 	Height    int    `json:"height"`
 	Active    bool   `json:"active"`
 	Date      string `json:"date"`
+}
+
+// Creamos Users struct
+type Users struct {
+	service users.Service
 }
