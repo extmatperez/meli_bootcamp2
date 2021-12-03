eeb13430272d963250d6a0eb1b141eeac9fc1ea6
benjaminconti <benjamin.conti@mercadolibre.com>
2021-12-03 12:39:05 -0300

    swagger documentation

--- 8_goweb3/TT/proyecto/cmd/server/handler/product.go
+++ 8_goweb3/TT/proyecto/cmd/server/handler/product.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"fmt"
-	"os"
 	"strconv"
 
 	productos "github.com/extmatperez/meli_bootcamp2/8_goweb3/TT/proyecto/internal/productos"
@@ -28,25 +27,34 @@ func NewProducto(s productos.Service) *Producto {
 	return &Producto{service: s}
 }
 
+// ListProducts godoc
+// @Summary List productos
+// @Tags Productos
+// @Description get productos
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /productos [get]
 func (controller *Producto) GetAll() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token := c.GetHeader("token")
+		// token := c.GetHeader("token")
 
-		if token == "" {
-			//c.String(400, "Token no enviado")
-			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
-			return
-		}
+		// if token == "" {
+		// 	//c.String(400, "Token no enviado")
+		// 	c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
+		// 	return
+		// }
 
-		tokenENV := os.Getenv("TOKEN")
+		// tokenENV := os.Getenv("TOKEN")
 
-		if token != tokenENV {
-			//c.String(401, "Token inválido")
-			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
-			return
-		}
+		// if token != tokenENV {
+		// 	//c.String(401, "Token inválido")
+		// 	c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
+		// 	return
+		// }
 
 		productos, err := controller.service.GetAll()
 
@@ -61,25 +69,35 @@ func (controller *Producto) GetAll() gin.HandlerFunc {
 	}
 }
 
+// StoreProducts godoc
+// @Summary Store productos
+// @Tags Productos
+// @Description store producto
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Param product body request true "Product to store"
+// @Success 201 {object} web.Response
+// @Router /productos [post]
 func (controller *Producto) Store() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token := c.GetHeader("token")
+		// token := c.GetHeader("token")
 
-		if token == "" {
-			//c.String(400, "Token no enviado")
-			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
-			return
-		}
+		// if token == "" {
+		// 	//c.String(400, "Token no enviado")
+		// 	c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
+		// 	return
+		// }
 
-		tokenENV := os.Getenv("TOKEN")
+		// tokenENV := os.Getenv("TOKEN")
 
-		if token != tokenENV {
-			//c.String(401, "Token inválido")
-			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
-			return
-		}
+		// if token != tokenENV {
+		// 	//c.String(401, "Token inválido")
+		// 	c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
+		// 	return
+		// }
 
 		var nuevoProducto request
 
@@ -127,32 +145,41 @@ func (controller *Producto) Store() gin.HandlerFunc {
 				c.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error al guardar los datos: %v", err.Error())))
 			} else {
 				//c.JSON(200, response)
-				c.JSON(200, web.NewResponse(200, response, ""))
+				c.JSON(201, web.NewResponse(201, response, ""))
 			}
 		}
 
 	}
 }
 
+// ListProducts godoc
+// @Summary Update producto
+// @Tags Productos
+// @Description update producto
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /productos/:id [put]
 func (controller *Producto) Update() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token := c.GetHeader("token")
+		// token := c.GetHeader("token")
 
-		if token == "" {
-			//c.String(400, "Token no enviado")
-			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
-			return
-		}
+		// if token == "" {
+		// 	//c.String(400, "Token no enviado")
+		// 	c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
+		// 	return
+		// }
 
-		tokenENV := os.Getenv("TOKEN")
+		// tokenENV := os.Getenv("TOKEN")
 
-		if token != tokenENV {
-			//c.String(401, "Token inválido")
-			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
-			return
-		}
+		// if token != tokenENV {
+		// 	//c.String(401, "Token inválido")
+		// 	c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
+		// 	return
+		// }
 
 		var productoActualizado request
 
@@ -215,25 +242,34 @@ func (controller *Producto) Update() gin.HandlerFunc {
 	}
 }
 
+// ListProducts godoc
+// @Summary Delete producto
+// @Tags Productos
+// @Description delete producto
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /productos/:id [delete]
 func (controller *Producto) Delete() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token := c.GetHeader("token")
+		// token := c.GetHeader("token")
 
-		if token == "" {
-			//c.String(400, "Token no enviado")
-			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
-			return
-		}
+		// if token == "" {
+		// 	//c.String(400, "Token no enviado")
+		// 	c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
+		// 	return
+		// }
 
-		tokenENV := os.Getenv("TOKEN")
+		// tokenENV := os.Getenv("TOKEN")
 
-		if token != tokenENV {
-			//c.String(401, "Token inválido")
-			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
-			return
-		}
+		// if token != tokenENV {
+		// 	//c.String(401, "Token inválido")
+		// 	c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
+		// 	return
+		// }
 
 		id, err := strconv.Atoi(c.Param("id"))
 
@@ -256,25 +292,34 @@ func (controller *Producto) Delete() gin.HandlerFunc {
 	}
 }
 
+// ListProducts godoc
+// @Summary Update(Nombre and Precio) producto
+// @Tags Productos
+// @Description update Nombre and Precio of a producto
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /productos/:id [patch]
 func (controller *Producto) UpdateNombrePrecio() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		token := c.GetHeader("token")
+		// token := c.GetHeader("token")
 
-		if token == "" {
-			//c.String(400, "Token no enviado")
-			c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
-			return
-		}
+		// if token == "" {
+		// 	//c.String(400, "Token no enviado")
+		// 	c.JSON(400, web.NewResponse(400, nil, "Token no enviado"))
+		// 	return
+		// }
 
-		tokenENV := os.Getenv("TOKEN")
+		// tokenENV := os.Getenv("TOKEN")
 
-		if token != tokenENV {
-			//c.String(401, "Token inválido")
-			c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
-			return
-		}
+		// if token != tokenENV {
+		// 	//c.String(401, "Token inválido")
+		// 	c.JSON(400, web.NewResponse(401, nil, "Token inválido"))
+		// 	return
+		// }
 
 		var productoActualizado request
 
