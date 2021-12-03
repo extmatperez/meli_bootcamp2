4ec42fa0cbc3e390c37bb4eae9854ea714358160
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-03 03:18:48 -0300

    ✨  Add Update_users handler functionality

--- 8_goweb3/morning_activities/Exercise_1/cmd/server/handler/users.go
+++ 8_goweb3/morning_activities/Exercise_1/cmd/server/handler/users.go
@@ -4,6 +4,7 @@ package handler
 // importo el package handler
 import (
 	"net/http"
+	"strconv"
 
 	users "github.com/extmatperez/meli_bootcamp2/tree/montenegro_edgar/8_goweb3/morning_activities/Exercise_1/internal/users"
 	"github.com/gin-gonic/gin"
@@ -58,6 +59,32 @@ func (controller *Users) Post_users() gin.HandlerFunc {
 				c.String(http.StatusBadRequest, "Something went wrong to post a new user")
 			} else {
 				c.JSON(http.StatusOK, response)
+			}
+		}
+	}
+}
+
+// Agregar Update_users handler que va a ser usado en el endpoint por main
+func (controller *Users) Update_users() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var use request
+
+		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+
+		if err != nil {
+			c.String(http.StatusBadRequest, "The id is not a valid id.")
+		}
+
+		err = c.ShouldBindJSON(&use)
+
+		if err != nil {
+			c.String(http.StatusBadRequest, "Something went wrong")
+		} else {
+			user_updated, err := controller.service.Update_users(int(id), use.FirstName, use.LastName, use.Email, use.Age, use.Height, use.Active, use.Date)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, err.Error())
+			} else {
+				c.JSON(http.StatusOK, user_updated)
 			}
 		}
 	}
