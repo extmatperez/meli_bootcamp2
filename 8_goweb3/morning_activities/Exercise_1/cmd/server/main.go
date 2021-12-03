1fc53be93bd602e6fca54872fcb7f12b3d6b650e
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-03 01:55:59 -0300

    📝  Add structure to Exercise_1

---
+++ 8_goweb3/morning_activities/Exercise_1/cmd/server/main.go
@@ -0,0 +1,10 @@
+/* Ejercicio 1 - Generar método PUT
+
+Se solicita implementar una funcionalidad que modifique completamente una entidad. Para
+lograrlo, es necesario, seguir los siguientes pasos:
+1. Generar un método PUT para modificar la entidad completa
+2. Desde el Path enviar el ID de la entidad que se modificará
+3. En caso de no existir, retornar un error 404
+4. Realizar todas las validaciones (todos los campos son requeridos) */
+
+package main
