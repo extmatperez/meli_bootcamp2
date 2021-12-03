ad6bf7cfe3f757f56600922ff1b06fb53139180e
BeSaavedram <be.saavedram@gmail.com>
2021-12-03 09:54:38 -0300

    Exercise 1-3 / practice 1

--- 6_goweb1/practica_1/main.go
+++ 6_goweb1/practica_1/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
 
+type Users struct {
+	ID           int     `json:"id"`
+	FirstName    string  `json:"first_name"`
+	LastName     string  `json:"last_name"`
+	Email        string  `json:"email"`
+	Age    int     `json:"age"`
+	Height float64 `json:"height"`
+	Active bool    `json:"active"`
+	CreationDate string  `json:"creation_date"`
+}
 func main() {
- router := gin.
+	// Routers
+	router := gin.New()
+	router.GET("/sayHello/:name", sayHello)
+	router.GET("/users", GetALL)
+	router.GET("/users/filter", GetFilter)
+	router.Run()
+}
+func GetFilter(c *gin.Context) {
+	//variables
+	var (
+		//users []Users
+		id, _ = c.GetQuery("ID")
+		//first_name, _ = c.GetQuery("firstName")
+	)
+	idInt, _ := strconv.Atoi(id)
+	c.JSON(200,idInt)
+}
+func GetALL(c *gin.Context) {
+	// Variables
+	var (
+		users []Users
+	)
+	// UnMarshal
+	jsonFile, _ := os.Open("users.json")
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &users)
+	c.JSON(200, users)
+}
+func sayHello(c *gin.Context)  {
+	// set param variable
+	name := c.Param("name")
+	c.JSON(200, gin.H{
+		"message": "Hello, "+name,
+	})
 }
