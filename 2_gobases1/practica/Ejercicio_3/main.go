7b5ad4b6e14e6968e1cb2178cc2330e3f6b46193
BeSaavedram <be.saavedram@gmail.com>
2021-12-03 09:52:30 -0300

    fixes

--- 2_gobases1/practica/Ejercicio_3/main.go
+++ 2_gobases1/practica/Ejercicio_3/main.go
@@ -1,24 +1,24 @@
 package main
 
-import "fmt"
-
-func main(){
-    // Incorrecta. variable 1nombre se inicia con un numero.
-    // var 1nombre string
-    var nombre1 string
-    // Correcta.
-    var apellido string
-    // Incorrecta. variable edad, tiene la sintaxis al reves.
-    // var int edad
-    var edad int
-    // Incorrecta. 1apellido Comienza con un numero y además se declara un numero en el.
-    // 1apellido := 6
-    apellido1 := "Apellido"
-    // Correcto
-    var licencia_de_conducir = true
-    // Incorrecta. el nombre de la variable no puede tener espacios en blancos.
-    // var estatura de la persona int
-    var estatura_de_la_persona int
-    // Correcto
-    cantidadDeHijos := 2
+func main() {
+	// Incorrecta. variable 1nombre se inicia con un numero.
+	// var 1nombre string
+	/*
+	   var nombre1 string
+	   // Correcta.
+	   var apellido string
+	   // Incorrecta. variable edad, tiene la sintaxis al reves.
+	   // var int edad
+	   var edad int
+	   // Incorrecta. 1apellido Comienza con un numero y además se declara un numero en el.
+	   // 1apellido := 6
+	   apellido1 := "Apellido"
+	   // Correcto
+	   var licencia_de_conducir = true
+	   // Incorrecta. el nombre de la variable no puede tener espacios en blancos.
+	   // var estatura de la persona int
+	   var estatura_de_la_persona int
+	   // Correcto
+	   cantidadDeHijos := 2
+	*/
 }
