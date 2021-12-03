c947bbb2641bd4889b8fb2c58de5f59dbb9ba5a5
jriosMELI <jose.rios@mercadolibre.cl>
2021-12-03 09:29:57 -0300

    Copy file TM to TT go web 4

---
+++ 9_goweb4/C4_GoWeb/C4-GoWeb-Sincronic/ExampleTT/internal/users/service.go
@@ -0,0 +1,76 @@
+/*
+Servicio, debe contener la lógica de nuestra aplicación.
+- Se debe crear el archivo service.go.
+- Se debe generar la interface Service con todos sus métodos.
+- Se debe generar la estructura service que contenga el repositorio.
+- Se debe generar una función que devuelva el Servicio.
+- Se deben implementar todos los métodos correspondientes a las operaciones a realizar (GetAll, Store, etc..).
+*/
+
+package internal
+
+type Service interface {
+	GetAll() ([]User, error)
+	Store(first_name string, last_name string, email string, age int, height int, active bool, create_date string) (User, error)
+	LoadUser() error
+	Update(id int, first_name string, last_name string, email string, age int, height int, active bool, create_date string) (User, error)
+	Delete(id int) error
+	UpdateLastName(id int, last_name string) (User, error)
+	UpdateAge(id int, age int) (User, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func (ser *service) GetAll() ([]User, error) {
+	user, err := ser.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (ser *service) Store(first_name string, last_name string, email string, age int, height int, active bool, create_date string) (User, error) {
+	ultimoID, err := ser.repository.LastId()
+
+	if err != nil {
+		return User{}, err
+	}
+
+	us, err := ser.repository.Store(ultimoID+1, first_name, last_name, email, age, height, active, create_date)
+	if err != nil {
+		return User{}, err
+	}
+
+	return us, nil
+}
+
+func (ser *service) LoadUser() error {
+	err := ser.repository.LoadUser()
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ser *service) Update(id int, first_name string, last_name string, email string, age int, height int, active bool, create_date string) (User, error) {
+	return ser.repository.Update(id, first_name, last_name, email, age, height, active, create_date)
+}
+
+func (ser *service) Delete(id int) error {
+	return ser.repository.Delete(id)
+}
+
+func (ser *service) UpdateLastName(id int, last_name string) (User, error) {
+	return ser.repository.UpdateLastName(id, last_name)
+}
+
+func (ser *service) UpdateAge(id int, age int) (User, error) {
+	return ser.repository.UpdateAge(id, age)
+}
+
+func NewService(repository Repository) Service {
+	return &service{repository: repository}
+}
