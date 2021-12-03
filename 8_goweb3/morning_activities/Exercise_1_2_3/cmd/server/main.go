6a93b20d3ec16e36fabc3156184a2f451f72e7d1
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-03 17:13:07 -0300

    📝  Add Exercises 2 & 3 explanation

--- 8_goweb3/morning_activities/Exercise_1_2_3/cmd/server/main.go
+++ 8_goweb3/morning_activities/Exercise_1_2_3/cmd/server/main.go
@@ -5,7 +5,27 @@ lograrlo, es necesario, seguir los siguientes pasos:
 1. Generar un método PUT para modificar la entidad completa
 2. Desde el Path enviar el ID de la entidad que se modificará
 3. En caso de no existir, retornar un error 404
-4. Realizar todas las validaciones (todos los campos son requeridos) */
+4. Realizar todas las validaciones (todos los campos son requeridos)
+
+Ejercicio 2 - Generar método DELETE
+Es necesario implementar una funcionalidad para eliminar una entidad. Para lograrlo, es
+necesario, seguir los siguientes pasos:
+1. Generar un método DELETE para eliminar la entidad en base al ID
+2. En caso de no existir, retornar un error 404
+
+Ejercicio 3 - Generar método PATCH
+
+Se requiere implementar una funcionalidad que modifique la entidad parcialmente, solo se
+deben modificar 2 campos:
+- Si se seleccionó Productos, los campos nombre y precio.
+- Si se seleccionó Usuarios, los campos apellido y edad.
+- Si se seleccionó Transacciones, los campos código de transacción y monto.
+.Para lograrlo, es necesario, seguir los siguientes pasos:
+1. Generar un método PATCH para modificar la entidad parcialmente, modificando solo 2
+campo (a elección)
+2. Desde el Path enviar el ID de la entidad que se modificara
+3. En caso de no existir, retornar un error 404
+4. Realizar las validaciones de los 2 campos a enviar */
 
 package main
 
