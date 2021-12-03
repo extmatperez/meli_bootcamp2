9ecbe50294a54a742d164482cc2d40218b367d97
Santiago Panceri <santiago.panceri@mercadolibre.com>
2021-12-03 13:45:38 -0300

    add swagger

--- 6_goweb1/TT/api/cmd/server/handler/producto.go
+++ 6_goweb1/TT/api/cmd/server/handler/producto.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 
 	product "github.com/extmatperez/meli_bootcamp2/tree/panceri_santiago/6_goweb1/TT/api/internal/producto"
+	"github.com/extmatperez/meli_bootcamp2/tree/panceri_santiago/6_goweb1/TT/api/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -29,18 +31,29 @@ func NewProduct(ser product.Service) *Product {
 func validarToken(ctx *gin.Context) bool {
 	token := ctx.GetHeader("token")
 	if token == "" {
-		ctx.String(400, "Falta token")
+
+		ctx.JSON(400, web.NewResponse(400, nil, "Falta token"))
 		return false
 	}
 	tokenENV := os.Getenv("TOKEN")
 	if token != tokenENV {
-		ctx.String(404, "Token incorrecto")
+		ctx.JSON(404, web.NewResponse(404, nil, "Token Incorrecto"))
+
 		return false
 	}
 
 	return true
 }
 
+// ListProducts godoc
+// @Summary List products
+// @Tags Products
+// @Description get products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /products [get]
 func (pro *Product) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
@@ -51,14 +64,25 @@ func (pro *Product) GetAll() gin.HandlerFunc {
 		products, err := pro.service.GetAll()
 
 		if err != nil {
-			ctx.String(400, "Hubo un error %v", err)
+			ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error %v", err)))
+
 		} else {
-			ctx.JSON(200, products)
+			ctx.JSON(200, web.NewResponse(200, products, ""))
 		}
 
 	}
 }
 
+// StoreProducts godoc
+// @Summary Store products
+// @Tags Products
+// @Description store products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Param product body Request true "Product to store"
+// @Success 200 {object} web.Response
+// @Router /products [post]
 func (pro *Product) Store() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
@@ -71,18 +95,28 @@ func (pro *Product) Store() gin.HandlerFunc {
 		err := ctx.ShouldBindJSON(&product)
 
 		if err != nil {
-			ctx.String(400, "Hubo un error al querer cargar los datos %v", err)
+			ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error al querer cargar los datos %v", err)))
 		} else {
 			response, err := pro.service.Store(product.Name, product.Color, product.Price, product.Stock, product.Code, product.IsPublished, product.CreatedAt)
 			if err != nil {
-				ctx.String(400, "No se pudo guardad el producto %v", err)
+				ctx.JSON(400, web.NewResponse(400, nil, fmt.Sprintf("Hubo un error al querer cargar los datos %v", err)))
 			} else {
-				ctx.JSON(200, response)
+				ctx.JSON(200, web.NewResponse(200, response, ""))
 			}
 		}
 	}
 }
 
+// StoreProducts godoc
+// @Summary Store products
+// @Tags Products
+// @Description store products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Param product body Request true "Product to store"
+// @Success 200 {object} web.Response
+// @Router /products [delete]
 func (pro *Product) Delete() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
@@ -93,6 +127,8 @@ func (pro *Product) Delete() gin.HandlerFunc {
 		idStr := ctx.Param("id")
 		id, err := strconv.Atoi(idStr)
 		if err != nil {
+			ctx.JSON(200, web.NewResponse(200, nil, fmt.Sprintf("Hubo un error: %v", err)))
+
 			ctx.String(400, "Hubo un error")
 			return
 		}
@@ -100,13 +136,26 @@ func (pro *Product) Delete() gin.HandlerFunc {
 		mes, err := pro.service.Delete(int64(id))
 
 		if err != nil {
+			ctx.JSON(200, web.NewResponse(200, nil, fmt.Sprintf("No se pudo guardad el producto %v: ", err)))
+
 			ctx.String(400, "No se pudo guardad el producto %v: ", err)
 		} else {
-			ctx.JSON(200, mes)
+			ctx.JSON(200, web.NewResponse(200, mes, ""))
+
 		}
 	}
 }
 
+// StoreProducts godoc
+// @Summary Store products
+// @Tags Products
+// @Description store products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Param product body Request true "Product to store"
+// @Success 200 {object} web.Response
+// @Router /products [put]
 func (pro *Product) Update() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
@@ -138,6 +187,16 @@ func (pro *Product) Update() gin.HandlerFunc {
 	}
 }
 
+// StoreProducts godoc
+// @Summary Store products
+// @Tags Products
+// @Description store products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Param product body Request true "Product to store"
+// @Success 200 {object} web.Response
+// @Router /products [patch]
 func (pro *Product) UpdateNombre() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
--- 6_goweb1/TT/api/cmd/server/main.go
+++ 6_goweb1/TT/api/cmd/server/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/extmatperez/meli_bootcamp2/tree/panceri_santiago/6_goweb1/TT/api/cmd/server/handler"
+	"github.com/extmatperez/meli_bootcamp2/tree/panceri_santiago/6_goweb1/TT/api/docs"
 	producto "github.com/extmatperez/meli_bootcamp2/tree/panceri_santiago/6_goweb1/TT/api/internal/producto"
 	"github.com/extmatperez/meli_bootcamp2/tree/panceri_santiago/6_goweb1/TT/api/pkg/store"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title MELI Bootcamp API
+// @version 1.0
+// @description This API Handle MELI Products.
+// @termsOfService https://developers.mercadolibre.com.ar/es_ar/terminos-y-condiciones
+// @contact.name API Support
+// @contact.url https://developers.mercadolibre.com.ar/support
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
 
 	err := godotenv.Load()
@@ -19,6 +32,8 @@ func main() {
 
 	router := gin.Default()
 
+	docs.SwaggerInfo.Host = os.Getenv("HOST")
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	db := store.New(store.FileType, "../../internal/producto/products.json")
 
 	repository := producto.NewRepository(db)
@@ -27,6 +42,8 @@ func main() {
 
 	routerGroup := router.Group("/products")
 	{
+		//routerGroup.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 		routerGroup.GET("", controller.GetAll())
 		routerGroup.POST("/add", controller.Store())
 		routerGroup.DELETE("/:id", controller.Delete())
