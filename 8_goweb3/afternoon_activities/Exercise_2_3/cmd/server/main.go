f1fe8a329cf0a8f2bc30be08b67c3e1b0ef8043c
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-05 23:09:34 -0300

    🚚  Rename Exercise_2 folder to Exercise_2_3

---
+++ 8_goweb3/afternoon_activities/Exercise_2_3/cmd/server/main.go
@@ -0,0 +1,42 @@
+/* Ejercicio 2 - Guardar información
+Se debe implementar la funcionalidad para guardar la información de la petición en un
+archivo json, para eso se deben realizar los siguientes pasos:
+1. En lugar de guardar los valores de nuestra entidad en memoria, se debe crear un
+archivo; los valores que se vayan agregando se guardan en él. */
+
+package main
+
+import (
+	"log"
+
+	"github.com/extmatperez/meli_bootcamp2/tree/montenegro_edgar/8_goweb3/afternoon_activities/Exercise_2_3/cmd/server/handler"
+	users "github.com/extmatperez/meli_bootcamp2/tree/montenegro_edgar/8_goweb3/afternoon_activities/Exercise_2_3/internal/users"
+	"github.com/extmatperez/meli_bootcamp2/tree/montenegro_edgar/8_goweb3/afternoon_activities/Exercise_2_3/pkg/store"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+// Creo la función main, agrego mi router y lo inicializo, creo las rutas necesarias y agrego los handlers (controllers)
+func main() {
+	// Levanto las variables de entorno del archivo .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Something went wrong with the .env file")
+	}
+
+	router := gin.Default()
+
+	// Creamos una variable que guarde lo que vamos a escribir en nuestro json y se lo pasamos el repo
+	db := store.New(store.File_type, "./users.json")
+	repo := users.New_repository(db)
+	service := users.New_service(repo)
+	controller := handler.New_user(service)
+
+	router.GET("/users", controller.Get_users())
+	router.POST("/users", controller.Post_users())
+	router.PUT("/users/:id", controller.Update_users())
+	router.PATCH("/users/:id", controller.Update_users_first_name())
+	router.DELETE("/users/:id", controller.Delete_users())
+
+	router.Run()
+}
