2f37bfb2b204d53560405de378bf33c79f35f325
nicoarguello <nicolas.arguello@mercadolibre.com>
2021-12-06 09:31:13 -0300

    documentacion

--- 8_goweb3/proyecto/cmd/server/handler/productos.go
+++ 8_goweb3/proyecto/cmd/server/handler/productos.go
@@ -45,6 +45,15 @@ func validarToken(c *gin.Context) bool {
 	return true
 }
 
+// ListProducts godoc
+// @Summary List products
+// @Tags Products
+// @Description get products
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /products [get]
 func (prod *Producto) GetAll() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
@@ -63,6 +72,16 @@ func (prod *Producto) GetAll() gin.HandlerFunc {
 	}
 }
 
+// StoreProducts godoc
+// @Summary Store products
+// @Tags Products
+// @Description store products
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Param product body request true "Product to store"
+// @Success 200 {object} web.Response
+// @Router /addProductos [post]
 func (controller *Producto) Store() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
@@ -87,6 +106,16 @@ func (controller *Producto) Store() gin.HandlerFunc {
 	}
 }
 
+// ModifyProducts godoc
+// @Summary Modify products
+// @Tags Products
+// @Description modify products
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Param product body request true "Product to Modify"
+// @Success 200 {object} web.Response
+// @Router /modify/:id [put]
 func (controller *Producto) Modify() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
@@ -119,6 +148,16 @@ func (controller *Producto) Modify() gin.HandlerFunc {
 	}
 }
 
+// Modify Name Price Products godoc
+// @Summary Modify Name Price products
+// @Tags Products
+// @Description Modify Name Price products
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Param product body request true "Product to Modify Name Price"
+// @Success 200 {object} web.Response
+// @Router /modifyNaPr/:id [patch]
 func (controller *Producto) ModifyNamePrice() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
@@ -152,6 +191,16 @@ func (controller *Producto) ModifyNamePrice() gin.HandlerFunc {
 	}
 }
 
+// Delete Products godoc
+// @Summary Delete products
+// @Tags Products
+// @Description Delete products
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Param product body request true "Product to Delete"
+// @Success 200 {object} web.Response
+// @Router /delete/:id [delete]
 func (controller *Producto) Delete() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
--- 8_goweb3/proyecto/cmd/server/main.go
+++ 8_goweb3/proyecto/cmd/server/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/extmatperez/meli_bootcamp2/8_goweb3/proyecto/cmd/server/handler"
+	"github.com/extmatperez/meli_bootcamp2/8_goweb3/proyecto/docs"
 	producto "github.com/extmatperez/meli_bootcamp2/8_goweb3/proyecto/internal/productos"
 	"github.com/extmatperez/meli_bootcamp2/8_goweb3/proyecto/pkg/store"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
+
+// @title MELI Bootcamp API
+// @version 0.1
+// @description This API Handle MELI Products.
+// @termsOfService https://developers.mercadolibre.com.ar/es_ar/terminos-y-condiciones
+// @contact.name API Support
+// @contact.url https://developers.mercadolibre.com.ar/support
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
 func main() {
 	err := godotenv.Load()
@@ -22,6 +35,9 @@ func main() {
 	repo := producto.NewRepository(db)
 	service := producto.NewService(repo)
 	controller := handler.NewProducto(service)
+
+	docs.SwaggerInfo.Host = os.Getenv("HOST")
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.GET("/productos", controller.GetAll())
 	router.POST("/addProductos", controller.Store())
