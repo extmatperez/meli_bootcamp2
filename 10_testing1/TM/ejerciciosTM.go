4d9df90acc0f3b55e8c1a47918f1d7021ba8ac85
nicoarguello <nicolas.arguello@mercadolibre.com>
2021-12-10 12:28:13 -0300

    Testing 1 TM

---
+++ 10_testing1/TM/ejerciciosTM.go
@@ -0,0 +1,40 @@
+package main
+
+//* Ejercicio 1
+//? ¿Cuáles son las diferencias entre White Box y Black Box?
+/*
+Black Box: es una estructura de testing donde el funcionamiento interno es desconocido
+           por quien lo prueba. El objetivo es evaluar el comportamiento de un componente
+ 		   en distintos escenarios.
+White Box: Quien ejecuta las pruebas, ve y conoce el codigo. Su diseño esta ligado al cod fuente.
+		   Este procedimiento se aplica para poder detectar vulnerabilidades en el codigo.
+		   Pueden aplicarse en Test Unitarios y de Integracion.
+
+
+*/
+
+//* Ejercicio 2
+// ?¿Qué es un test funcional?.
+/*
+	Son pruebas basadas en la entrada y salida del software, con el objetivo de comprobar que
+la respuesta del mismo en distintos escenarios coincida con el resultado esperado.
+*/
+
+//* Ejercicio 3
+// ?¿Qué es un Test de Integración?
+/*
+	Son los tests que prueban la comunicacion entre distintos componentes de la aplicacion.
+	el objetivo es probar que todo los bloques que se probaron de forma unitaria, interactuen entre si
+	y generen los resultados esperados.
+*/
+
+//* Ejercicio 4
+// ?Indicar las dimensiones de calidad prioritarias en MELI.
+/*
+	Creo que el principio F.I.R.S.T se adapta a lo que espera MELi por ser:
+	Rapidos
+	Independientes
+	Repetibles
+	Auto-Validados
+	Completos
+*/
