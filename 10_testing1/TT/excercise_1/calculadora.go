f347d80482c59a36bcc3d1849e03b21984772aed
andreskask <andres.pachon@mercadolibre.com.co>
2021-12-10 14:27:09 -0500

    excercise_1_TT

---
+++ 10_testing1/TT/excercise_1/calculadora.go
@@ -0,0 +1,5 @@
+package calculadora
+
+func Restar(numero1, numero2 int) int {
+	return numero1 - numero2
+}
---
+++ 10_testing1/TT/excercise_1/calculadora_test.go
@@ -0,0 +1,18 @@
+package calculadora
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRestar(t *testing.T) {
+
+	num1 := 5
+	num2 := 3
+	resutlEsp := 2
+
+	resultado := Restar(num1, num2)
+
+	assert.Equal(t, resutlEsp, resultado)
+}
