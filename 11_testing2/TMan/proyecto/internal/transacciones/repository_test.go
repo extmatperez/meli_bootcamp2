92d8bcd3da48e103d031f1bbe4fa90366bc5b825
jpacostasc <juan.acosta@mercadolibre.com>
2021-12-13 15:10:19 -0300

    Ejercicio 2 TM terminado

--- 11_testing2/TMan/proyecto/internal/transacciones/repository_test.go
+++ 11_testing2/TMan/proyecto/internal/transacciones/repository_test.go
@@ -24,8 +24,31 @@ var perso string = `[{
     "receptor": "Hickle-Barrows",
     "fecha_transaccion": "25/06/2021"
   }]`
+  
+
+  var per string = `[{
+    "id": 1,
+    "codigo_transaccion": 486499,
+    "moneda": "pesos",
+    "monto": 84.16,
+    "emisor": "Emisor Anterior",
+    "receptor": "Hauck-Carter",
+    "fecha_transaccion": "13/08/2021"
+  }]`
 
 type StubStore struct{}
+type mockStorage struct{
+  readed bool
+}
+
+func (m *mockStorage) Read(data interface{}) error{
+  m.readed = true
+  return json.Unmarshal([]byte(per), &data)
+}
+
+func (m *mockStorage) Write(data interface{}) error{
+  return nil
+}
 
 func (s *StubStore) Read(data interface{}) error{
 	return json.Unmarshal([]byte(perso), &data)
@@ -34,6 +57,8 @@ func (s *StubStore) Read(data interface{}) error{
 func (s *StubStore) Write(data interface{}) error{
 	return nil
 }
+
+
 
 
 
@@ -46,4 +71,20 @@ func TestGetAll(t *testing.T){
 	json.Unmarshal([]byte(perso), &esperado)
 
 	assert.Equal(t, esperado, personas)
+}
+
+func TestUpdateEmisor(t *testing.T) {
+  store := mockStorage{}
+	repo := NewRepository(&store)
+
+  store.readed = false
+
+  actualizado, _ := repo.UpdateEmisor(1, "Emisor actualizado")
+
+  var esperado []Transaccion
+	json.Unmarshal([]byte(per), &esperado)
+
+  assert.Equal(t, store.readed, true)
+  assert.NotEqual(t, esperado, actualizado)
+
 }
