c6c167ac5d635fd0db3b4c7845c714e56c431a7a
FranciscoPalacioMeli <francisco.fpalacio@mercadolibre.com>
2021-12-13 17:12:14 -0300

    Test ready 76,6 COVER

--- 11_testing2/TurnoTarde/internal/transaccion/service_test.go
+++ 11_testing2/TurnoTarde/internal/transaccion/service_test.go
@@ -110,3 +110,59 @@ func TestDeleteError(t *testing.T){
 	assert.NotNil(t,err)
 	assert.Error(t,err)
 }
+
+
+func TestStore(t *testing.T){
+	transacionTest := Transaction{
+		Codigo: "New001",
+		Moneda: "ARS",
+		Monto: "850",
+		Emisor: "Luis",
+		Receptor: "Ppepe",
+		Fecha: "13/12/2021",
+	}
+
+	mock := store.Mock{Data: []byte(transactions),IsStoreRead: false}
+	typeFileMock := store.FileStore{Mock: &mock}
+	myRepo := NewRepository(&typeFileMock)
+	myService:= NewService(myRepo)
+
+	newTransaction,err := myService.Store(transacionTest.Codigo,transacionTest.Moneda,transacionTest.Monto,
+										transacionTest.Emisor,transacionTest.Receptor,transacionTest.Fecha)
+
+	assert.Nil(t,err)
+	assert.True(t,mock.IsStoreRead)
+	assert.True(t,mock.IsStoreWrite)
+	assert.Equal(t,transacionTest.Codigo,newTransaction.Codigo)
+	assert.Equal(t,transacionTest.Emisor,newTransaction.Emisor)
+	assert.Equal(t,transacionTest.Fecha,newTransaction.Fecha)
+	assert.Equal(t,transacionTest.Moneda,newTransaction.Moneda)
+	assert.Equal(t,transacionTest.Monto,newTransaction.Monto)
+	assert.Equal(t,transacionTest.Receptor,newTransaction.Receptor)
+}
+
+
+func TestStoreError(t *testing.T){
+	transacionTest := Transaction{
+		Codigo: "New001",
+		Moneda: "ARS",
+		Monto: "850",
+		Emisor: "Luis",
+		Receptor: "Ppepe",
+		Fecha: "13/12/2021",
+	}
+	erCreated:= errors.New("Error al updtear transaction")
+	mock := store.Mock{Data: []byte(transactions),Err: erCreated}
+	typeFileMock := store.FileStore{Mock: &mock}
+	myRepo := NewRepository(&typeFileMock)
+	myService:= NewService(myRepo)
+
+	newTransaction,err := myService.Store(transacionTest.Codigo,transacionTest.Moneda,transacionTest.Monto,
+										transacionTest.Emisor,transacionTest.Receptor,transacionTest.Fecha)
+
+	assert.True(t,!mock.IsStoreRead)
+	assert.True(t,!mock.IsStoreWrite)
+	assert.NotNil(t,err,erCreated)
+	assert.Equal(t,Transaction{},newTransaction)
+
+}
