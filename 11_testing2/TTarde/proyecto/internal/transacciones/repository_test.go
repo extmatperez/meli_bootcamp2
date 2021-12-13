577a0f2e11904c2520b05a8600045084576a137a
jpacostasc <juan.acosta@mercadolibre.com>
2021-12-13 16:49:26 -0300

    Ejercicio 1 TT caminando

---
+++ 11_testing2/TTarde/proyecto/internal/transacciones/repository_test.go
@@ -0,0 +1,113 @@
+package internal
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/extmatperez/meli_bootcamp2/11_testing2/TTarde/proyecto/pkg/store"
+	"github.com/go-playground/assert/v2"
+)
+
+
+var perso string = `[{
+    "id": 1,
+    "codigo_transaccion": 486499,
+    "moneda": "pesos",
+    "monto": 84.16,
+    "emisor": "Lakin LLC",
+    "receptor": "Hauck-Carter",
+    "fecha_transaccion": "13/08/2021"
+  }, {
+    "id": 2,
+    "codigo_transaccion": 232323,
+    "moneda": "yenes",
+    "monto": 99.13,
+    "emisor": "Breitenberg and Sons",
+    "receptor": "Hickle-Barrows",
+    "fecha_transaccion": "25/06/2021"
+  }]`
+  
+
+  var per string = `[{
+    "id": 1,
+    "codigo_transaccion": 486499,
+    "moneda": "pesos",
+    "monto": 84.16,
+    "emisor": "Emisor Anterior",
+    "receptor": "Hauck-Carter",
+    "fecha_transaccion": "13/08/2021"
+  }]`
+
+type StubStore struct{}
+type mockStorage struct{
+  readed bool
+}
+
+func (m *mockStorage) Read(data interface{}) error{
+  m.readed = true
+  return json.Unmarshal([]byte(per), &data)
+}
+
+func (m *mockStorage) Write(data interface{}) error{
+  return nil
+}
+
+func (s *StubStore) Read(data interface{}) error{
+	return json.Unmarshal([]byte(perso), &data)
+}
+
+func (s *StubStore) Write(data interface{}) error{
+	return nil
+}
+
+
+
+
+
+func TestGetAll(t *testing.T){
+	store := StubStore{}
+	repo := NewRepository(&store)
+
+	personas, _ := repo.GetAll()
+	var esperado []Transaccion
+	json.Unmarshal([]byte(perso), &esperado)
+
+	assert.Equal(t, esperado, personas)
+}
+
+func TestUpdateEmisor(t *testing.T) {
+  store := mockStorage{}
+	repo := NewRepository(&store)
+
+  store.readed = false
+
+  actualizado, _ := repo.UpdateEmisor(1, "Emisor actualizado")
+
+  var esperado Transaccion
+	json.Unmarshal([]byte(per), &esperado)
+
+  assert.Equal(t, store.readed, true)
+  assert.Equal(t, esperado.Emisor, actualizado.Emisor)
+
+}
+
+func TestUpdateMock (t *testing.T) {
+  dataByte := []byte(per)
+  var transaccionEsperada Transaccion
+
+  json.Unmarshal(dataByte, &transaccionEsperada)
+  transaccionEsperada.Emisor = "Emisor Actualizado"
+
+  dbMock := store.Mock{Data: dataByte}
+  storeStub := store.FileStore{Mock: &dbMock}
+  repo := NewRepository(&storeStub)
+
+
+  per_actualizada, _ := repo.Update(1, 486499, "pesos", 84.16, "Emisor Actualizado", "Hauck-Carter", "13/08/2021")
+
+
+  assert.Equal(t, transaccionEsperada, per_actualizada)
+}
+
+
+
---
+++ 11_testing2/TTarde/proyecto/pkg/store/file.go
@@ -0,0 +1,76 @@
+package store
+
+import (
+	"os"
+	"encoding/json"
+)
+
+type Store interface {
+	Read(data interface{}) error
+	Write(data interface{}) error
+}
+
+type Type string
+
+const (
+	FileType Type = "file"
+)
+
+
+func New(store Type, filename string) Store{
+	switch store{
+	case FileType:
+		return &FileStore{FileName: filename}
+	}
+	return nil
+}
+
+type FileStore struct{
+	FileName string 
+	Mock *Mock
+}
+
+type Mock struct{
+	Data []byte
+	Err error
+}
+
+func (fs *FileStore) AddMock(mock *Mock){
+	fs.Mock = mock
+}
+
+func (fs *FileStore) ClearMock() {
+	fs.Mock = nil
+}
+
+func (fs *FileStore) Write(data interface{}) error{
+	fileData, err := json.MarshalIndent(data, "", " ")
+	if err != nil{
+		return err
+	}
+	if fs.Mock != nil{
+		if fs.Mock.Err != nil{
+			return fs.Mock.Err
+		}
+		fs.Mock.Data = fileData
+		return nil
+	}
+
+	return os.WriteFile(fs.FileName, fileData, 0644)
+}
+
+func (fs *FileStore) Read(data interface{}) error{
+
+	if fs.Mock != nil {
+        if fs.Mock.Err != nil {
+            return fs.Mock.Err
+        }
+        return json.Unmarshal(fs.Mock.Data, data)
+    }
+
+	file, err := os.ReadFile(fs.FileName)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(file, &data)
+}
