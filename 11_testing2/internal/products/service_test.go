c2a09a493b8631f08edd103b5ab7f70817c058f5
Matias Ziliotto <matias.ziliotto@mercadolibre.com>
2021-12-13 17:10:56 -0300

    Add get all & store tests

--- 11_testing2/internal/products/service_test.go
+++ 11_testing2/internal/products/service_test.go
@@ -1,7 +1,9 @@
 package internal
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/extmatperez/meli_bootcamp2/11_testing2/pkg/store"
@@ -30,7 +32,45 @@ var productsServiceTest = `
 	"created_at": "12/10/2021"
 }]`
 
-func TestUpdateMock(t *testing.T) {
+// Get all tests
+func TestServiceGetAllMock(t *testing.T) {
+	// Arrange
+	productBytes := []byte(productsServiceTest)
+	dbMock := store.Mock{Data: productBytes, ReadUsed: false}
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	var expectedResult []Product
+	_ = json.Unmarshal(dbMock.Data, &expectedResult)
+
+	// Act
+	result, _ := service.GetAll()
+
+	// Assert
+	assert.Equal(t, expectedResult, result, "should be equal")
+	assert.True(t, dbMock.ReadUsed)
+}
+
+func TestServiceGetAllMockError(t *testing.T) {
+	// Arrange
+	dbMock := store.Mock{Error: fmt.Errorf("test error get all")}
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	var expectedResult []Product
+	_ = json.Unmarshal(dbMock.Data, &expectedResult)
+
+	// Act
+	_, err := service.GetAll()
+
+	// Assert
+	assert.Error(t, err)
+}
+
+// Update tests
+func TestServiceUpdateMock(t *testing.T) {
 	// Arrange
 	productBytes := []byte(productsServiceTest)
 	dbMock := store.Mock{Data: productBytes, ReadUsed: false}
@@ -48,7 +88,7 @@ func TestUpdateMock(t *testing.T) {
 	assert.True(t, dbMock.ReadUsed)
 }
 
-func TestUpdateMockIdNotExists(t *testing.T) {
+func TestServiceUpdateMockIdNotExists(t *testing.T) {
 	// Arrange
 	productBytes := []byte(productsServiceTest)
 	dbMock := store.Mock{Data: productBytes}
@@ -64,7 +104,7 @@ func TestUpdateMockIdNotExists(t *testing.T) {
 	assert.Equal(t, Product{}, result, "should be equal")
 }
 
-func TestUpdateMockError(t *testing.T) {
+func TestServiceUpdateMockError(t *testing.T) {
 	// Arrange
 	dbMock := store.Mock{}
 	storeStub := store.FileStore{Mock: &dbMock}
@@ -80,7 +120,7 @@ func TestUpdateMockError(t *testing.T) {
 }
 
 // Delete tests
-func TestDeleteMock(t *testing.T) {
+func TestServiceDeleteMock(t *testing.T) {
 	// Arrange
 	productBytes := []byte(productsServiceTest)
 	dbMock := store.Mock{Data: productBytes}
@@ -100,7 +140,7 @@ func TestDeleteMock(t *testing.T) {
 	assert.Equal(t, Product{}, product)
 }
 
-func TestDeleteMockIdNotExists(t *testing.T) {
+func TestServiceDeleteMockIdNotExists(t *testing.T) {
 	// Arrange
 	productBytes := []byte(productsServiceTest)
 	dbMock := store.Mock{Data: productBytes}
@@ -113,4 +153,36 @@ func TestDeleteMockIdNotExists(t *testing.T) {
 
 	// Assert
 	assert.Error(t, err)
+}
+
+// Store tests
+func TestServiceStoreMock(t *testing.T) {
+	// Arrange
+	dbMock := store.Mock{Data: []byte(`[]`)}
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	expectedResult := Product{1, "Pelota", "Negra", 1000.05, 100, "#CodeStored", false, "21/11/1997"}
+
+	// Act
+	result, _ := service.Store("Pelota", "Negra", 1000.05, 100, "#CodeStored", false, "21/11/1997")
+
+	// Assert
+	assert.Equal(t, expectedResult, result, "should be equal")
+}
+
+func TestServiceStoreMockError(t *testing.T) {
+	// Arrange
+	dbMock := store.Mock{Error: fmt.Errorf("error test")}
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	// Act
+	result, err := service.Store("Pelota", "Negra", 1000.05, 100, "#CodeStored", false, "21/11/1997")
+
+	// Assert
+	assert.Error(t, err)
+	assert.Equal(t, Product{}, result, "should be equal")
 }
