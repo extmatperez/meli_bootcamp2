63d9cb7d0b810c2b7734b4bdea2367cd77c2ba6c
MatiasDBonis <matias.boni@mercadolibre.com>
2021-12-13 15:10:32 -0300

    Ej 1 testing2

--- 11_testing2/go-web/internal/productos/repository_test.go
+++ 11_testing2/go-web/internal/productos/repository_test.go
@@ -12,8 +12,7 @@ type StubStore struct{}
 var productos string = "[{\"id\": 1,\"nombre\": \"Otro producto\",\" color\": \" Otro color\",\"precio\": \"$800\",\"stock\": 10,\"codigo\": \"AAAAAAAAAA\",\"publicado\": false,\"creado\": \"10/10/2020\"},{\"id\": 3,\"nombre\": \"Producto\",\"color\": \"\",\"precio\": \"$700\",\"stock\": 21,\"codigo\": \"32CRHI85275114\",\"publicado\": true,\"creado\": \"23/5/2020\"}]"
 
 func (s *StubStore) Read(data interface{}) error {
-	err := json.Unmarshal([]byte(productos), &data)
-	return err
+	return json.Unmarshal([]byte(productos), &data)
 }
 
 func (s *StubStore) Write(data interface{}) error {
