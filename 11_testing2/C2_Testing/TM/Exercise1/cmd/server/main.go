7804af17719499eb5f3709e80da84da37a1a6f8f
jriosMELI <jose.rios@mercadolibre.cl>
2021-12-13 13:03:55 -0300

    Exercise 1 test get all repo TM

---
+++ 11_testing2/C2_Testing/TM/Exercise1/cmd/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/extmatperez/meli_bootcamp2/11_testing2/C2_Testing/TM/Exercise1/cmd/server/handler"
+	users "github.com/extmatperez/meli_bootcamp2/11_testing2/C2_Testing/TM/Exercise1/internal/users"
+	"github.com/extmatperez/meli_bootcamp2/11_testing2/C2_Testing/TM/Exercise1/pkg/store"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load("/Users/joserios/Desktop/bootcamp/meli_bootcamp2/11_testing2/C2_Testing/TM/Exercise1/cmd/server/.env")
+	if err != nil {
+		log.Fatal("error al intentar cargar archivo")
+	}
+	router := gin.Default()
+
+	db := store.New(store.FileType, "./users.json")
+
+	repo := users.NewRepository(db)
+	service := users.NewService(repo)
+	controller := handler.NewUser(service)
+
+	routerUser := router.Group("/users")
+	routerUser.GET("/get", controller.GetAll())
+	routerUser.POST("/add", controller.Store())
+	routerUser.PUT("/:id", controller.Update())
+	routerUser.DELETE("/:id", controller.Delete())
+	routerUser.PATCH("/lastname/:id", controller.UpdateLastName())
+	routerUser.PATCH("/age/:id", controller.UpdateAge())
+
+	router.Run()
+}
