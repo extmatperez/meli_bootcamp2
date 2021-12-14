ba12b99d28db89d849f7dff1866b3ba66a010f97
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-14 12:58:58 -0300

    📝  Change Exercise comment explanation

--- 12_testing3/morning_activities/Exercise_1_2/cmd/server/main.go
+++ 12_testing3/morning_activities/Exercise_1_2/cmd/server/main.go
@@ -1,17 +1,8 @@
-/* Ejercicio 1 - Test Unitario GetAll()
-Generar un Stub del Store cuya función “Read” retorne dos productos con las especificaciones que deseen.
-Comprobar que GetAll() retorne la información exactamente igual a la esperada. Para esto:
-Dentro de la carpeta /internal/products, crear un archivo repository_test.go con el test diseñado.
-
- Ejercicio 2 - Test Unitario UpdateName()
-Diseñar Test de UpdateName, donde se valide que la respuesta retornada sea correcta para la actualización del nombre de un
-producto específico. Y además se compruebe que efectivamente se usa el método “Read” del Storage para buscar el producto. Para esto:
-Crear un mock de Storage, dicho mock debe contener en su data un producto específico cuyo nombre puede ser “Before Update”.
-El método Read del Mock, debe contener una lógica que permita comprobar que dicho método fue invocado. Puede ser a través de un
-boolean como se observó en la clase.
-Para dar el test como OK debe validarse que al invocar el método del Repository UpdateName, con el id del producto mockeado
-y con el nuevo nombre “After Update”, efectivamente haga la actualización. También debe validarse que el método Read haya sido
-ejecutado durante el test.
+/* Ejercicio 1 - Mejorar el código
+Se debe utilizar Golangci-lint para detectar y corregir todos los posibles errores en el proyecto Go-Web. Para eso se deben seguir los siguientes pasos:
+Instalar Golangci-lint.
+Ejecutar Golangci-lint (con la configuración por defecto es suficiente).
+Si existe algún error, se debe corregir.
 */
 
 package main
