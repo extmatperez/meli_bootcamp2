ec0fb0a3998d646eac5c8d85283c85ef501ad45e
FranciscoPalacioMeli <francisco.fpalacio@mercadolibre.com>
2021-12-14 13:25:11 -0300

    testing tm ready

---
+++ 12_testing3/TurnoTarde/cmd/server/handler/transaction.go
@@ -0,0 +1,420 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+
+	tra "github.com/extmatperez/meli_bootcamp2/tree/palacio_francisco/12_testing3/TurnoTarde/internal/transaccion"
+	"github.com/extmatperez/meli_bootcamp2/tree/palacio_francisco/12_testing3/TurnoTarde/pkg/store/web"
+	"github.com/gin-gonic/gin"
+)
+
+type request struct {
+	Codigo   string `json:"codigo"`
+	Moneda   string `json:"moneda"`
+	Monto    string `json:"monto"`
+	Emisor   string `json:"emisor"`
+	Receptor string `json:"receptor"`
+	Fecha    string `json:"fecha"`
+}
+
+type Transaction struct {
+	service tra.Service
+}
+
+func NewTransaction(service tra.Service) *Transaction {
+	return &Transaction{service}
+}
+
+// ListTransactions godoc
+// @Summary List Transactions
+// @Tags Transactions
+// @Description get Transactions
+// @Produce json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Error 401 {object} web.Response
+// @Router /transactions [get]
+func (tran Transaction) GetAll() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		err := ValidateToken(c.GetHeader("token"))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, err.Error()))
+			return
+		}
+
+		transactions, err := tran.service.GetAll()
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+		} else {
+			c.JSON(http.StatusOK, web.NewResponse(http.StatusOK, transactions, ""))
+		}
+	}
+}
+
+// TransactionById godoc
+// @Summary Transaction by Id
+// @Tags Transactions
+// @Description get transactions by id
+// @Produce json
+// @Param token header string true "token"
+// @Param id query integer true "id"
+// @Success 200 {object} web.Response
+// @Error 401 {object} web.Response
+// @Router /transactions:id [get]
+func (tran Transaction) GetTransactionById() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		err := ValidateToken(c.GetHeader("token"))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, err.Error()))
+			return
+		}
+
+		idTransaction, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+		transaction, err := tran.service.GetTransactionById(idTransaction)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		c.JSON(http.StatusOK, web.NewResponse(http.StatusOK, transaction, ""))
+
+	}
+}
+
+// FilterTransactions godoc
+// @Summary Filter Transactions
+// @Tags Transactions
+// @Description get transactions by filter from json
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Param transaction body request true "filtros"
+// @Success 200 {object} web.Response
+// @Error 401 {object} web.Response
+// @Router /transactions/filtros [get]
+func (tran Transaction) GetTransactionsExlusive() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		err := ValidateToken(c.GetHeader("token"))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, err.Error()))
+			return
+		}
+
+		transactions, err := tran.service.GetAll()
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		var parametros request
+		err = c.BindJSON(&parametros)
+		if err != nil {
+			c.JSON(http.StatusForbidden, web.NewResponse(http.StatusForbidden, nil, err.Error()))
+			return
+		}
+
+		filtros := GetParamsFromBody(parametros)
+
+		if len(filtros) == 0 {
+			c.JSON(http.StatusForbidden, web.NewResponse(http.StatusForbidden, nil,
+				"Debes pasar al menos un flitro con los datos a buscar"))
+			return
+		}
+
+		filtrados := transactions
+
+		for _, filtro := range filtros {
+			fmt.Println(filtro)
+			fmt.Println(reflect.ValueOf(parametros).FieldByName(filtro).String())
+			filtrados = filtrar(filtrados, filtro, reflect.ValueOf(parametros).FieldByName(filtro).String())
+		}
+
+		c.JSON(http.StatusOK, web.NewResponse(http.StatusOK, filtrados, ""))
+
+	}
+}
+
+// StoreTransactions godoc
+// @Summary Store Transactions
+// @Tags Transactions
+// @Description store Transactions
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Param transaction body request true "Transactions  to store"
+// @Success 200 {object} web.Response
+// @Router /transactions  [post]
+func (tran Transaction) Store() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		err := ValidateToken(c.GetHeader("token"))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, err.Error()))
+			return
+		}
+		var transactionRecived request
+
+		err = c.ShouldBindJSON(&transactionRecived)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		invalidParams := InValidParams(transactionRecived)
+
+		// validamos que esten todos los parametros en el request
+		if len(invalidParams) > 0 {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, fmt.Sprintf("Faltan los campos %v", invalidParams)))
+			return
+		}
+
+		tranUpdate, err := tran.service.Store(transactionRecived.Codigo, transactionRecived.Moneda, transactionRecived.Monto,
+			transactionRecived.Emisor, transactionRecived.Receptor, transactionRecived.Fecha)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		c.JSON(http.StatusOK, web.NewResponse(http.StatusOK, tranUpdate, ""))
+
+	}
+}
+
+// UpdateTransactions godoc
+// @Summary Update Transactions
+// @Tags Transactions
+// @Description store Transactions
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Param id query integer true "id"
+// @Param transaction body request true "Transactions  to store"
+// @Success 200 {object} web.Response
+// @Router /transactions:id  [put]
+func (tran Transaction) Update() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		err := ValidateToken(c.GetHeader("token"))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, err.Error()))
+			return
+		}
+
+		idTransaction, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		var transactionRecived request
+
+		err = c.ShouldBindJSON(&transactionRecived)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		invalidParams := InValidParams(transactionRecived)
+
+		// validamos que esten todos los parametros en el request
+		if len(invalidParams) > 0 {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, fmt.Sprintf("Faltan los campos %v", invalidParams)))
+			return
+		}
+
+		tranUpdate, err := tran.service.Update(idTransaction, transactionRecived.Codigo, transactionRecived.Moneda, transactionRecived.Monto,
+			transactionRecived.Emisor, transactionRecived.Receptor, transactionRecived.Fecha)
+
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		c.JSON(http.StatusOK, web.NewResponse(http.StatusOK, tranUpdate, ""))
+
+	}
+}
+
+func (tran Transaction) UpdateCodigoAndMonto() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		err := ValidateToken(c.GetHeader("token"))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, err.Error()))
+			return
+		}
+		idTransaction, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		var transactionRecived request
+
+		err = c.ShouldBindJSON(&transactionRecived)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		parametrosRequired := []string{"Codigo", "Monto"}
+		differences := ValidParms(transactionRecived, parametrosRequired)
+
+		// validamos que esten todos los parametrosRequired en el body de la petecion
+		if len(differences) > 0 {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, fmt.Sprintf("Faltan los campos %v", differences)))
+			return
+		}
+
+		tranUpdate, err := tran.service.UpdateCodigoAndMonto(idTransaction, transactionRecived.Codigo,
+			transactionRecived.Monto)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		c.JSON(http.StatusOK, web.NewResponse(http.StatusOK, tranUpdate, ""))
+
+	}
+}
+
+func (tran Transaction) Delete() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		err := ValidateToken(c.GetHeader("token"))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, err.Error()))
+
+			return
+		}
+
+		idTransaction, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		err = tran.service.Delete(idTransaction)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+		c.JSON(http.StatusOK, web.NewResponse(http.StatusOK, fmt.Sprintf("Se elimino correctamente la transaccion con id: %v", idTransaction), ""))
+	}
+}
+
+func ValidParms(transactionRecived request, parametrosRequired []string) []string {
+	var diff []string
+
+	parametrosFromBody := GetParamsFromBody(transactionRecived)
+
+	for _, s1 := range parametrosRequired {
+		found := false
+		for _, s2 := range parametrosFromBody {
+			if s1 == s2 {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			diff = append(diff, s1)
+		}
+	}
+
+	return diff
+}
+
+func GetParamsFromBody(parametros request) []string {
+	var list []string
+	r := reflect.ValueOf(parametros)
+
+	for i := 0; i < r.NumField(); i++ {
+		varValor := r.Field(i).Interface()
+		if varValor != "" && varValor != 0 {
+
+			list = append(list, r.Type().Field(i).Name)
+		}
+
+	}
+	return list
+}
+
+func InValidParams(parametros request) []string {
+	var list []string
+	r := reflect.ValueOf(parametros)
+
+	for i := 0; i < r.NumField(); i++ {
+		varValor := r.Field(i).Interface()
+		if varValor == "" || varValor == 0 {
+			list = append(list, r.Type().Field(i).Name)
+
+		}
+
+	}
+	return list
+
+}
+
+func filtrar(sliceTransaccion []tra.Transaction, campo string, valor string) []tra.Transaction {
+	var filtrado []tra.Transaction
+
+	var per tra.Transaction
+	fmt.Println(per)
+	tipos := reflect.TypeOf(per)
+	fmt.Println(tipos)
+	i := 0
+	for i = 0; i < tipos.NumField(); i++ {
+		if tipos.Field(i).Name == campo {
+			break
+		}
+	}
+
+	for _, v := range sliceTransaccion {
+		cadena := fmt.Sprintf("%v", reflect.ValueOf(v).Field(i).Interface())
+		if cadena == valor {
+			filtrado = append(filtrado, v)
+		}
+	}
+
+	return filtrado
+}
+
+func ValidateToken(token string) error {
+
+	if token == "" {
+		return errors.New("token Vacio")
+	}
+
+	tokenFromEnv := os.Getenv("TOKEN")
+	if token != tokenFromEnv {
+		return errors.New("no tiene permisos para realizar la petición solicitada")
+	}
+	return nil
+}
---
+++ 12_testing3/TurnoTarde/internal/transaccion/repository_test.go
@@ -0,0 +1,140 @@
+package internal
+
+import (
+	"encoding/json"
+	"errors"
+
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type mockStore struct{
+	transactionBeforeUpdate Transaction
+}
+
+func (m *mockStore) Read(tran Transaction) bool {
+	m.transactionBeforeUpdate = tran
+	return true
+}
+
+
+var Datos string =  `[{
+	"id": 2,
+	"codigo": "24safdsadfasdf385",
+	"moneda": "Peso Colombiano",
+	"monto": "$8228845654645678",
+	"emisor": "Luis",
+	"receptor": "Perez",
+	"fecha": "01/01/2001"
+   },
+   {
+	"id": 3,
+	"codigo": "11673-417",
+	"moneda": "Franc",
+	"monto": "$2.76",
+	"emisor": "minstone2",
+	"receptor": "sinnott2",
+	"fecha": "1/4/2021"
+   }]`
+
+
+type StubStore struct{
+	useMethodRead bool
+}
+
+func(s *StubStore) Write(data interface{}) error{
+	return nil
+}
+func(s *StubStore) Read(data interface{}) error{
+	s.useMethodRead = true
+	return json.Unmarshal([]byte(Datos), &data)
+}
+
+
+type StubStoreError struct{
+	useMethodRead bool
+}
+
+func(s *StubStoreError) Write(data interface{}) error{
+	return errors.New("Error al cargar transaccion")
+}
+func(s *StubStoreError) Read(data interface{}) error{
+	s.useMethodRead = true
+	return errors.New("No hay un archivo con trasnacciones")
+}
+
+
+func TestGetAll(t *testing.T){
+	stubStore := &StubStore{}
+	repo := NewRepository(stubStore)
+
+	var excepted []Transaction
+
+	err := json.Unmarshal([]byte(Datos), &excepted)
+
+	assert.Nil(t,err)
+	
+	tran,err := repo.GetAll()
+
+	assert.Nil(t,err)
+	assert.Equal(t,excepted,tran)
+}
+
+func TestGetAllError(t *testing.T){
+	stubStored := &StubStoreError{}
+	repod := NewRepository(stubStored)
+
+	tran,err := repod.GetAll()
+
+
+	assert.Nil(t,tran)
+	assert.True(t,stubStored.useMethodRead)
+	assert.Error(t,err)
+}
+
+func TestUpdateCodigo(t *testing.T){
+	stubStore := &StubStore{false}
+	repo := NewRepository(stubStore)
+	tran2,_ := repo.GetTransactionById(2)
+	codgUpdate :="AfterUpdatecod-123"
+
+
+	tranUpdate,err := repo.Update(2,codgUpdate,"Peso","55.6","pepe","luis","13/12/2021")
+
+	assert.True(t,stubStore.useMethodRead)
+	assert.Equal(t,tran2.ID,tranUpdate.ID)
+	assert.Equal(t,codgUpdate, codgUpdate)
+	assert.Nil(t,err)
+}
+
+func TestUpdateCodigoError(t *testing.T){
+	stubStore := &StubStoreError{false}
+	repo := NewRepository(stubStore)
+	codgUpdate :="AfterUpdatecod-123"
+
+
+	tranUpdate,err := repo.Update(2,codgUpdate,"Peso","55.6","pepe","luis","13/12/2021")
+
+	assert.True(t,stubStore.useMethodRead)
+	assert.Equal(t,Transaction{},tranUpdate)
+	assert.Error(t,err)
+}
+
+
+func TestUpdateCodigoAndMonto(t *testing.T){
+	stubStore := &StubStore{false}
+	repo := NewRepository(stubStore)
+	codgUpdate :="AfterUpdatecod-123"
+	monto :="88.5"
+	transactionTest := Transaction{2, codgUpdate, "Peso Colombiano", monto, "Luis", "Perez", "01/01/2001"}
+	mock := &mockStore{transactionTest}
+	isRead := mock.Read(transactionTest)
+
+	tranUpdate,err := repo.UpdateCodigoAndMonto(2,codgUpdate,monto)
+
+	assert.True(t,stubStore.useMethodRead)
+	assert.Nil(t,err)
+	assert.Equal(t,tranUpdate,mock.transactionBeforeUpdate)
+	assert.True(t,isRead)
+}
