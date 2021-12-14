ca66a5d74835ed01b35a2b8b76d1ba105e565731
benjaminconti <benjamin.conti@mercadolibre.com>
2021-12-14 12:35:01 -0300

    ejercicio1 done

---
+++ 12_testing3/TM/proyecto/internal/productos/service_test.go
@@ -0,0 +1,195 @@
+package internal
+
+import (
+	"encoding/json"
+	"errors"
+	"testing"
+
+	"github.com/extmatperez/meli_bootcamp2/11_testing2/TT/proyecto/pkg/store"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateServiceMock(t *testing.T) {
+	//Arrenge
+	newProduct := Producto{
+		ID:            1,
+		Nombre:        "Asdf",
+		Color:         "Negro",
+		Precio:        "$ 50",
+		Stock:         2,
+		Codigo:        "123456R",
+		Publicado:     true,
+		FechaCreacion: "01/06/1996",
+	}
+
+	dbMock := store.Mock{Data: []byte(productosTest)}
+
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	//Act
+	createdProduct, err := service.Store(newProduct.Nombre, newProduct.Color, newProduct.Precio, newProduct.Stock, newProduct.Codigo, newProduct.Publicado, newProduct.FechaCreacion)
+
+	//Assert
+	assert.Equal(t, newProduct.Nombre, createdProduct.Nombre)
+	assert.Equal(t, newProduct.Codigo, createdProduct.Codigo)
+	assert.Nil(t, err)
+	assert.True(t, dbMock.Used)
+}
+
+func TestCreateServiceMockError(t *testing.T) {
+	//Arrenge
+	newProduct := Producto{
+		ID:            1,
+		Nombre:        "Asdf",
+		Color:         "Negro",
+		Precio:        "$ 50",
+		Stock:         2,
+		Codigo:        "123456R",
+		Publicado:     true,
+		FechaCreacion: "01/06/1996",
+	}
+
+	expectedError := errors.New("error en los datos del Mock")
+
+	dbMock := store.Mock{Data: []byte(productosTest), Err: expectedError}
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	//Act
+	_, err := service.Store(newProduct.Nombre, newProduct.Color, newProduct.Precio, newProduct.Stock, newProduct.Codigo, newProduct.Publicado, newProduct.FechaCreacion)
+
+	//Assert
+	assert.Equal(t, expectedError, err)
+	assert.True(t, dbMock.Used)
+}
+
+func TestGetAllMock(t *testing.T) {
+	//Arrenge
+	dbMock := store.Mock{Data: []byte(productosTest)}
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	var expectedList []Producto
+	errUnmarshal := json.Unmarshal([]byte(productosTest), &expectedList)
+
+	//Act
+	productsList, err := service.GetAll()
+
+	//Assert
+	assert.Equal(t, expectedList, productsList)
+	assert.Nil(t, err)
+	assert.Nil(t, errUnmarshal)
+	assert.True(t, dbMock.Used)
+}
+
+func TestGetAllMockError(t *testing.T) {
+	//Arrenge
+	expectedError := errors.New("The Mock is empty")
+
+	dbMock := store.Mock{Data: []byte(``), Err: expectedError}
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	//Act
+	_, err := service.GetAll()
+
+	//Assert
+	assert.Equal(t, expectedError, err)
+	assert.True(t, dbMock.Used)
+}
+
+func TestUpdateServiceMock(t *testing.T) {
+	//Arrenge
+	productToUpdate := Producto{
+		ID:            1,
+		Nombre:        "Asdf",
+		Color:         "Negro",
+		Precio:        "$ 50",
+		Stock:         2,
+		Codigo:        "123456R",
+		Publicado:     true,
+		FechaCreacion: "01/06/1996",
+	}
+
+	dbMock := store.Mock{Data: []byte(productosTest)}
+
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	//Act
+	updatedProduct, err := service.Update(productToUpdate.ID, productToUpdate.Nombre, productToUpdate.Color, productToUpdate.Precio, productToUpdate.Stock, productToUpdate.Codigo, productToUpdate.Publicado, productToUpdate.FechaCreacion)
+
+	//Assert
+	assert.Equal(t, productToUpdate, updatedProduct)
+	assert.Nil(t, err)
+	assert.True(t, dbMock.Used)
+}
+
+func TestUpdateServiceMockError(t *testing.T) {
+	//Arrenge
+	productToUpdate := Producto{
+		ID:            0,
+		Nombre:        "Asdf",
+		Color:         "Negro",
+		Precio:        "$ 50",
+		Stock:         2,
+		Codigo:        "123456R",
+		Publicado:     true,
+		FechaCreacion: "01/06/1996",
+	}
+
+	dbMock := store.Mock{Data: []byte(productosTest)}
+
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	//Act
+	_, err := service.Update(productToUpdate.ID, productToUpdate.Nombre, productToUpdate.Color, productToUpdate.Precio, productToUpdate.Stock, productToUpdate.Codigo, productToUpdate.Publicado, productToUpdate.FechaCreacion)
+
+	//Assert
+	assert.Error(t, err)
+	assert.True(t, dbMock.Used)
+}
+
+func TestDeleteServiceMock(t *testing.T) {
+	//Arrenge
+	productToUpdateID := 1
+
+	dbMock := store.Mock{Data: []byte(productosTest)}
+
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	//Act
+	err := service.Delete(productToUpdateID)
+
+	//Assert
+	assert.Nil(t, err)
+	assert.True(t, dbMock.Used)
+}
+
+func TestDeleteServiceMockError(t *testing.T) {
+	//Arrenge
+	productToUpdateID := 0
+
+	dbMock := store.Mock{Data: []byte(productosTest)}
+
+	storeStub := store.FileStore{Mock: &dbMock}
+	repository := NewRepository(&storeStub)
+	service := NewService(repository)
+
+	//Act
+	err := service.Delete(productToUpdateID)
+
+	//Assert
+	assert.Error(t, err)
+	assert.True(t, dbMock.Used)
+}
