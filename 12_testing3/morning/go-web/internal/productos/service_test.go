94e2b1c43cce98a9e050949aa4488d54590219bf
Ariel Romero <arielrom27@gmail.com>
2021-12-15 19:00:48 -0300

    add(productos) exercise 3 solved

--- 12_testing3/morning/go-web/internal/productos/service_test.go
+++ 12_testing3/morning/go-web/internal/productos/service_test.go
@@ -1,7 +1,8 @@
 package internal
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
 	"testing"
 
 	"github.com/extmatperez/meli_bootcamp2/11_testing2/afternoon/go-web/pkg/store"
@@ -76,21 +77,62 @@ func TestDelete(t *testing.T) { //test para cuando el ID si existe
 
 	// Se ejecuta el test
 	//para ver que efectivamente se borro el producto, hago dos pruebas, intentando borrar el mismo registro dos veces
-	respuesta1, err1 := service.Delete(1)
-	respuesta2, err2 := service.Delete(1)
+	response1, err1 := service.Delete(1)
+	response2, err2 := service.Delete(1)
 
 	// Se validan los resultados
 	//sabiendo las respuestas de Delete, hago las validaciones pertinentes
 	//cuando SI existe el producto:
-	assert.Equal(t, "Producto eliminado", respuesta1, "deben ser iguales")
+	assert.Equal(t, "Producto eliminado", response1, "deben ser iguales")
 	assert.Nil(t, err1, "debe ser nil")
 
 	//cuando NO existe el producto:
-	assert.Equal(t, "", respuesta2, "deben ser iguales")
+	assert.Equal(t, "", response2, "deben ser iguales")
 	assert.NotNil(t, err2, "debe ser distinto de nil")
+}
 
-	fmt.Println("1------", respuesta1)
-	fmt.Println("2------", respuesta2)
-	fmt.Println("3------", err1)
-	fmt.Println("4------", err2)
+func TestGetAll(t *testing.T) {
+	//Arrange
+	myMockStore := store.MockStore{Data: []byte(testStorage)}
+	store := store.FileStore{Mock: &myMockStore}
+	repo := NewRepository(&store)
+	service := NewService(repo)
+	var mockData []Producto
+	json.Unmarshal([]byte(testStorage), &mockData)
+	//fmt.Println(mockData)
+
+	//Act
+	response, _ := service.GetAll()
+	//fmt.Println(response)
+
+	//Assert
+	assert.Equal(t, mockData, response, "deben ser iguales")
+}
+
+func TestErrorGetAll(t *testing.T) {
+	//Arrange
+	myMockStore := store.MockStore{Err: errors.New("Error")}
+	store := store.FileStore{Mock: &myMockStore}
+	repo := NewRepository(&store)
+	service := NewService(repo)
+
+	//Act
+	_, err := service.GetAll()
+
+	//Assert
+	assert.Error(t, err, "debe ser error")
+}
+
+func TestStore(t *testing.T) {
+	//Arrange
+	myMockStore := store.MockStore{Data: []byte(testStorage)}
+	store := store.FileStore{Mock: &myMockStore}
+	repo := NewRepository(&store)
+	service := NewService(repo)
+	testProduct := Producto{3, "mate", "marron", 99.9, 10, "mat 111", true, "12/9/1999"}
+	//Act
+	product, _ := service.Store("mate", "marron", 99.9, 10, "mat 111", true, "12/9/1999")
+
+	//Assert
+	assert.Equal(t, testProduct, product, "deben ser iguales")
 }
