46ea48807a18c02d0897d503fc5bedeb8dfc0cef
Matias Ziliotto <matias.ziliotto@mercadolibre.com>
2021-12-15 12:09:18 -0300

    Ejercicios SQL TM

---
+++ 13_sql1/Ejercicio3-script-insert.sql
@@ -0,0 +1,23 @@
+# Insert into departments
+INSERT INTO departments (name, address) VALUES ("IT", "PIL1");
+INSERT INTO departments (name, address) VALUES ("People", "PIL2");
+INSERT INTO departments (name, address) VALUES ("QA", "PIL3");
+INSERT INTO departments (name, address) VALUES ("Prev. de Fraude", "PIL4");
+INSERT INTO departments (name, address) VALUES ("Finanzas", "PIL5");
+
+# Insert into employees
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (1, "#1000", 40100200, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (1, "#1001", 40100201, "Jorge", "Miño", "1997-11-21", "2021-11-15", "Software Developer Analyst", 106000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (1, "#1002", 40100202, "Julian", "Rodriguez Petz", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100400.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (1, "#1003", 40100203, "Nahuel", "Sotuyo", "1997-11-21", "2021-11-15", "Software Developer Analyst", 104000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (1, "#1004", 40100204, "Nahuel", "Scerca", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (1, "#1005", 40100205, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (2, "#1006", 40100206, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (2, "#1007", 40100207, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (2, "#1008", 40100208, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (3, "#1009", 40100209, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (3, "#1010", 40100210, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (4, "#1011", 40100211, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (4, "#1012", 40100212, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (5, "#1013", 40100213, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);
+INSERT INTO employees (department_id, legajo, dni, name, last_name, birthdate, incorporation, position, net_income) VALUES (5, "#1014", 40100214, "Matias", "Ziliotto", "1997-11-21", "2021-11-15", "Software Developer Analyst", 100000.00);departments
