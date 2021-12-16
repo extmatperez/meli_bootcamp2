0b736bcadd9252429b7fe9ae70cca225153d642a
FacuBouza <facundo.bouza@mercadolibre.com>
2021-12-15 21:41:39 -0300

    Add testing2 excercise2

--- 11_testing2/PracticaTT/internal/transacciones/service_test.go
+++ 11_testing2/PracticaTT/internal/transacciones/service_test.go
@@ -17,7 +17,7 @@ var transactions []Transaccion = []Transaccion{
 	{3, "asdfv32", "pesos", 1231.33, "Sebastian", "Esteban", "21/09/2021"},
 }
 
-func TestUpdateMock(t *testing.T) {
+func TestUpdateTransacMock(t *testing.T) {
 	//Arrange
 	var newTransac Transaccion = Transaccion{1, "abcd1234", "dolar", 550.33, "Facundo", "Esteban", "21/11/2021"}
 
@@ -29,8 +29,40 @@ func TestUpdateMock(t *testing.T) {
 	repo := NewRepository(&storeStub)
 	service := NewService(repo)
 
+	//Act
 	resTransac, err := service.Update(newTransac.Id, newTransac.CodTransaccion, newTransac.Moneda, newTransac.Monto, newTransac.Emisor, newTransac.Receptor, newTransac.FechaTrans)
 
+	//Assert
 	assert.Equal(t, resTransac, newTransac)
 	assert.Nil(t, err)
+}
+
+func TestDeleteTransacMock(t *testing.T) {
+	//Arrange
+	transSpected := Transaccion{2, "c1c2c3", "pesos", 5555.33, "Jorge", "Esteban", "21/09/2021"}
+
+	dataByte, _ := json.Marshal(transactions)
+	dbMock := store.MockStore{Data: dataByte}
+	storeStub := store.FileStore{Mock: &dbMock}
+
+	repo := NewRepository(&storeStub)
+	service := NewService(repo)
+
+	//Act
+	transDeleted1, err1 := service.Delete(2)
+	transDeleted2, err2 := service.Delete(8)
+
+	transDeleted3, err3 := service.Search("2")
+
+	//Assert
+	assert.Equal(t, transSpected, transDeleted1)
+	assert.Nil(t, err1)
+
+	assert.Empty(t, transDeleted2)
+	assert.NotNil(t, err2)
+	assert.Error(t, err2)
+
+	assert.Empty(t, transDeleted3)
+	assert.NotNil(t, err3)
+	assert.Error(t, err3)
 }
