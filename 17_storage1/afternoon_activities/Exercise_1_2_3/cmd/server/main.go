059c10e6748e07a9e263c1a1032fc023564e6a74
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-28 09:44:57 -0300

    🚚  Copy exercise 12 to continue applying storage & testing

---
+++ 17_storage1/afternoon_activities/Exercise_1_2_3/cmd/server/main.go
@@ -0,0 +1,113 @@
+/* Ejercicio 1 - Mejorar el código
+Se debe utilizar Golangci-lint para detectar y corregir todos los posibles errores en el proyecto Go-Web. Para eso se deben seguir los
+siguientes pasos:
+Instalar Golangci-lint.
+Ejecutar Golangci-lint (con la configuración por defecto es suficiente).
+Si existe algún error, se debe corregir.
+
+--------------------------------------------------------------------------------------------------------------------
+Ejercicio 2 - Medir el Coverage
+
+Se debe medir el Coverage del proyecto Go - Web, para eso se deben seguir los siguientes pasos:
+Medir el code coverage del proyecto Go - Web.
+Generar el coverage report del proyecto Go - Web
+Identificar todos las partes del código que no tienen o tienen poca cobertura.
+
+--------------------------------------------------------------------------------------------------------------------
+
+Ejercicio 3 - Aumentar el Coverage
+
+Para mejorar la calidad del código se debe aumentar el coverage del proyecto Go - Web, seguir los siguientes pasos:
+
+Después de identificar las partes del código que no tienen cobertura, se debe elegir una o más partes del código donde sea razonable aumentar la cobertura.
+Aumentar la cobertura de las partes elegidas.
+Comparar el code coverage y el coverage report actual contra el anterior.
+*/
+
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/extmatperez/meli_bootcamp2/tree/montenegro_edgar/17_storage1/afternoon_activities/Exercise_1_2_3/cmd/server/handler"
+	"github.com/extmatperez/meli_bootcamp2/tree/montenegro_edgar/17_storage1/afternoon_activities/Exercise_1_2_3/docs"
+	users "github.com/extmatperez/meli_bootcamp2/tree/montenegro_edgar/17_storage1/afternoon_activities/Exercise_1_2_3/internal/users"
+	"github.com/extmatperez/meli_bootcamp2/tree/montenegro_edgar/17_storage1/afternoon_activities/Exercise_1_2_3/pkg/store"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+)
+
+func respondWithError(c *gin.Context, code int, message interface{}) {
+	c.AbortWithStatusJSON(code, gin.H{"error": message})
+}
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	requiredToken := os.Getenv("TOKEN")
+
+	// We want to make sure the token is set, bail if not
+	if requiredToken == "" {
+		log.Fatal("Please set API_TOKEN environment variable")
+	}
+	return func(c *gin.Context) {
+		token := c.GetHeader("token")
+
+		if token == "" {
+			respondWithError(c, 401, "API token is required")
+			return
+		}
+		if token != requiredToken {
+			respondWithError(c, 401, "Invalid API token")
+			return
+		}
+		c.Next()
+	}
+}
+
+// @title MELI Bootcamp API
+// @version 1.0
+// @description This API Handle MELI Users.
+// @termsOfService https://developers.mercadolibre.com.ar/es_ar/terminos-y-condiciones
+
+// @contact.name API Support
+// @contact.url https://developers.mercadolibre.com.ar/support
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// Creo la función main, agrego mi router y lo inicializo, creo las rutas necesarias y agrego los handlers (controllers)
+func main() {
+	// Levanto las variables de entorno del archivo .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Something went wrong with the .env file")
+	}
+
+	docs.SwaggerInfo.Host = os.Getenv("HOST")
+
+	// Creamos una variable que guarde lo que vamos a escribir en nuestro json y se lo pasamos el repo
+	db := store.New(store.File_type, "./users.json")
+	repo := users.New_repository(db)
+	service := users.New_service(repo)
+	controller := handler.New_user(service)
+
+	router := gin.Default()
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	// Utilizo el middleware que va a autenticar el token, debajo de las rutas de documentación para que no cree conflicto
+	router.Use(TokenAuthMiddleware())
+
+	router.GET("/users", controller.Get_users())
+	router.POST("/users", controller.Post_users())
+	router.PUT("/users/:id", controller.Update_users())
+	router.PATCH("/users/:id", controller.Update_users_first_name())
+	router.DELETE("/users/:id", controller.Delete_users())
+
+	err = router.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
