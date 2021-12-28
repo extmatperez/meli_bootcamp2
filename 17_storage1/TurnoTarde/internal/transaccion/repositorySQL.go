c0578bdc9cb14af29ceebcd58e764d0d968c1515
FranciscoPalacioMeli <francisco.fpalacio@mercadolibre.com>
2021-12-28 10:33:39 -0300

    getallsql ready

--- 17_storage1/TurnoTarde/internal/transaccion/repositorySQL.go
+++ 17_storage1/TurnoTarde/internal/transaccion/repositorySQL.go
@@ -6,44 +6,40 @@ import (
 
 	"github.com/extmatperez/meli_bootcamp2/tree/palacio_francisco/17_storage1/TurnoTarde/db"
 	"github.com/extmatperez/meli_bootcamp2/tree/palacio_francisco/17_storage1/TurnoTarde/internal/transaccion/models"
-
 )
 
-type RepositorySql interface{
-	Store(transaction models.Transaction) (models.Transaction,error)
-	GetById(id int) (models.Transaction,error)
-
+type RepositorySql interface {
+	Store(transaction models.Transaction) (models.Transaction, error)
+	GetById(id int) (models.Transaction, error)
+	GetAll() ([]models.Transaction, error)
 }
 
-
-type repositorySQL struct{
-
+type repositorySQL struct {
 }
 
-func NewRepositorySQL() RepositorySql{
+func NewRepositorySQL() RepositorySql {
 	return &repositorySQL{}
 }
 
 const (
 	InsertOne = "INSERT INTO transaction(Codigo,Moneda,Monto,Emisor,Receptor,Fecha)" +
-										"VALUES(?, ?, ?, ?, ?, ?)"
-	GetById= "SELECT Id,Codigo,Moneda,Monto,Emisor,Receptor,Fecha FROM transaction WHERE Id=?"
+		"VALUES(?, ?, ?, ?, ?, ?)"
+	GetById = "SELECT Id,Codigo,Moneda,Monto,Emisor,Receptor,Fecha FROM transaction WHERE Id=?"
+	GetAll  = "SELECT Id,Codigo,Moneda,Monto,Emisor,Receptor,Fecha FROM transaction"
 )
 
-
-
-func (r *repositorySQL) Store(transaction models.Transaction) (models.Transaction,error){
-	db := db.StorageDB // se inicializa la base
+func (r *repositorySQL) Store(transaction models.Transaction) (models.Transaction, error) {
+	db := db.StorageDB                 // se inicializa la base
 	stmt, err := db.Prepare(InsertOne) // se prepara el SQL
 	if err != nil {
-	log.Fatal(err)
+		return models.Transaction{},err
 	}
 	defer stmt.Close() // se cierra la sentencia al terminar. Si quedan abiertas se genera consumos de memoria
 	var result sql.Result
-	result, err = stmt.Exec(transaction.Codigo,transaction.Moneda,transaction.Monto,transaction.Emisor,
-		transaction.Receptor,transaction.Fecha) // retorna un sql.Result y un error
+	result, err = stmt.Exec(transaction.Codigo, transaction.Moneda, transaction.Monto, transaction.Emisor,
+		transaction.Receptor, transaction.Fecha) // retorna un sql.Result y un error
 	if err != nil {
-	return models.Transaction{}, err
+		return models.Transaction{}, err
 	}
 	insertedId, _ := result.LastInsertId() // del sql.Resul devuelto en la ejecución obtenemos el Id insertado
 	transaction.ID = int(insertedId)
@@ -51,25 +47,43 @@ func (r *repositorySQL) Store(transaction models.Transaction) (models.Transactio
 
 }
 
-func (r *repositorySQL) GetById(id int) (models.Transaction,error){
+func (r *repositorySQL) GetAll() ([]models.Transaction, error) {
+	var transactions []models.Transaction
+	db := db.StorageDB
+	rows, err := db.Query(GetAll)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var transaction models.Transaction
+
+		err := rows.Scan(&transaction.ID, &transaction.Codigo, &transaction.Moneda, &transaction.Monto, &transaction.Emisor,
+			&transaction.Receptor, &transaction.Fecha)
+		if err != nil {
+			return nil, err
+		}
+
+		transactions = append(transactions, transaction)
+	}
+	return transactions, nil
+
+}
+
+func (r *repositorySQL) GetById(id int) (models.Transaction, error) {
 	var transaction models.Transaction
 	db := db.StorageDB
 	rows, err := db.Query(GetById, id)
 	if err != nil {
-	log.Println(err)
-	return transaction,err
+		log.Println(err)
+		return transaction, err
 	}
 	for rows.Next() {
-	if err := rows.Scan(&transaction.ID,&transaction.Codigo,&transaction.Moneda,&transaction.Monto,&transaction.Emisor,
-		&transaction.Receptor,&transaction.Fecha); err != nil {
-	log.Println(err.Error())
-	return transaction,err
+		if err := rows.Scan(&transaction.ID, &transaction.Codigo, &transaction.Moneda, &transaction.Monto, &transaction.Emisor,
+			&transaction.Receptor, &transaction.Fecha); err != nil {
+			log.Println(err.Error())
+			return transaction, err
+		}
 	}
-	}
-	return transaction,nil
-   
+	return transaction, nil
+
 }
-
-
-
-
--- 17_storage1/TurnoTarde/internal/transaccion/repository_test.go
+++ 17_storage1/TurnoTarde/internal/transaccion/repository_test.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	"testing"
 
@@ -10,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type mockStore struct{
+type mockStore struct {
 	transactionBeforeUpdate Transaction
 }
 
@@ -19,8 +20,7 @@ func (m *mockStore) Read(tran Transaction) bool {
 	return true
 }
 
-
-var Datos string =  `[{
+var Datos string = `[{
 	"id": 2,
 	"codigo": "24safdsadfasdf385",
 	"moneda": "Peso Colombiano",
@@ -39,34 +39,31 @@ var Datos string =  `[{
 	"fecha": "1/4/2021"
    }]`
 
-
-type StubStore struct{
+type StubStore struct {
 	useMethodRead bool
 }
 
-func(s *StubStore) Write(data interface{}) error{
+func (s *StubStore) Write(data interface{}) error {
 	return nil
 }
-func(s *StubStore) Read(data interface{}) error{
+func (s *StubStore) Read(data interface{}) error {
 	s.useMethodRead = true
 	return json.Unmarshal([]byte(Datos), &data)
 }
 
-
-type StubStoreError struct{
+type StubStoreError struct {
 	useMethodRead bool
 }
 
-func(s *StubStoreError) Write(data interface{}) error{
+func (s *StubStoreError) Write(data interface{}) error {
 	return errors.New("Error al cargar transaccion")
 }
-func(s *StubStoreError) Read(data interface{}) error{
+func (s *StubStoreError) Read(data interface{}) error {
 	s.useMethodRead = true
 	return errors.New("No hay un archivo con trasnacciones")
 }
 
-
-func TestGetAll(t *testing.T){
+func TestGetAll(t *testing.T) {
 	stubStore := &StubStore{}
 	repo := NewRepository(stubStore)
 
@@ -74,117 +71,121 @@ func TestGetAll(t *testing.T){
 
 	err := json.Unmarshal([]byte(Datos), &excepted)
 
-	assert.Nil(t,err)
-	
-	tran,err := repo.GetAll()
+	assert.Nil(t, err)
 
-	assert.Nil(t,err)
-	assert.Equal(t,excepted,tran)
+	tran, err := repo.GetAll()
+
+	assert.Nil(t, err)
+	assert.Equal(t, excepted, tran)
 }
 
-func TestGetAllError(t *testing.T){
+func TestGetAllError(t *testing.T) {
 	stubStored := &StubStoreError{}
 	repod := NewRepository(stubStored)
 
-	tran,err := repod.GetAll()
+	tran, err := repod.GetAll()
 
-
-	assert.Nil(t,tran)
-	assert.True(t,stubStored.useMethodRead)
-	assert.Error(t,err)
+	assert.Nil(t, tran)
+	assert.True(t, stubStored.useMethodRead)
+	assert.Error(t, err)
 }
 
-func TestUpdateCodigo(t *testing.T){
-
+func TestUpdateCodigo(t *testing.T) {
 
 	stubStore := &StubStore{false}
 	repo := NewRepository(stubStore)
-	tran2,_ := repo.GetTransactionById(2)
-	codgUpdate :="AfterUpdatecod-123"
+	tran2, _ := repo.GetTransactionById(2)
+	codgUpdate := "AfterUpdatecod-123"
 
+	tranUpdate, err := repo.Update(2, codgUpdate, "Peso", "55.6", "pepe", "luis", "13/12/2021")
 
-	tranUpdate,err := repo.Update(2,codgUpdate,"Peso","55.6","pepe","luis","13/12/2021")
-
-	assert.True(t,stubStore.useMethodRead)
-	assert.Equal(t,tran2.ID,tranUpdate.ID)
-	assert.Equal(t,codgUpdate, codgUpdate)
-	assert.Nil(t,err)
+	assert.True(t, stubStore.useMethodRead)
+	assert.Equal(t, tran2.ID, tranUpdate.ID)
+	assert.Equal(t, codgUpdate, codgUpdate)
+	assert.Nil(t, err)
 }
 
-func TestUpdateCodigoError(t *testing.T){
+func TestUpdateCodigoError(t *testing.T) {
 	stubStore := &StubStoreError{false}
 	repo := NewRepository(stubStore)
-	codgUpdate :="AfterUpdatecod-123"
+	codgUpdate := "AfterUpdatecod-123"
 
+	tranUpdate, err := repo.Update(2, codgUpdate, "Peso", "55.6", "pepe", "luis", "13/12/2021")
 
-	tranUpdate,err := repo.Update(2,codgUpdate,"Peso","55.6","pepe","luis","13/12/2021")
-
-	assert.True(t,stubStore.useMethodRead)
-	assert.Equal(t,Transaction{},tranUpdate)
-	assert.Error(t,err)
+	assert.True(t, stubStore.useMethodRead)
+	assert.Equal(t, Transaction{}, tranUpdate)
+	assert.Error(t, err)
 }
 
-
-func TestUpdateCodigoAndMonto(t *testing.T){
+func TestUpdateCodigoAndMonto(t *testing.T) {
 	stubStore := &StubStore{false}
 	repo := NewRepository(stubStore)
-	codgUpdate :="AfterUpdatecod-123"
-	monto :="88.5"
+	codgUpdate := "AfterUpdatecod-123"
+	monto := "88.5"
 	transactionTest := Transaction{2, codgUpdate, "Peso Colombiano", monto, "Luis", "Perez", "01/01/2001"}
 	mock := &mockStore{transactionTest}
 	isRead := mock.Read(transactionTest)
 
-	tranUpdate,err := repo.UpdateCodigoAndMonto(2,codgUpdate,monto)
+	tranUpdate, err := repo.UpdateCodigoAndMonto(2, codgUpdate, monto)
 
-	assert.True(t,stubStore.useMethodRead)
-	assert.Nil(t,err)
-	assert.Equal(t,tranUpdate,mock.transactionBeforeUpdate)
-	assert.True(t,isRead)
+	assert.True(t, stubStore.useMethodRead)
+	assert.Nil(t, err)
+	assert.Equal(t, tranUpdate, mock.transactionBeforeUpdate)
+	assert.True(t, isRead)
 }
 
-func TestInsert(t *testing.T){
+func TestInsert(t *testing.T) {
 
 	transaction := models.Transaction{
-		Codigo: "24safdsadfasdf385",
-		Moneda: "Peso Colombiano",
-		Monto: "$8228845654645678",
-		Emisor: "Luis",
+		Codigo:   "24safdsadfasdf385",
+		Moneda:   "Peso Colombiano",
+		Monto:    "$8228845654645678",
+		Emisor:   "Luis",
 		Receptor: "Perez",
-		Fecha: "01/01/2001",
-	   }
+		Fecha:    "01/01/2001",
+	}
 
 	repo := NewRepositorySQL()
-	tranUpdate,err := repo.Store(transaction)
+	tranUpdate, err := repo.Store(transaction)
 
-	assert.Equal(t,transaction.Codigo,tranUpdate.Codigo)
-	assert.Equal(t,transaction.Moneda,tranUpdate.Moneda)
-	assert.Equal(t,transaction.Moneda,tranUpdate.Moneda)
-	assert.Equal(t,transaction.Emisor,tranUpdate.Emisor)
-	assert.Equal(t,transaction.Emisor,tranUpdate.Emisor)
-	assert.Equal(t,transaction.Receptor,tranUpdate.Receptor)
-	assert.Nil(t,err)
+	assert.Equal(t, transaction.Codigo, tranUpdate.Codigo)
+	assert.Equal(t, transaction.Moneda, tranUpdate.Moneda)
+	assert.Equal(t, transaction.Moneda, tranUpdate.Moneda)
+	assert.Equal(t, transaction.Emisor, tranUpdate.Emisor)
+	assert.Equal(t, transaction.Emisor, tranUpdate.Emisor)
+	assert.Equal(t, transaction.Receptor, tranUpdate.Receptor)
+	assert.Nil(t, err)
 }
 
-
-func TestGetById(t *testing.T){
+func TestGetById(t *testing.T) {
 
 	transaction := models.Transaction{
-		Codigo: "24safdsadfasdf385",
-		Moneda: "Peso Colombiano",
-		Monto: "$8228845654645678",
-		Emisor: "Luis",
+		Codigo:   "24safdsadfasdf385",
+		Moneda:   "Peso Colombiano",
+		Monto:    "$8228845654645678",
+		Emisor:   "Luis",
 		Receptor: "Perez",
-		Fecha: "01/01/2001",
-	   }
+		Fecha:    "01/01/2001",
+	}
 
 	repo := NewRepositorySQL()
-	tranUpdate,err := repo.GetById(1)
+	tranUpdate, err := repo.GetById(1)
 
-	assert.Equal(t,transaction.Codigo,tranUpdate.Codigo)
-	assert.Equal(t,transaction.Moneda,tranUpdate.Moneda)
-	assert.Equal(t,transaction.Moneda,tranUpdate.Moneda)
-	assert.Equal(t,transaction.Emisor,tranUpdate.Emisor)
-	assert.Equal(t,transaction.Emisor,tranUpdate.Emisor)
-	assert.Equal(t,transaction.Receptor,tranUpdate.Receptor)
-	assert.Nil(t,err)
+	assert.Equal(t, transaction.Codigo, tranUpdate.Codigo)
+	assert.Equal(t, transaction.Moneda, tranUpdate.Moneda)
+	assert.Equal(t, transaction.Moneda, tranUpdate.Moneda)
+	assert.Equal(t, transaction.Emisor, tranUpdate.Emisor)
+	assert.Equal(t, transaction.Emisor, tranUpdate.Emisor)
+	assert.Equal(t, transaction.Receptor, tranUpdate.Receptor)
+	assert.Nil(t, err)
+}
+
+func TestGetAllSql(t *testing.T) {
+
+	repo := NewRepositorySQL()
+	transUpdate, err := repo.GetAll()
+	fmt.Println(transUpdate)
+	assert.NotNil(t, transUpdate)
+	assert.True(t, len(transUpdate) >= 0)
+	assert.Nil(t, err)
 }
