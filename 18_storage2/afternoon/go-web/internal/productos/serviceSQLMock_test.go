cabd2ca2b2107706b3071cfc9595f9393215c658
Ariel Romero <arielrom27@gmail.com>
2021-12-28 18:27:45 -0300

    18_storage2 - internal - testing Mock added(activity 3)

---
+++ 18_storage2/afternoon/go-web/internal/productos/serviceSQLMock_test.go
@@ -0,0 +1,101 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/extmatperez/meli_bootcamp2/18_storage2/afternoon/go-web/internal/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStoreOkMock(t *testing.T) {
+
+	//arrange
+	productoNuevo := models.Producto{
+		Nombre:        "Camisa3",
+		Color:         "Roja",
+		Precio:        10.0,
+		Stock:         10,
+		Codigo:        "CAM-021",
+		Publicado:     true,
+		FechaCreacion: "2020-01-01",
+	}
+
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	mock.ExpectPrepare("INSERT INTO productos")
+	mock.ExpectExec("").WillReturnResult(sqlmock.NewResult(3, 1))
+
+	repo := NewRepositorySQLMock(db)
+	service := NewServiceSQLMock(repo)
+
+	//act
+	producto, err := service.Store(productoNuevo)
+
+	//assert
+	assert.Nil(t, err)
+	assert.Equal(t, 3, producto.ID)
+}
+
+func TestGetOneOkMock(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "nombre", "color", "precio", "stock", "codigo", "publicado", "fechaCreacion"})
+	rows.AddRow(6, "Camisa1", "Roja", 10.0, 10, "CAM-001", true, "2020-01-01")
+	mock.ExpectQuery(getOne).WithArgs(6).WillReturnRows(rows)
+
+	repo := NewRepositorySQLMock(db)
+	service := NewServiceSQLMock(repo)
+
+	producto, err := service.GetOne(6)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 6, producto.ID)
+}
+
+func TestUpdateOkMock(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	producto := models.Producto{
+		Nombre:        "Camisa1",
+		Color:         "Roja",
+		Precio:        10.0,
+		Stock:         10,
+		Codigo:        "CAM-001",
+		Publicado:     true,
+		FechaCreacion: "2020-01-01",
+	}
+
+	mock.ExpectPrepare("UPDATE productos")
+	mock.ExpectExec("").WillReturnResult(sqlmock.NewResult(3, 1))
+
+	repo := NewRepositorySQLMock(db)
+	service := NewServiceSQLMock(repo)
+
+	productoDevuelto, err := service.Update(producto, 3)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 3, productoDevuelto.ID)
+}
+
+func TestDeleteOkMock(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	mock.ExpectPrepare("DELETE FROM productos")
+	mock.ExpectExec("").WillReturnResult(sqlmock.NewResult(1, 1))
+
+	repo := NewRepositorySQLMock(db)
+	service := NewServiceSQLMock(repo)
+
+	err = service.Delete(1888)
+
+	assert.Nil(t, err)
+}
---
+++ 18_storage2/afternoon/go-web/test/products_test.go
@@ -0,0 +1,114 @@
+package test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/extmatperez/meli_bootcamp2/18_storage2/afternoon/go-web/cmd/server/handler"
+	internal "github.com/extmatperez/meli_bootcamp2/18_storage2/afternoon/go-web/internal/productos"
+	"github.com/extmatperez/meli_bootcamp2/18_storage2/afternoon/go-web/pkg/store"
+	"github.com/extmatperez/meli_bootcamp2/18_storage2/afternoon/go-web/pkg/web"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func createServer() *gin.Engine {
+	_ = os.Setenv("TOKEN", "123456")
+	db := store.New(store.FileType, "../cmd/server/productos.json")
+	repo := internal.NewRepository(db)
+	service := internal.NewService(repo)
+	handler := handler.NewProducto(service)
+
+	r := gin.Default()
+
+	pr := r.Group("/productos")
+	pr.PUT("/:id", handler.Update())
+	pr.DELETE("/:id", handler.Delete())
+
+	return r
+}
+
+func createRequestTest(method string, url string, body string) (*http.Request, *httptest.ResponseRecorder) {
+	req := httptest.NewRequest(method, url, bytes.NewBuffer([]byte(body)))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("TOKEN", "123456")
+	return req, httptest.NewRecorder()
+}
+
+func Test_UpdateProduct_OK(t *testing.T) {
+	// crear el Server y definir las Rutas
+	r := createServer()
+	sentProd := `{
+		"nombre": "after Update",
+		"color": "gris",
+		"precio": 1,
+		"stock": 12,
+		"codigo": "sfsdf 444 3 www",
+		"publicado": false,
+		"fechaCreacion": "12/9/1999"
+	  }`
+	var objRes web.Response
+	// crear Request del tipo Put y Response para obtener el resultado
+	req, rr := createRequestTest(http.MethodPut, "/productos/1", sentProd)
+
+	// indicar al servidor que pueda atender la solicitud
+	r.ServeHTTP(rr, req)
+	//guardo la respuesta en una estructura de tipo web.Response para hacer una evaluacion mas especifica
+	err := json.Unmarshal(rr.Body.Bytes(), &objRes)
+	assert.Equal(t, 200, rr.Code)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, objRes.StatusCode, "deben ser iguales")
+}
+
+func Test_UpdateProduct_Fail(t *testing.T) { //falla porque se le envia un numero de id que no existe
+	// crear el Server y definir las Rutas
+	r := createServer()
+	sentProd := `{
+		"nombre": "after Update",
+		"color": "gris",
+		"precio": 1,
+		"stock": 12,
+		"codigo": "sfsdf 444 3 www",
+		"publicado": false,
+		"fechaCreacion": "12/9/1999"
+	  }`
+	var objRes web.Response
+
+	// crear Request del tipo Put y Response para obtener el resultado
+	req, rr := createRequestTest(http.MethodPut, "/productos/9999", sentProd)
+
+	// indicar al servidor que pueda atender la solicitud
+	r.ServeHTTP(rr, req)
+	//guardo la respuesta en una estructura de tipo web.Response para hacer una evaluacion mas especifica
+	err := json.Unmarshal(rr.Body.Bytes(), &objRes)
+	assert.Equal(t, 400, rr.Code, "debe ser 400")
+
+	assert.Nil(t, err)
+	assert.Equal(t, 400, objRes.StatusCode, "deben ser iguales")
+}
+
+func Test_DeleteProduct_Ok(t *testing.T) {
+	// crear el Server y definir las Rutas
+	r := createServer()
+
+	var objRes web.Response
+
+	// crear Request del tipo Delete y Response para obtener el resultado
+	req, rr := createRequestTest(http.MethodDelete, "/productos/7", "")
+
+	// indicar al servidor que pueda atender la solicitud
+	r.ServeHTTP(rr, req)
+
+	//guardo la respuesta en una estructura de tipo web.Response para hacer una evaluacion mas especifica
+	err := json.Unmarshal(rr.Body.Bytes(), &objRes)
+
+	assert.Equal(t, 200, rr.Code, "debe ser 200")
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, objRes.StatusCode, "deben ser iguales")
+}
