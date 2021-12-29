f7c32f797042faf6611f740bb7e2afd7d6e887f6
emontenegro <edgar.montenegro@mercadolibre.com>
2021-12-28 23:34:54 -0300

    👷  Add City struct to do the relation between users_sql & city tables in DB

--- 18_storage2/morning_activities/Exercise_1_2_3/internal/models/models_users.go
+++ 18_storage2/morning_activities/Exercise_1_2_3/internal/models/models_users.go
@@ -1,6 +1,12 @@
 package models
 
 // Estructura de los datos que voy a manipular
+type City struct {
+	ID          int    `json:"id"`
+	CityName    string `json:"city_name"`
+	CountryName string `json:"country_name"`
+}
+
 type Users struct {
 	ID        int    `json:"id"`
 	FirstName string `json:"first_name"`
@@ -10,4 +16,5 @@ type Users struct {
 	Height    int    `json:"height"`
 	Active    bool   `json:"active"`
 	Date      string `json:"date"`
+	Address   City   `json:"address"`
 }
